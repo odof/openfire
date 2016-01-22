039a4cedb0f266e6172cd8473a8af6a987350b34
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2016-01-22 17:56:49 +0100

    of_zz_fdc07: Affichage du planning dans le formulaire du partenaire. Non duplication des plannings a la duplication du partenaire

--- of_zz_fdc07/of_zz_fdc07.py
+++ of_zz_fdc07/of_zz_fdc07.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class res_partner(models.Model):
-    
     _inherit = 'res.partner'
-    
+
     of_service = fields.Char(u'Service', size=64)
-    
+
+    @api.one
+    def copy(self, default=None):
+        default = dict(default or {}, meeting_ids=[])
+        return super(res_partner, self).copy(default)
