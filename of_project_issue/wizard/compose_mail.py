296a959c80fd70940d376e082d1e6a96fc732636
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2017-01-27 09:59:55 +0100

    of_project_issue : Ajout de la possibilite d'afficher les categories de sav dans les courriers

--- of_project_issue/wizard/compose_mail.py
+++ of_project_issue/wizard/compose_mail.py
@@ -23,13 +23,21 @@ class of_compose_mail(models.TransientModel):
         result = super(of_compose_mail,self)._get_dict_values(data, o, objects)
 
         sav = objects.get('sav')
+        sav_categ = sav and sav.of_categorie_id
+        sav_categ_mere = sav_categ and sav_categ.parent_id
+        while sav_categ_mere.parent_id:
+            sav_categ_mere = sav_categ_mere.parent_id
+
         result.update({
-            'sav_of_code'     : sav and sav.of_code,
-            'sav_name'        : sav and sav.name or '',
-            'sav_user'        : sav and sav.user_id and sav.user_id.name or '',
-            'sav_description' : sav and sav.description or '',
-            'sav_pieces'      : sav and sav.of_piece_commande or '',
-            'sav_intervention': sav and sav.of_intervention or '',
+            'sav_of_code'      : sav and sav.of_code,
+            'sav_name'         : sav and sav.name or '',
+            'sav_user'         : sav and sav.user_id and sav.user_id.name or '',
+            'sav_description'  : sav and sav.description or '',
+            'sav_pieces'       : sav and sav.of_piece_commande or '',
+            'sav_intervention' : sav and sav.of_intervention or '',
+            'sav_categ'        : sav_categ and sav_categ.name or '',
+            'sav_categ_mere'   : sav_categ_mere and sav_categ_mere.name or '',
+            'sav_categ_complet': sav_categ and sav_categ.name_get()[0][1] or '',
 
             # Lignes gardées pour compatibilité, mais pi référence désormais les parcs installés
             'pi_of_code'              : sav and sav.of_code,
