d16cb62bdfe9d3f1b95ef0689b708d6aaab00e9d
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2017-02-24 15:03:46 +0100

    of_planning :  - Dissociation adresse - client dans les interventions  - Saisie d'interventions depuis le formulaire du client / du contact

--- of_planning/models/of_planning_intervention.py
+++ of_planning/models/of_planning_intervention.py
@@ -7,24 +7,24 @@ from __builtin__ import False
 
 class OfPlanningTache(models.Model):
     _name = "of.planning.tache"
-    _description = u"Planning OpenFire : Tâches"
+    _description = u"Planning OpenFire : Taches"
 
     name = fields.Char(u'Libellé', size=64, required=True)
     description = fields.Text('Description')
     verr = fields.Boolean(u'Verrouillé')
     product_id = fields.Many2one('product.product', 'Produit')
     active = fields.Boolean('Actif', default=True)
-    imp_detail = fields.Boolean(u'Imprimer Détail', help=u"""Impression du détail des tâches dans le planning semaine
-Si cette option n'est pas cochée, seule la tâche le plus souvent effectuée dans la journée apparaîtra""", default=True)
+    imp_detail = fields.Boolean(u'Imprimer Détail', help=u"""Impression du détail des taches dans le planning semaine
+Si cette option n'est pas cochée, seule la tache la plus souvent effectuée dans la journée apparaîtra""", default=True)
     duree = fields.Float(u'Durée par défaut', digits=(12, 5), default=1.0)
     category_id = fields.Many2one('hr.employee.category', string=u"Catégorie d'employés")
-    is_crm = fields.Boolean(u'Tâche CRM')
+    is_crm = fields.Boolean(u'Tache CRM')
     equipe_ids = fields.Many2many('of.planning.equipe', 'equipe_tache_rel', 'tache_id', 'equipe_id', 'Équipes')
 
     @api.multi
     def unlink(self):
         if self.search([('id','in',self._ids),('verr','=',True)]):
-            raise ValidationError(u'Vous essayez de supprimer une tâche verrouillée.')
+            raise ValidationError(u'Vous essayez de supprimer une tache verrouillée.')
         return super(OfPlanningTache, self).unlink()
 
 class OfPlanningEquipe(models.Model):
@@ -94,17 +94,92 @@ class OfPlanningIntervention(models.Model):
 #             result[intervention.id] = cal_color and (intervention.state == 'draft' and cal_color.color2 or cal_color.color) or ''
 #         return result
 
+    @api.depends('date', 'duree', 'hor_md', 'hor_mf', 'hor_ad', 'hor_af', 'hor_sam', 'hor_dim')
+    def _get_date_deadline(self):
+        for intervention in self:
+            if intervention.hor_md > 24 or intervention.hor_mf > 24 or intervention.hor_ad > 24 or intervention.hor_af > 24:
+                raise UserError(u"L'heure doit être inferieure ou égale à 24")
+            if intervention.hor_mf < intervention.hor_md or intervention.hor_ad < intervention.hor_mf:
+                raise UserError(u"L'heure de début ne peut pas être supérieure à l'heure de fin")
+            if intervention.hor_ad < intervention.hor_mf:
+                raise UserError(u"L'heure de l'après-midi ne peut pas être inférieure à l'heure du matin")
+
+            if not intervention.date:
+                return
+            if not intervention.duree:
+                return
+
+            # Datetime UTC
+            dt_utc = datetime.strptime(intervention.date, "%Y-%m-%d %H:%M:%S")
+            # Datetime local
+            dt_local = fields.Datetime.context_timestamp(intervention, dt_utc)
+
+            weekday = dt_local.weekday()
+            if weekday == 5 and not intervention.hor_sam:
+                raise UserError(u"L'équipe ne travaille pas le samedi")
+            elif weekday == 6 and not intervention.hor_dim:
+                raise UserError(u"L'équipe ne travaille pas le dimanche")
+
+            duree_repos = intervention.hor_ad - intervention.hor_mf
+            duree_matin = intervention.hor_mf - intervention.hor_md
+            duree_apres = intervention.hor_af - intervention.hor_ad
+            duree_jour = duree_matin + duree_apres
+
+            dt_heure = dt_local.hour + (dt_local.minute + dt_local.second / 60.0) / 60.0
+            # Déplacement de l'horaire de début au début de la journée pour faciliter le calcul
+            duree = intervention.duree
+            if intervention.hor_md <= dt_heure <= intervention.hor_mf:
+                duree += dt_heure - intervention.hor_md
+            elif intervention.hor_ad <= dt_heure <= intervention.hor_af:
+                duree += duree_matin + dt_heure - intervention.hor_ad
+            else:
+                # L'horaire de debut des travaux est en dehors des heures de travail
+                raise UserError(u"Il faut respecter l'horaire de travail")
+            dt_local -= timedelta(hours=dt_heure)
+
+            # Calcul du nombre de jours
+            jours, duree = duree // duree_jour, duree % duree_jour
+
+            if not (intervention.hor_sam and intervention.hor_dim):
+                # Deplacement de l'horaire de debut au debut de la semaine pour faciliter le calcul
+                # Le debut de la semaine peut eventuellement etre un dimanche matin
+                jours_sem = (weekday + intervention.hor_dim) % 6
+                dt_local -= timedelta(days=jours_sem)
+                jours += jours_sem
+
+                # Ajout des jours de repos a la duree de la tache pour arriver la meme date de fin
+                jours += (2 - intervention.hor_sam - intervention.hor_dim) * (jours // (5 + intervention.hor_sam + intervention.hor_dim))
+
+            # Ajout des heures non travaillées de la derniere journée
+            duree += intervention.hor_md + (duree > duree_matin and duree_repos)
+
+            # Calcul de la nouvelle date
+            dt_local += timedelta(days=jours, hours=duree)
+            # Conversion en UTC
+            dt_utc = dt_local - dt_local.tzinfo._utcoffset
+            date_deadline = dt_utc.strftime("%Y-%m-%d %H:%M:%S")
+            intervention.date_deadline = date_deadline
+
+    @api.depends('address_id', 'address_id.parent_id')
+    def _get_partner_id(self):
+        for intervention in self:
+            partner = intervention.address_id or False
+            if partner:
+                while partner.parent_id:
+                    partner = partner.parent_id
+            intervention.partner_id = partner and partner.id
+
     name = fields.Char(string=u'Libellé', required=True)
     date = fields.Datetime(string='Date intervention', required=True)
-    date_deadline = fields.Datetime(string='Deadline')
-    date_deadline_display = fields.Datetime(related='date_deadline', string="Date Fin")
+    date_deadline = fields.Datetime(compute="_get_date_deadline", string='Date Fin', store=True)
     duree = fields.Float(string=u'Durée intervention', required=True, digits=(12, 5))
     user_id = fields.Many2one('res.users', string='Utilisateur', default=lambda self: self.env.uid)
-    partner_id = fields.Many2one('res.partner', string='Client', oldname='part_id')
-    partner_city = fields.Char(related='partner_id.city')
+    partner_id = fields.Many2one('res.partner', string='Client', compute='_get_partner_id', store=True)
+    address_id = fields.Many2one('res.partner', string='Adresse')
+    partner_city = fields.Char(related='address_id.city')
     raison_id = fields.Many2one('of.planning.intervention.raison', string='Raison')
-    tache_id = fields.Many2one('of.planning.tache', string=u'Tâche', required=True)
-    equipe_id = fields.Many2one('of.planning.equipe', string='Équipe', required=True, oldname='poseur_id')
+    tache_id = fields.Many2one('of.planning.tache', string='Tache', required=True)
+    equipe_id = fields.Many2one('of.planning.equipe', string=u'Équipe', required=True, oldname='poseur_id')
     employee_ids = fields.Many2many(related='equipe_id.employee_ids', string='Intervenants', readonly=True)
     state = fields.Selection([
             ('draft', 'Brouillon'),
@@ -123,7 +198,7 @@ class OfPlanningIntervention(models.Model):
     hor_sam = fields.Boolean(string='Samedi')
     hor_dim = fields.Boolean(string='Dimanche')
 
-    category_id = fields.Many2one(related='tache_id.category_id', string=u"Type de tâche")
+    category_id = fields.Many2one(related='tache_id.category_id', string=u"Type de tache")
     verif_dispo = fields.Boolean(string=u'Vérif', help=u"Vérifier la disponibilité de l'équipe sur ce créneau", default=True)
 #    gb_employee_id = fields.Many2one('hr.employee', compute='lambda *a, **k:{}', search='search_gb_employee_id',
 #                                     string="Intervenant", of_custom_groupby=True),
@@ -135,87 +210,21 @@ class OfPlanningIntervention(models.Model):
 #    }
     _order = 'date'
 
-    @api.onchange('partner_id')
-    def _onchange_partner_id(self):
+    @api.onchange('address_id')
+    def _onchange_address_id(self):
         name = False
-        if self.partner_id:
-            name = [self.partner_id.name_get()[0][1]]
+        if self.address_id:
+            name = [self.address_id.name_get()[0][1]]
             for field in ('zip','city'):
-                if self.partner_id.get(field):
-                    name.append(self.partner_id[field])
+                val = getattr(self.address_id, field)
+                if val:
+                    name.append(val)
         self.name = name and " ".join(name) or "Intervention"
 
     @api.onchange('tache_id')
     def _onchange_tache_id(self):
         if self.tache_id and self.tache_id.duree:
             self.duree = self.tache_id.duree
-
-    @api.onchange('date', 'duree', 'hor_md', 'hor_mf', 'hor_ad', 'hor_af', 'hor_sam', 'hor_dim')
-    def _onchange_date(self):
-        if self.hor_md > 24 or self.hor_mf > 24 or self.hor_ad > 24 or self.hor_af > 24:
-            raise UserError(u"L'heure doit être inferieure ou égale à 24")
-        if self.hor_mf < self.hor_md or self.hor_ad < self.hor_mf:
-            raise UserError(u"L'heure de début ne peut pas être supérieure à l'heure de fin")
-        if self.hor_ad < self.hor_mf:
-            raise UserError(u"L'heure de l'après-midi ne peut pas être inférieure à l'heure du matin")
-
-        if not self.date:
-            return
-        if not self.duree:
-            return
-
-        # Datetime UTC
-        dt_utc = datetime.strptime(self.date, "%Y-%m-%d %H:%M:%S")
-        # Datetime local
-        dt_local = fields.Datetime.context_timestamp(self, dt_utc)
-
-        weekday = dt_local.weekday()
-        if weekday == 5 and not self.hor_sam:
-            raise UserError(u"L'équipe ne travaille pas le samedi")
-        elif weekday == 6 and not self.hor_dim:
-            raise UserError(u"L'équipe ne travaille pas le dimanche")
-
-        duree_repos = self.hor_ad - self.hor_mf
-        duree_matin = self.hor_mf - self.hor_md
-        duree_apres = self.hor_af - self.hor_ad
-        duree_jour = duree_matin + duree_apres
-
-        dt_heure = dt_local.hour + (dt_local.minute + dt_local.second / 60) / 60
-        # Déplacement de l'horaire de début au début de la journée pour faciliter le calcul
-        duree = self.duree
-        if self.hor_md <= dt_heure <= self.hor_mf:
-            duree += dt_heure - self.hor_md
-        elif self.hor_ad <= dt_heure <= self.hor_af:
-            duree += duree_matin + dt_heure - self.hor_ad
-        else:
-            # L'horaire de debut des travaux est en dehors des heures de travail
-            raise UserError(u"Il faut respecter l'horaire de travail")
-        dt_local -= timedelta(hours=dt_heure)
-
-        if not (self.hor_sam and self.hor_dim):
-            # Deplacement de l'horaire de debut au debut de la semaine pour faciliter le calcul
-            # Le debut de la semaine peut eventuellement etre un dimanche matin
-            jours_sem = (weekday + self.hor_dim) % 6
-            dt_local -= timedelta(days=jours_sem)
-            duree += jours_sem * duree_jour
-
-            # Ajout des jours de repos a la duree de la tache pour arriver la meme date de fin
-            jours = duree // duree_jour
-            duree += (self.hor_sam + self.hor_dim) * (jours // (7 - self.hor_sam - self.hor_dim))
-
-        # Calcul du nombre de jours
-        jours, duree = duree // duree_jour, duree % duree_jour
-
-        # Ajout des heures non travaillées de la derniere journée
-        duree += self.hor_md + (duree > duree_matin and duree_repos)
-
-        # Calcul de la nouvelle date
-        dt_local += timedelta(days=jours, hours=duree)
-        # Conversion en UTC
-        dt_utc = dt_local - dt_local.tzinfo._utcoffset
-        date_deadline = dt_utc.strftime("%Y-%m-%d %H:%M:%S")
-        self.date_deadline = date_deadline
-        self.date_deadline_display = date_deadline
 
     @api.onchange('equipe_id')
     def _onchange_equipe_id(self):
@@ -301,7 +310,7 @@ class OfPlanningIntervention(models.Model):
                     ('state', 'not in', ('cancel','postponed')),
                 ])
                 if rdv:
-                    raise ValidationError('Attention', u'Cette équipe a déjà %s rendez-vous sur ce créneau' % (len(rdv),))
+                    raise ValidationError(u'Cette équipe a déjà %s rendez-vous sur ce créneau' % (len(rdv),))
         return True
 
     @api.model
@@ -328,4 +337,5 @@ class OfPlanningIntervention(models.Model):
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    intervention_ids = fields.One2many('of.planning.intervention', 'partner_id', "Plannings d'intervention", copy=False)
+    intervention_partner_ids = fields.One2many('of.planning.intervention', 'partner_id', "Interventions client")
+    intervention_address_ids = fields.One2many('of.planning.intervention', 'address_id', "Interventions adresse")
