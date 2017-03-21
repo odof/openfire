b596280739ebf1f477a8fa5ac16bcd1efd294696
Gwenaël D'AUBENTON <gd@openfire.fr>
2017-03-21 17:57:05 +0100

    of_date_naissance : creation du module

---
+++ of_date_naissance/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenFire
+#
+##############################################################################
+
+{
+        "name" : "OpenFire / Module date de naissance",
+        "version" : "1.1",
+        "author" : "OpenFire",
+        "website" : "openfire.fr",
+        "category" : "OpenFire",
+        "description": """
+Module qui ajoute la date de naissance pour les partenaires
+""",
+        "depends" : [
+        ],
+        "demo_xml" : [ ],
+        "data" : [
+            'views/of_date_naissance_view.xml',
+        ],
+        "installable": True,
+        'active': False,
+}
+
