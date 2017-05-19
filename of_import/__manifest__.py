a72c5fb67838dfc08b726e02eb1b7751fcc0c761
Gwenaël D'AUBENTON <gd@openfire.fr>
2017-05-19 10:55:32 +0200

    of_import : ajout des droits pour les utilisateurs autre qu'administrateur.

--- of_import/__manifest__.py
+++ of_import/__manifest__.py
@@ -13,6 +13,7 @@ Import OpenFire
     "depends" : [ ],
     "demo_xml" : [ ],
     'data': [
+        'security/ir.model.access.csv',
         'views/of_import_view.xml',
     ],
     "installable": True,
