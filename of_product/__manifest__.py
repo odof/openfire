b40795eae1df0ece4333dd9860394929cb2d7d0d
Gael Frasson <fragae@openfire.fr>
2017-06-29 10:57:21 +0200

    of_product: creation du module

--- of_product/__manifest__.py
+++ of_product/__manifest__.py
@@ -9,14 +9,16 @@
 Module produits OpenFire.
 ======================================
 
-Ce module apporte une personnalisation des produits :
-- Mettre par défaut la référence produit (default_code) de l'article de base lors de la création d'une variante
+Ce module apporte une personnalisation des produits :\n
+- Mettre par défaut la référence produit (default_code) de l'article de base lors de la création d'une variante\n
+- Ajout des champs 'modele', 'marge', 'description_fabricant' et 'date_tarif' dans product.template\n
+- Ajout des champs 'old_code', 'pp_ht' et 'remise' dans product.supplierinfo
 """,
     'depends' : [
         'product',
     ],
     'data': [
-        'views/of_product_view.xml',
+        'views/of_product_views.xml',
     ],
     'installable': True,
 }
--- of_product/models/of_product.py
+++ of_product/models/of_product.py
@@ -6,18 +6,21 @@ import odoo.addons.decimal_precision as dp
 class OfProductTemplate(models.Model):
     _inherit = "product.template"
 
-class OfProductProduct(models.Model):
-    _inherit = "product.product"
-
     modele = fields.Char(string='Modèle')
-    marge = fields.Float(string='Marge',digits=2,compute="_compute_marge")
+    marge = fields.Float(string='Marge',digits=(4,2),compute="_compute_marge",
+                help="Marge calculée sur base du prix de vente")
+    description_fabricant = fields.Text('Description du fabricant', translate=True)
     date_tarif = fields.Date(string="Date du tarif")
-    old_code = fields.Char(string="Ancienne Référence")
 
     @api.multi
+    @api.depends('lst_price','standard_price')
     def _compute_marge(self):
+        # la marge est calculée en fonction du prix de vente, faire en fonction du prix d'achat?
         for product in self:
-            product.marge = (product.lst_price - product.standard_price) * 100 / product.lst_price
+            product.marge = (product.lst_price - product.standard_price) * 100.00 / product.lst_price
+
+class OfProductProduct(models.Model):
+    _inherit = "product.product"
 
     @api.model
     def _add_missing_default_values(self, values):
@@ -25,3 +28,18 @@ class OfProductProduct(models.Model):
         if 'product_tmpl_id' in values and values['product_tmpl_id']:
             values['default_code'] = self.env['product.template'].browse(values['product_tmpl_id']).default_code
         return super(OfProductProduct, self)._add_missing_default_values(values)
+
+class OFSuppliferInfo(models.Model):
+    _inherit = "product.supplierinfo"
+
+    old_code = fields.Char(string="Ancienne Référence")
+    pp_ht = fields.Float(string='Prix public HT', default=1.0, digits=dp.get_precision('Product Price'),
+        required=True, help="Prix Public HT conseillé par le fabricant")
+    pp_currency_id = fields.Many2one(related='currency_id')
+    remise = fields.Float(string='Remise',digits=(4,2),compute="_compute_remise")
+
+    @api.multi
+    @api.depends('pp_ht','price')
+    def _compute_remise(self):
+        for supinfo in self:
+            supinfo.remise = (supinfo.pp_ht - supinfo.price) * 100.00 / supinfo.pp_ht
