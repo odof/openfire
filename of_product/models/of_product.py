b549af9c21eef39a10ea8ae29b1961ba385b6eec
Gael Frasson <fragae@openfire.fr>
2017-06-30 17:49:08 +0200

    [FIX]of_product: correction marge et remise division par zero

--- of_product/models/of_product.py
+++ of_product/models/of_product.py
@@ -17,7 +17,11 @@ class OfProductTemplate(models.Model):
     def _compute_marge(self):
         # la marge est calculée en fonction du prix de vente, faire en fonction du prix d'achat?
         for product in self:
-            product.marge = (product.lst_price - product.standard_price) * 100.00 / product.lst_price
+            lst_price = product.lst_price
+            if lst_price != 0:
+                product.marge = (lst_price - product.standard_price) * 100.00 / lst_price
+            else: # division par 0!
+                product.marge = -100
 
 class OfProductProduct(models.Model):
     _inherit = "product.product"
@@ -42,4 +46,8 @@ class OFSuppliferInfo(models.Model):
     @api.depends('pp_ht','price')
     def _compute_remise(self):
         for supinfo in self:
-            supinfo.remise = (supinfo.pp_ht - supinfo.price) * 100.00 / supinfo.pp_ht
+            pp_ht = supinfo.pp_ht
+            if pp_ht != 0:
+                supinfo.remise = (pp_ht - supinfo.price) * 100.00 / pp_ht
+            else: # division par 0!
+                supinfo.remise = -100
