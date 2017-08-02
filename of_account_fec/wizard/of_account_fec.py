b0286552be03599447ab8066c9ef09284a40e485
Gael Frasson <fragae@openfire.fr>
2017-08-02 17:14:58 +0200

    of_account_fec: remplacement date par create_date

--- of_account_fec/wizard/of_account_fec.py
+++ of_account_fec/wizard/of_account_fec.py
@@ -242,7 +242,7 @@ class OFAccountFrFec(models.TransientModel):
             replace(aj.code, '|', '/') AS JournalCode,
             replace(aj.name, '|', '/') AS JournalLib,
             replace(am.name, '|', '/') AS EcritureNum,
-            TO_CHAR(am.date, 'YYYYMMDD') AS EcritureDate,
+            TO_CHAR(am.create_date, 'YYYYMMDD') AS EcritureDate,
             CASE WHEN aa.code LIKE '455%%' THEN '455000'
                  WHEN aa.internal_type = 'payable' THEN '401000'
                  WHEN aa.internal_type = 'receivable' THEN '411000'
@@ -289,8 +289,8 @@ class OFAccountFrFec(models.TransientModel):
             LEFT JOIN res_currency rc ON rc.id = aml.currency_id
             LEFT JOIN account_full_reconcile rec ON rec.id = aml.full_reconcile_id
         WHERE
-            am.date >= %s
-            AND am.date <= %s
+            am.create_date >= %s
+            AND am.create_date <= %s
             AND am.company_id = %s
             AND (aml.debit != 0 OR aml.credit != 0)
         '''
@@ -305,7 +305,7 @@ class OFAccountFrFec(models.TransientModel):
         AND am.journal_id IN %s
         '''
 
-        sql_sort = 'am.date, am.name, aml.id'
+        sql_sort = 'am.create_date, am.name, aml.id'
         if self.sortby == 'sort_journal_partner':
             sql_sort = 'aj.code, rp.name, aml.id'
         sql_query += '\nORDER BY ' + sql_sort
