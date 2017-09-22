f97bbcd7038f8d2c0ee2975e0e48f73dfdb4f11a
Gael Frasson <fragae@openfire.fr>
2017-09-22 14:41:49 +0200

    of_bandeau_base: migration V10

--- of_bandeau_base/static/src/js/nom_base.js
+++ of_bandeau_base/static/src/js/nom_base.js
@@ -1,37 +1,26 @@
-// Afficher de façon permanente le nom de la base dans la barre de menu
+odoo.define('of_bandeau_base.UserMenu', function (require) {
+"use strict";
 
-openerp.of_bandeau_base = function (instance) {
-   
-    instance.web.UserMenu.include({
+var UserMenu = require('web.UserMenu');
+var session = require('web.session');
 
-	do_update: function () {
-        var self = this;
-        var fct = function() {
-            var $avatar = self.$el.find('.oe_topbar_avatar');
+UserMenu.include({
+    do_update: function () {
+        var $avatar = this.$('.oe_topbar_avatar');
+        if (!session.uid) {
             $avatar.attr('src', $avatar.data('default-src'));
-            if (!self.session.uid)
-                return;
-            var func = new instance.web.Model("res.users").get_func("read");
-            return self.alive(func(self.session.uid, ["name", "company_id"])).then(function(res) {
-                var topbar_name = res.name;
-                // Inhibé par OpenFire pour afficher dans tous les cas le nom de la base
-                // if(instance.session.debug)
-                topbar_name = _.str.sprintf("%s (%s)", topbar_name, instance.session.db);
-                if(res.company_id[0] > 1)
-                    topbar_name = _.str.sprintf("%s (%s)", topbar_name, res.company_id[1]);
-                self.$el.find('.oe_topbar_name').text(topbar_name);
-                /* Inhibé par OpenFire pour afficher dans tous les cas le nom de la base
-                if (!instance.session.debug) {
-                    topbar_name = _.str.sprintf("%s (%s)", topbar_name, instance.session.db);
-                } */
-                var avatar_src = self.session.url('/web/binary/image', {model:'res.users', field: 'image_small', id: self.session.uid});
-                $avatar.attr('src', avatar_src);
-
-                openerp.web.bus.trigger('resize');  // Re-trigger the reflow logic
-            });
-        };
-        this.update_promise = this.update_promise.then(fct, fct);
+            return $.when();
+        }
+        var topbar_name = session.name;
+        // Inhibé par OpenFire pour afficher dans tous les cas le nom de la base
+        //if(session.debug) {
+        topbar_name = _.str.sprintf("%s (%s)", topbar_name, session.db);
+        //}
+        this.$('.oe_topbar_name').text(topbar_name);
+        var avatar_src = session.url('/web/image', {model:'res.users', field: 'image_small', id: session.uid});
+        $avatar.attr('src', avatar_src);
     },
+});
 
-    });
-};
+return UserMenu;
+});
--- of_bandeau_base_V9/__manifest__.py
+++ of_bandeau_base_V9/__manifest__.py
@@ -11,9 +11,9 @@ Afficher en permanence le nom de la base dans le bandeau.
     'website' : "www.openfire.fr",
     'category': "OpenFire",
     'data' : [
-        'views/of_bandeau_base_templates.xml',
+        'of_bandeau_base_view.xml',
     ],
-    'installable': True,
+    'installable': False,
     'application': False,
     'auto_install': False,
 }
