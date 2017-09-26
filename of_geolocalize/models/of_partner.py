497ffcf95b30dbc6cc9b939ca1891ac1f123ac19
Gael Frasson <fragae@openfire.fr>
2017-09-26 17:10:44 +0200

    of_geolocalize: ajout inhibition geocoding sur import via context dans write et create

--- of_geolocalize/models/of_partner.py
+++ of_geolocalize/models/of_partner.py
@@ -359,7 +359,7 @@ class ResPartner(models.Model):
 
         partner = super(ResPartner, self).create(vals)
         # inhiber sur imports et autres
-        if not self._context.get('inhiber_geocode') and \
+        if not self._context.get('from_import') and not self._context.get('inhiber_geocode') and \
           partner.env['ir.config_parameter'].get_param('Deactivate_Geocoding_On_Create') == "0":
             if (partner.street or partner.street2) and partner.zip and partner.city:
                 partner.geo_code()
@@ -374,16 +374,16 @@ class ResPartner(models.Model):
         Case geocoding didn't change and either geo_lat or geo_lng changed: the partner was localized manually.
         Case address changed: we will try to geocode the newly set address
         """
-        if not self._context.get('inhiber_geocode') and \
+        if not self._context.get('from_import') and not self._context.get('inhiber_geocode') and \
           self.env['ir.config_parameter'].get_param('Deactivate_Geocoding_On_Write') == "0":
-            if 'geocoding' not in vals and ('geo_lat' in vals or 'geo_lng' in vals) and (vals.get('geo_lat') != 0 or vals.get('geo_lng') != 0):
+            if 'geocoding' not in vals and ( ('geo_lat' in vals and vals.get('geo_lat') != 0) or ('geo_lng' in vals and vals.get('geo_lng') != 0) ):
                 vals['geocoding'] = 'manual'
 
         super(ResPartner, self).write(vals)
 
         if len(self._ids) == 1:
-            if self.geocoding != 'manual' and \
-              self.env['ir.config_parameter'].get_param('Deactivate_Geocoding_On_Write') == "0":
+            if not self._context.get('from_import') and not self._context.get('inhiber_geocode') and \
+              self.geocoding != 'manual' and self.env['ir.config_parameter'].get_param('Deactivate_Geocoding_On_Write') == "0":
                 for key in vals:
                     if key in ('street', 'street2', 'city', 'state_id', 'country_id', 'zip'):
                         if self.geocoding == 'failure' or self.geocoding == 'failure_retry':
