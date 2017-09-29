d2d020126bd3fc938b5a6d8771b40bd10c8e4028
Gael Frasson <fragae@openfire.fr>
2017-09-29 14:57:41 +0200

    of_external: ajout 3eme ligne de pied de page custom

--- of_external/models/of_company.py
+++ of_external/models/of_company.py
@@ -7,5 +7,7 @@ class ResCompany(models.Model):
     use_of_custom_footer = fields.Boolean(string='use company custom footer', help='check if you want to use this custom footer for your pdf reports')
     of_custom_footer_line_1 = fields.Char(string='line 1')
     of_custom_footer_line_2 = fields.Char(string='line 2')
+    of_custom_footer_line_3 = fields.Char(string='line 3')
     of_custom_footer_line_1_size = fields.Selection([('medium', 'Medium'), ('small', 'Small'), ('smaller', 'Smaller'), ('x-small', 'X-Small')], string='Size', default='small', required=True)
     of_custom_footer_line_2_size = fields.Selection([('medium', 'Medium'), ('small', 'Small'), ('smaller', 'Smaller'), ('x-small', 'X-Small')], string='Size', default='small', required=True)
+    of_custom_footer_line_3_size = fields.Selection([('medium', 'Medium'), ('small', 'Small'), ('smaller', 'Smaller'), ('x-small', 'X-Small')], string='Size', default='smaller', required=True)
