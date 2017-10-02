db46fd04791f9de8e8e5e3c11644a5188048f50f
Gael Frasson <fragae@openfire.fr>
2017-10-02 17:06:29 +0200

    of_utils: creation du module

---
+++ of_utils/models/of_utils.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+from openerp import api, models, fields
+
+
+class of_mois(models.Model):
+    _name = 'of.mois'
+    _description = u"Mois de l'année"
+    _order = 'id'
+
+    name = fields.Char('Mois', size=16)
+    abr = fields.Char(u'Abréviation', size=16)
+
+
+class of_jours(models.Model):
+    _name = 'of.jours'
+    _description = "Jours de la semaine"
+    _order = 'id'
+
+    name = fields.Char('Jour', size=16)
+    abr = fields.Char(u'Abréviation', size=16)
+
