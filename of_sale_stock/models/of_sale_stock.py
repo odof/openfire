4a9229c55472eb99ddb828ae971bab519ab5b720
Gael Frasson <fragae@openfire.fr>
2017-10-26 17:10:24 +0200

    of_sale_stock: ajout traductions + tansfert config.settings of_sale -> of_sale_stock (1/2)

--- of_sale_stock/models/of_sale_stock.py
+++ of_sale_stock/models/of_sale_stock.py
@@ -4,6 +4,10 @@
 from odoo import api, fields, models
 import odoo.addons.decimal_precision as dp
 
+class OFSaleStockInventory(models.Model):
+    _inherit = "stock.inventory"
+    _order = "date desc, name"
+
 class OFSaleStockInventoryLine(models.Model):
     _inherit = "stock.inventory.line"
 
@@ -18,15 +22,23 @@ class OFSaleStockInventoryLine(models.Model):
             # @TODO: gérer le multi-currency
             line.product_value = line.product_id.standard_price * line.product_qty
 
-    #@api.onchange('product_qty')
+class OFSaleStockSaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.onchange('product_uom_qty', 'product_uom', 'route_id')
+    def _onchange_product_id_check_availability(self):
+        # inhiber la vérification de stock
+        afficher_warning = self.env['ir.values'].get_default('sale.config.settings', 'of_stock_warning_setting')
+        if afficher_warning:
+            return super(OFSaleStockSaleOrderLine, self)._onchange_product_id_check_availability()
 
 class OFSaleConfiguration(models.TransientModel):
     _inherit = 'sale.config.settings'
 
-    stock_warning_setting = fields.Boolean(string="Avertissements de stock", required=True, default=False,
+    of_stock_warning_setting = fields.Boolean(string="Avertissements de stock", required=True, default=False,
             help="Afficher les messages d'avertissement de stock?")
 
     @api.multi
     def set_stock_warning_defaults(self):
         return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'stock_warning_setting', self.stock_warning_setting)
+            'sale.config.settings', 'of_stock_warning_setting', self.of_stock_warning_setting)
