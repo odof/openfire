64384168b37d68e8dec53ca635811642c906d804
Gwenaël D'AUBENTON <gd@openfire.fr>
2017-12-28 00:14:35 +0100

    of_l10n_fr_certification : [FIX] Oubli de mettre ir.model.access.csv dans __openerp__.py

--- of_l10n_fr_certification/__openerp__.py
+++ of_l10n_fr_certification/__openerp__.py
@@ -8,13 +8,14 @@
     "category" : "OpenFire",
     "description": """Module qui implémente les obligations légales en France :
 
- - Certification logiciel de caisse
+- Certification logiciel de caisse
  """,
     "depends" : ['of_account_payment_mode'],
     "init_xml" : [ ],
     "demo_xml" : [ ],
     "update_xml" : [
         "views/of_l10n_fr_certification.xml",
+        "security/ir.model.access.csv",
         ],
     "installable": True,
 }
