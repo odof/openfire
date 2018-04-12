99d463121dcde66799c96507a852efbc06920c01
Gwenaël D'AUBENTON <gd@openfire.fr>
2018-04-12 11:22:06 +0200

    of_sale_kit : suppression du module (remplace par module of_kit)

--- of_sale_kit/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import of_product
-import of_bom
-import of_sale
-import of_procurement
-import of_account
-import of_stock
--- of_sale_kit/models/of_procurement.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-class ProcurementOrder(models.Model):
-    _inherit = 'procurement.order'
-    sale_comp_id = fields.Many2one('sale.order.line.comp', string='Sale Order Line Component')
