7213c5884cf0cf3c3a1a53dda9df494971509ee6
Stan Heurtault <heusta@openfire.fr>
2018-05-23 14:21:20 +0200

    of_sale_appro : ajout bouton 'Approvisionner' et colonnes quantité stock et quantité prévisionnelle dans bon de livraison

---
+++ of_sale_appro/wizards/of_popup_message_wizard.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+class OfPopupMessageWizard(models.TransientModel):
+    """créer une boîte de dialogue pour confirmation approvisionnement"""
+
+    _name = "of.popup.message.wizard"
