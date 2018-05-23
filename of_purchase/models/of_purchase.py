79d112de9307b0b29681c17875a182174e89cd68
Juanu Sandoval <juanu.sandoval@openfire.fr>
2018-05-23 13:14:07 +0200

    of_purchase : création smart button "Achats" dans les bons de commande

--- of_purchase/models/of_purchase.py
+++ of_purchase/models/of_purchase.py
@@ -6,6 +6,20 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     delivery_expected = fields.Char(string='Livraison attendue', states={'done': [('readonly', True)]})
+    purchase_ids = fields.One2many("purchase.order", "sale_order_id", string="Achats")
+    purchase_count = fields.Integer(compute='_compute_purchase_count')
+
+    @api.depends('purchase_ids')
+    @api.multi
+    def _compute_purchase_count(self):
+        for sale_order in self:
+            sale_order.purchase_count = len(sale_order.purchase_ids)
+
+    @api.multi
+    def action_view_achats(self):
+        action = self.env.ref('of_purchase.of_purchase_open_achats').read()[0]
+        action['domain'] = [('sale_order_id', 'in', self._ids)]
+        return action
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
