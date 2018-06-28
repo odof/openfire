218db55608611e05b8491b3b088705daacc264b0
Juanu Sandoval <juanu.sandoval@openfire.fr>
2018-06-28 09:08:24 +0200

    FIRST COMMIT

--- of_planning_tournee/wizard/__init__.py
+++ of_planning_tournee/wizard/__init__.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 from . import planification
+from . import planification_2
 from . import rdv
+from . import rdv_2
 #import add_partner
 #import search_partner
 #import impression
