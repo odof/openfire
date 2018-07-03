e4983a209ac122819681c8dcba7fc71e4ffebeac
Gwenaël D'AUBENTON <gd@openfire.fr>
2018-07-03 12:19:57 +0200

    of_l10n_eu : création du module. Mise en place exigence RGPD sur accord recevoir communication.

---
+++ of_l10n_eu/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'OpenFire / Implémentation obligations légales en Europe',
+    'author': 'OpenFire',
+    'version': '9.0.1.0.0',
+    'category': 'OpenFire modules',
+    'summary': 'Obligations légales européennes',
+    'license': '',
+    'description': """
+Module qui implèmente les obligations légales en Europe :
+
+- Partenaires : ajout des champs d'autorisation d'utilisation des données personnelles (RGPD)
+    """,
+    'website': 'openfire.fr',
+    'depends': [
+        'mail',
+        ],
+    'data': [
+        'views/rgpd_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ of_l10n_eu/models/rgpd.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    # Champs pour autorisation recevoir communication (exigence RGPD)
+    of_accord_communication = fields.Boolean(string=u"Autorise recevoir communication")
+    of_date_accord_communication = fields.Date(string=u"Date de l'autorisation")
+    of_preuve_accord_communication = fields.Text(string=u"Preuve autorisation")
+    # On redéfinit le champ opt_out pour le mettre en champ calculé (inverse de of_accord_communication
+    opt_out = fields.Boolean('Opt-Out', compute='_compute_opt_out',
+        help="If opt-out is checked, this contact has refused to receive emails for mass mailing and marketing campaign. "
+        "Filter 'Available for Mass Mailing' allows users to filter the partners when performing mass mailing.")
+
+    @api.multi
+    def _compute_opt_out(self):
+        for partner in self:
+            partner.opt_out = not partner.of_accord_communication
