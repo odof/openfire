25aae2e1887539aae3dd853c1335a2d1f6292d92
Stan Heurtault <heusta@openfire.fr>
2018-10-02 11:45:07 +0200

    of_sale_stock : ajout du champ route dans les devis/commandes pour choisir la route de toutes les lignes de commande en une sélection

--- of_sale_stock/__manifest__.py
+++ of_sale_stock/__manifest__.py
@@ -10,15 +10,15 @@
 Extension OpenFire du module sale_stock
 =======================================
 
-- Ajout des champ 'product_value_unit' et 'product_value' dans 'stock.inventory.line' pour le groupe Comptabilité - Gestionnaire
-- Ajout du rapport "Inventaire valorisé" dans 'stock.inventory.line' pour le groupe Comptabilité - Gestionnaire
+- Ajout des champ "product_value_unit" et "product_value" dans "stock.inventory.line" pour le groupe Comptabilité - Gestionnaire
+- Ajout du rapport "Inventaire valorisé" dans "stock.inventory.line" pour le groupe Comptabilité - Gestionnaire
 - Ajout d'un filtre avancé dans les devis/bons de commandes sur la date et la date de transfert du bon de livraison
 - Ajout d'une option pour afficher l'article et sa description dans le bon de livraison et les documents imprimables associés
 - Ajout d'un rapport de gestion des stocks permettant de générer des fichiers Excel comprenant une liste d'article en fonction d'un ou plusieurs emplacements de stock et d'une date
+- Ajout d'un champ "Route" sur les devis/bon de commandes pour remplir le champ "Route" des lignes de commandes (n'apparait que si l'option d'afficher les routes par ligne est cochée)
 
-
-Paramètres de ventes (sale.config.settings)
--------------------------------------------
+Paramètres de ventes
+--------------------
 
 - Description paramètre (module(s) concerné(s))
 - Inhibition avertissements de stock (of_sale_kit)
