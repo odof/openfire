38f6f99c115a32c71e7dcc6b1350bd9fd51dae48
Gwenaël D'AUBENTON <gd@openfire.fr>
2018-12-18 15:59:20 +0100

    of_date_naissance : création du module (migration depuis Openfire Odoo 9)

---
+++ of_date_naissance/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenFire
+#
+##############################################################################
+
+{
+        "name" : "OpenFire / Date de naissance",
+        "version" : "10.0.1.1",
+        "author" : "OpenFire",
+        "website" : "openfire.fr",
+        "category" : "OpenFire",
+        "description": """
+Module qui ajoute la date de naissance pour les partenaires
+""",
+        "depends" : [
+        ],
+        "demo_xml" : [ ],
+        "data" : [
+            'views/of_date_naissance_view.xml',
+        ],
+        "installable": True,
+        'active': False,
+}
---
+++ of_date_naissance/models/of_date_naissance.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class project_issue(models.Model):
+    _inherit = "res.partner"
+
+    of_date_naissance = fields.Date(u'Date de naissance')
+    of_date_naissance_char = fields.Char(u'Date anniversaire (MM-JJ)', compute='return')
