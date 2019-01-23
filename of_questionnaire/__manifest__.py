c9db914990364c99e6d382dde8a95bdf4309d918
Stan Heurtault <heusta@openfire.fr>
2019-01-23 15:45:08 +0100

    of_planning : Ajout des raisons et notes de fin d'intervention               Modification des onglets de l'intervention

--- of_questionnaire/__manifest__.py
+++ of_questionnaire/__manifest__.py
@@ -30,7 +30,7 @@ Fonctionnalités
 """,
     'depends' : [
         'of_planning',
-        'of_parc_installe',
+        'of_service_parc_installe',
         ],
     'data' : [
         'views/of_questionnaire_views.xml',
