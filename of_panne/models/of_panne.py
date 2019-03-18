e5ccc4333fa685ac3e5b56acea564dfd25d3c55c
Stan Heurtault <heusta@openfire.fr>
2019-03-18 15:42:30 +0100

    of_panne : WIP

--- of_panne/models/of_panne.py
+++ of_panne/models/of_panne.py
@@ -6,14 +6,26 @@ class OfPanne(models.Model):
     _name = "of.panne"
 
     name = fields.Char(string="Nom", required=True)
+    active = fields.Boolean(strin="Actif", default=True)
     partner_id = fields.Many2one('res.partner', string="Client", required=True)
     parc_installe_id = fields.Many2one('of.parc.installe', string=u"Parc installé", required=True)
     tache_id = fields.Many2one('of.planning.tache', string=u"Tâche", required=True)
     intervention_model_id = fields.Many2one('of.planning.intervention.model', string=u"Modèle d'intervention")
-    equipe_id = fields.Many2one('of.planning.equipe', string=u"Équipe")
+    intervention_id = fields.Many2one('of.planning.intervention', string="Intervention", copy=False)
+    equipe_id = fields.Many2one('of.planning.equipe', string=u"Équipe", copy=False)
     notes = fields.Text(string="Notes")
     notes_client = fields.Text(related='partner_id.comment', string="Notes client", readonly=True)
-    state = fields.Selection([('todo', u'À prendre en charge'), ('doing', 'Prise en charge'), ('done', u'Terminée')], string=u"État", default="todo", required=True)
+    state = fields.Selection([('todo', u'À prendre en charge'), ('doing', 'Prise en charge'), ('done', u'Terminée')], string=u"État", compute="_compute_state", store=True)
+
+    @api.depends('intervention_id', 'intervention_id.state')
+    def _compute_state(self):
+        for panne in self:
+            if not panne.intervention_id:
+                panne.state = 'todo'
+            elif panne.intervention_id.state in ['done', 'cancel']:
+                panne.state = 'done'
+            else:
+                panne.state = 'doing'
 
     @api.onchange('parc_installe_id')
     def onchange_parc_installe(self):
@@ -22,4 +34,19 @@ class OfPanne(models.Model):
     @api.onchange('intervention_model_id')
     def onchange_intervention_model(self):
         self.tache_id = self.intervention_model_id.tache_id
+
+    def toggle_active(self):
+        self.active = not self.active
+
+    def action_view_interventions(self):
+        if not self.intervention_id:
+            return
+        action = self.env.ref('of_planning.of_sale_order_open_interventions').read()[0]
+        action['domain'] = [('id', '=', self.intervention_id.id)]
+        return action
+
+class OfPlanningIntervention(models.Model):
+    _inherit = "of.planning.intervention"
+
+    panne_id = fields.Many2one('of.panne', string=u"Panne liée")
 
--- of_panne/wizard/of_panne_wizard.py
+++ of_panne/wizard/of_panne_wizard.py
@@ -8,25 +8,28 @@ class OfPanneWizard(models.TransientModel):
     equipe_id = fields.Many2one('of.planning.equipe', string=u"Équipe", required=True)
 
     def select_team(self):
-        test = self._intervention_fields(self.env['of.panne'].browse(self._context.get('active_ids')), self.equipe_id)
-        self.env['of.panne'].browse(self._context.get('active_ids')).write({'equipe_id': self.equipe_id.id, 'state': 'doing'})
+        vals = self._intervention_fields(self.env['of.panne'].browse(self._context.get('active_ids')), self.equipe_id)
+        intervention = self.env['of.planning.intervention'].create(vals)
+        self.env['of.panne'].browse(self._context.get('active_ids')).write({'equipe_id': self.equipe_id.id, 'state': 'doing', 'intervention_id': intervention.id})
 
     def _intervention_fields(self, panne, equipe):
-        return {'address_id' : panne.partner_id,
-                'tache_id' : panne.tache_id,  # Not null
-                'user_id' : self.create_uid,
-                'company_id' : self.create_uid.company_id,
+        return {'address_id' : panne.partner_id.id,
+                'tache_id' : panne.tache_id.id,  # Not null
+                'user_id' : self.create_uid.id,
+                'company_id' : self.create_uid.company_id.id,
                 'verif_dispo' : False,
-                'equipe_id' : equipe,  # Not null
+                'equipe_id' : equipe.id,  # Not null
                 'duree' : panne.tache_id.duree,  # Not null
                 'hor_md' : equipe.hor_md,  # Not null
                 'hor_mf' : equipe.hor_mf,  # Not null
                 'hor_ad' : equipe.hor_ad,  # Not null
                 'hor_af' : equipe.hor_af,  # Not null
                 'description' : panne.notes,
-                'date' : fields.Datetime.to_string(fields.Datetime.now()),  # Not null
+                'date' : fields.Datetime.now(),  # Not null
                 'name' : panne.name,  # Not null
-                'model_id' : panne.model_id,
-                'parc_installe_id' : panne.parc_installe_id,
+                'model_id' : panne.intervention_model_id.id,
+                'parc_installe_id' : panne.parc_installe_id.id,
+                'state': 'confirm',
+                'panne_id': panne.id,
             }
 
