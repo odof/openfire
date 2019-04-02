ae6e97c31178bb9b5d46041ed9c4778220ffeb18
Gwenaël D'AUBENTON <gd@openfire.fr>
2019-04-02 17:38:48 +0200

    of_sms : création du module
    Co-auteur (proposition) : Gael Frasson <fragae@openfire.fr>

---
+++ of_sms/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : "OpenFire / SMS",
+    'version' : "10.0.1.0.0",
+    'author' : "OpenFire",
+    'license': 'LGPL-3',
+    'website' : "www.openfire.fr",
+    'category' : "Generic Modules/",
+    'description': u"""
+Module de SMS OpenFire
+======================
+
+- Gestion de la passerelle OVH
+- Envoi de SMS depuis les partenaires, les prospects/opportunités, le planning d'intervention instantanément ou en différé
+- Modèles de SMS
+- Tâche automatique journalière envoyant des textos de rappel la veille aux équipes d'intervention et/ou aux clients
+""",
+    'depends' : [
+        'mail',
+        'of_planning_tournee',
+        'of_crm',
+    ],
+    'demo_xml' : [],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/of_sms_data.xml',
+        'views/of_sms_views.xml'
+    ],
+    'installable': True,
+}
