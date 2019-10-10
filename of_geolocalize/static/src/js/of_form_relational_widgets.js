795fca5b942236957743ab983034e391476afe2e
Gael Frasson <fragae@openfire.fr>
2019-10-10 12:07:16 +0200

    of_geolocalize: correctif widget qui indique absence de geoloc (generait erreur si pas de partenaire selectionne)

--- of_geolocalize/static/src/js/of_form_relational_widgets.js
+++ of_geolocalize/static/src/js/of_form_relational_widgets.js
@@ -29,47 +29,38 @@ FieldMany2One.include({
         this._super.apply(this, arguments);
 
         if (this.field.relation == 'res.partner') {
-            console.log("HAHAHAHAHAHAHAHAHAHAHA",this);
             this.geo_lat_field = (this.options && this.options.geo_lat_field) || this.geo_lat_field || 'geo_lat';
             if (!isNullOrUndef(this.field_manager.fields[this.geo_lat_field])) {
                 this.set({"geo_lat": this.field_manager.get_field_value(this.geo_lat_field)});
             }else{
                 this.set_geo_lat();
             }
-            //this.on("change:geo_lat", this, this.render_value);
         }
     },
     set_geo_lat: function () {
         var self = this;
 
-        var ResPartner = new Model("res.partner");
         var partner_id = this.field_manager.get_field_value(this.name)
-        ResPartner.query(['id', 'geo_lat', 'name']) // retrieve geo_lat from db
-            .filter([['id','=',partner_id]]) // id
-            .all()
-            .then(function (partners){
-                //console.log("partnerrrr",partners)
-                self.set({"geo_lat": partners[0]["geo_lat"]});
-                //console.log("HOHO",self);
-                // on peut vérifier la géo_loc maintenant
-                self.dfd_geo_lat.resolve();
-                //console.log("DFD RESOLVED " + self.name + " " + partners[0]["geo_lat"])
-                return partners[0]["geo_lat"]
-            })
-    },
-    start: function() {
-        this._super.apply(this, arguments);
-        if (this.field.relation == 'res.partner') {
-            // trigger a render_value when this field change
-            //this.field_manager.on("field_changed:" + this.name, this, function() {
-                //this.set({"geo_lat": this.field_manager.get_field_value(this.geo_lat_field)});
-                //this.render_value();
-            //});
+        if (!partner_id) {
+            self.set({"geo_lat": undefined});
+            self.dfd_geo_lat.resolve();
+        }else{
+            var ResPartner = new Model("res.partner");
+            ResPartner.query(['id', 'geo_lat', 'name']) // retrieve geo_lat from db
+                .filter([['id','=',partner_id]]) // id
+                .all()
+                .then(function (partners){
+                    self.set({"geo_lat": partners[0]["geo_lat"]});
+                    // on peut vérifier la géo_loc maintenant
+                    self.dfd_geo_lat.resolve();
+                    return partners[0]["geo_lat"]
+                })
         }
+
     },
     check_localized: function() {
         var partner_lat = this.get('geo_lat');
-        if (!partner_lat) {
+        if (partner_lat === 0) {
             return false;
         }
         return true;
@@ -83,10 +74,8 @@ FieldMany2One.include({
             // on attend d'avoir set la valeur de geo_lat avant de la verifier :D
             $.when(this.dfd_geo_lat).then(function(){
                 self.$label.next('.o_tz_warning').remove();
-                //console.log("RENDERED! " + self.name);
                 if(!self.check_localized()){
                     // n'est pas géolocalisé
-                    //console.log("not localized ", self);
                     var options = _.extend({
                         delay: { show: 501, hide: 0 },
                         title: _t((self.current_display || self.field_manager.datarecord[self.name][1] || "Ce partenaire ") + " n'est pas géolocalisé"),
