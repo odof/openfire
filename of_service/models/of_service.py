f409371b6326eb7adf7ac94cca87f09849348873
Gael Frasson <fragae@openfire.fr>
2019-10-30 16:30:05 +0100

    of_service: ajout quinzaines dans date.range avec auto-generation

--- of_service/models/of_service.py
+++ of_service/models/of_service.py
@@ -4,6 +4,11 @@ from odoo import api, models, fields
 from datetime import date
 from datetime import timedelta
 from dateutil.relativedelta import relativedelta
+from dateutil.rrule import (rrule,
+                            YEARLY,
+                            MONTHLY,
+                            WEEKLY,
+                            DAILY)
 from odoo.tools.safe_eval import safe_eval
 from odoo.exceptions import UserError
 
@@ -392,7 +397,7 @@ class OfService(models.Model):
             res_mois_int = min(mois_ints, key=lambda m: (m < date_next_mois_int, m))
             res_year_int = date_next_da.year + (res_mois_int < date_next_mois_int)
 
-            return fields.Date.to_string(date(res_year_int, res_mois_int, 1))
+            return fields.Date.to_string(date(res_year_int, 1, res_mois_int))
         else:
             return False
 
@@ -714,3 +719,52 @@ class ResPartner(models.Model):
             'hide_bouton_planif': True,
         }
         return action
+
+class DateRangeGenerator(models.TransientModel):
+    _inherit = 'date.range.generator'
+
+    @api.model
+    def auto_genere_quinzaines(self, date_start_str=False):
+        dr_type_obj = self.env['date.range.type']
+        type = dr_type_obj.search([('name', '=', 'Quinzaine civile')], limit=1)
+        if not type:
+            type = dr_type_obj.create({'name': 'Quinzaine civile', 'company_id': False, 'allow_overlap': False})
+        if not date_start_str:
+            dr = self.env['date.range'].search([('type_id', '=', type.id)], order="date_start DESC", limit=1)
+            if not dr:
+                year_int = fields.Date.from_string(fields.Date.today()).year
+                date_start_str = fields.Date.to_string(date(year_int, 1, 1))
+            else:
+                # le lendemain de la dernière période en date
+                date_start_da = fields.Date.from_string(dr.date_end) + timedelta(days=1)
+                date_start_str = fields.Date.to_string(date_start_da)
+
+        date_start_dt = fields.Datetime.from_string(date_start_str)
+        year_int = date_start_dt.year
+        year_str = str(year_int % 100)
+        name_prefix = "Q-%s-" % year_str
+        # replacer la date de début sur un lundi pour caler les quinzaine sur des semaines
+        date_start_dt -= timedelta(days=date_start_dt.weekday())
+
+        vals = {
+            'name_prefix': name_prefix,
+            'date_start': fields.Date.to_string(date_start_dt),
+            'type_id': type.id,
+            'company_id': False,
+            'unit_of_time': WEEKLY,
+            'duration_count': 2,
+            'count': 27,
+        }
+        dr_gen = self.create(vals)
+        date_ranges = dr_gen._compute_date_ranges()
+        if date_ranges:
+            # replacer la date de début de la première période sur le premier de l'an pour eviter les chevauchements de périodes
+            dr_first_debut_da = date(year_int, 1, 1)
+            date_ranges[0]['date_start'] = fields.Date.to_string(dr_first_debut_da)
+            # replacer la date de fin de la dernière période sur le 31 décembre pour eviter les chevauchements de périodes
+            dr_last_fin_da = date(year_int, 12, 31)
+            date_ranges[-1]['date_end'] = fields.Date.to_string(dr_last_fin_da)
+            for dr in date_ranges:
+                self.env['date.range'].create(dr)
+        return self.env['ir.actions.act_window'].for_xml_id(
+            module='date_range', xml_id='date_range_action')
