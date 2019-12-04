6f167f092eed8908583ac27574c1a2390ff5230f
Stan Heurtault <heusta@openfire.fr>
2019-12-04 09:30:30 +0100

    of_analytique (création du module) :  - Ajout d'une option dans la configuration des ventes pour forcer l'utilisation des comptes analytiques  - Ajouter le compte analytique sur la facture et cacher le compte sur les lignes de facture

---
+++ of_analytique/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "OpenFire / Analytique",
+    "version": "10.0.1.0.0",
+    "author": "OpenFire",
+    'license': '',
+    "category": "OpenFire",
+    "description": """
+Compte Analytique OpenFire
+==========================
+
+Configuration des ventes:
+-------------------------
+ - Forcer l'utilisation des comptes analytiques.
+
+Factures:
+---------
+ - Ajouter le compte analytique sur la facture et cacher le compte sur les lignes de factures.
+
+""",
+    "website": "www.openfire.fr",
+    "depends": [
+        "of_sale",
+        "analytic",
+        "base",
+    ],
+    "data": [
+        "views/of_analytique_views.xml",
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ of_analytique/models/of_analytique.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals['of_project_id'] = self.project_id and self.project_id.id or False
+        return invoice_vals
+
+
+class OFSaleConfiguration(models.TransientModel):
+    _inherit = 'sale.config.settings'
+
+    of_compte_analytique = fields.Boolean(string="(OF) Analytique")
+
+    @api.multi
+    def set_of_compte_analytique_setting(self):
+        view = self.env.ref('of_analytique.of_analytique_sale_order')
+        if view:
+            self.env.ref('of_analytique.of_analytique_sale_order').write({'active': self.of_compte_analytique})
+        return self.env['ir.values'].sudo().set_default(
+            'sale.config.settings', 'of_compte_analytique',
+            self.of_compte_analytique)
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    of_project_id = fields.Many2one('account.analytic.account', string='Compte analytique')
