6030c5171350cccfc49df8cb9b8b6c17744cc43c
Gwenaël D'AUBENTON <gd@openfire.fr>
2020-01-30 17:01:02 +0100

    of_external : permettre de réaliser les en-têtes société sous forme d'image

--- of_external/models/of_company.py
+++ of_external/models/of_company.py
@@ -17,14 +17,21 @@ class ResCompany(models.Model):
     of_custom_header_line_1 = fields.Char(string='Ligne 1')
     of_custom_header_line_2 = fields.Char(string='Ligne 2')
     of_custom_header_line_3 = fields.Char(string='Ligne 3')
+    of_custom_header_line_4 = fields.Char(string='Ligne 4')
     of_custom_header_line_1_size = fields.Selection([('medium', 'Moyen'), ('small', 'Petit'), ('smaller', 'Plus petit'), ('x-small', u'Très petit')], string='Taille', default='medium', required=True)
     of_custom_header_line_2_size = fields.Selection([('medium', 'Moyen'), ('small', 'Petit'), ('smaller', 'Plus petit'), ('x-small', u'Très petit')], string='Taille', default='medium', required=True)
     of_custom_header_line_3_size = fields.Selection([('medium', 'Moyen'), ('small', 'Petit'), ('smaller', 'Plus petit'), ('x-small', u'Très petit')], string='Taille', default='medium', required=True)
+    of_custom_header_line_4_size = fields.Selection([('medium', 'Moyen'), ('small', 'Petit'), ('smaller', 'Plus petit'), ('x-small', u'Très petit')], string='Taille', default='medium', required=True)
+
+    of_max_height_bandeau = fields.Integer(string=u'Hauteur max bandeau (px)', default=130, required=True)
+
     of_position_header_lines = fields.Selection(
         [
-            ('logo_under', u"Sous le logo"),
-            ('logo_right', u"À droite du logo"),
-        ], string="Position", default="logo_right",
+            ('logo_under', u"Logo société et adresse configurable dessous"),
+            ('logo_right', u"Logo société et adresse configurable à droite"),
+            ('bandeau_pastille', u"Bandeau image + pastille"),
+            ('bandeau_totalite', u"Bandeau image totalité page "),
+        ], string="Type d'en-tête société", default="logo_right",
         help=u"Position des lignes d'en-tête relativement au logo de société\n"
              u"Sous le logo : les lignes d'en-tête seront placées sous le logo de société.\n"
              u"À droite du logo : les lignes d'en-tête seront placées à droite du logo.")
@@ -39,6 +46,8 @@ class ResCompany(models.Model):
                 field_to_render = self.of_custom_header_line_2
             elif number == 3:
                 field_to_render = self.of_custom_header_line_3
+            elif number == 4:
+                field_to_render = self.of_custom_header_line_4
         else:
             if number == 1:
                 field_to_render = self.of_custom_footer_line_1
