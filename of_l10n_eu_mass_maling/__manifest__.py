68e78a76c2edc85bcbae17e665aeb15ecd7d6c0d
Stan Heurtault <heusta@openfire.fr>
2020-04-22 16:13:02 +0200

    of_l10n_eu : Champs RGPD séparés dans un AbstractModel et faire dépendre 'res.partner' de ce modèle.
    of_l10n_eu_mass_mailing : Adaptation de la fonction de désabonnement du module publipostage.

---
+++ of_l10n_eu_mass_maling/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'OpenFire / Implémentation obligations légales en Europe pour module publipostage',
+    'author': 'OpenFire',
+    'version': '10.0.1.0.0',
+    'category': 'OpenFire modules',
+    'summary': 'Obligations légales européennes',
+    'license': '',
+    'description': """
+Module qui implèmente les obligations légales en Europe :
+
+- Modification de la fonction de désinscription aux mails commerciaux du module de publipostage
+    """,
+    'website': 'openfire.fr',
+    'depends': [
+        'mass_mailing',
+        'of_l10n_eu',
+        ],
+    'data': [],
+    'installable': True,
+    'auto_install': True,
+}
