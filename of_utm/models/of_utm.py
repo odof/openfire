6a39a8db0d53ae0864afe9bbe704b3d8f0fa0436
Paul LE COZ <plc@openfire.fr>
2020-04-23 17:04:17 +0200

    of_utm : T1013 - Harmonisation du référentiel marketing et ajout du champ "Actif" sur les modèles

--- of_utm/models/of_utm.py
+++ of_utm/models/of_utm.py
@@ -2,19 +2,29 @@
 
 from odoo import api, fields, models
 
-class OFUtmMedium(models.Model):
+
+class UtmCampaign(models.Model):
+    _inherit = 'utm.campaign'
+
+    active = fields.Boolean(string="Actif", default=True)
+
+
+class UtmMedium(models.Model):
     _inherit = 'utm.medium'
 
     source_ids = fields.One2many('utm.source', 'medium_id', string="Origines disponibles")
 
-class OFUtmSource(models.Model):
+
+class UtmSource(models.Model):
     _inherit = 'utm.source'
     _order = 'sequence'
 
+    active = fields.Boolean(string="Actif", default=True)
     sequence = fields.Integer(string='Sequence', default=10)
     medium_id = fields.Many2one('utm.medium', string='Canal associé')
 
-class OFUtmMixin(models.AbstractModel):
+
+class UtmMixin(models.AbstractModel):
     _inherit = 'utm.mixin'
 
     source_id = fields.Many2one(domain="[('medium_id', '=', medium_id)]")
