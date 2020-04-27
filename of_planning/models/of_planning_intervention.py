bb0c6b6fe56f6f1b37d91ae6ba3053480527e9bb
Gael Frasson <fragae@openfire.fr>
2020-04-27 17:00:27 +0200

    T1042 - of_calendar: ajout de la possibilité de choisir la façon de coloriser les évènements de la vue calendar     of_planning: ajout de couleurs dans les tâches et de la possibilité de coloriser en fonction des tâches dans le calendrier des RDVs Tech     of_planning_view: ajout de la possibilité de coloriser en fonction des tâches dans la vue planning

--- of_planning/models/of_planning_intervention.py
+++ of_planning/models/of_planning_intervention.py
@@ -149,6 +149,8 @@ Si cette option n'est pas cochée, seule la tâche la plus souvent effectuée da
         'hr.employee', string=u"Employés qualifiés", compute="_compute_employee_ids", search="_search_employee_ids")
     category_id = fields.Many2one('hr.employee.category', string=u"Catégorie d'employés")
     fourchette_planif = fields.Selection(related="tache_categ_id.fourchette_planif", readonly=True)
+    color_ft = fields.Char(string="Couleur de texte", help="Choisissez votre couleur", default="#0D0D0D")
+    color_bg = fields.Char(string="Couleur de fond", help="Choisissez votre couleur", default="#F0F0F0")
 
     @api.multi
     def _compute_employee_ids(self):
@@ -246,6 +248,20 @@ Si cette option n'est pas cochée, seule la tâche la plus souvent effectuée da
                    'product_id'          : product.id,
                    'invoice_line_tax_ids': [(6, 0, taxes._ids)],
                    }, ""
+
+    @api.model
+    def get_caption_data(self):
+        data = {}
+        taches = self.search([])
+        for tache in taches:
+            data[tache.id] = {
+                'value': tache.id,
+                'label': tache.name,
+                'color_bg': tache.color_bg,
+                'color_ft': tache.color_ft,
+            }
+        return data
+
 
 class OfPlanningEquipe(models.Model):
     _name = "of.planning.equipe"
@@ -1278,6 +1294,31 @@ class OfPlanningIntervention(models.Model):
 
         return (invoice_data,
                 msg_succes % (self.name,))
+
+    @api.model
+    def get_color_filter_data(self):
+        res = {}
+        default_filter = self.env['ir.values'].get_default('of.intervention.settings',
+                                                           'planningview_color_filter',
+                                                           for_all_users=False)
+        res['intervenant'] = {
+            'is_checked': not default_filter or default_filter == u'intervenant',
+            'label': u'Intervenant',
+            'field': u'employee_ids',
+            'color_ft_field': u'of_color_ft',
+            'color_bg_field': u'of_color_bg',
+            'catpions': False,
+        }
+        res['tache'] = {
+            'is_checked': default_filter == u'tache',
+            'label': u'Tâche',
+            'field': u'tache_id',
+            'color_ft_field': u'color_ft',
+            'color_bg_field': u'color_bg',
+            'captions': self.env['of.planning.tache'].get_caption_data(),
+        }
+
+        return res
 
 
 class OfPlanningInterventionLine(models.Model):
