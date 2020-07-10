0f932a54d6ab946624c0e89367dc93e57bea7f60
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2020-07-10 19:55:46 +0200

    of_tiers : Creation des comptes à l'utilisation. Les comptes de tiers ne sont plus générés à la création/modification des partenaires, mais lorsque nécessaires dans les factures, paiements et pièces comptable. Le libellé des comptes de tiers n'est plus mis à jour une fois le compte généré.

--- of_tiers/__manifest__.py
+++ of_tiers/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name' : 'OpenFire / Comptes de tiers',
+    'name': 'OpenFire / Comptes de tiers',
     'author': 'OpenFire',
-    'version' : '10.0.1.0.0',
+    'version': '10.0.1.0.0',
     'summary': 'Utilisation de comptes de tiers pour les clients et fournisseurs',
     'description': """
 OpenFire / Comptes de tiers
@@ -11,7 +11,7 @@ OpenFire / Comptes de tiers
 Utilisation de comptes de tiers
 """,
     'category': 'Accounting',
-    'depends' : ['account'],
+    'depends': ['of_account'],
     'data': [
         'views/of_tiers_view.xml',
         'security/ir.model.access.csv',
