f68a6260b22b9364101e832cf8861066f01de1eb
Gael Frasson <fragae@openfire.fr>
2020-07-22 09:53:04 +0200

    of_planning: [FIX] apprivoisement d'un scarabée qui empêchait l'ajout récent du champ 'Est commercial' dans les employés d'être bien pris en compte

--- of_planning_view/models/of_planning_view.py
+++ of_planning_view/models/of_planning_view.py
@@ -399,8 +399,9 @@ class OFInterventionConfiguration(models.TransientModel):
     _inherit = 'of.intervention.settings'
 
     planningview_employee_exclu_ids = fields.Many2many(
-        'hr.employee', string=u"(OF) Exculsion d'intervenants", help=u"Intervenants à NE PAS montrer en vue planning",
-        domain=[('of_est_intervenant', '=', True)])
+        'hr.employee', string=u"(OF) Exculsion d'intervenants", help=u"Employés à NE PAS montrer en vue planning",
+        domain=['|', ('of_est_intervenant', '=', True), ('of_est_commercial', '=', True)]
+    )
 
     @api.multi
     def set_planningview_employee_exclu_ids_defaults(self):
