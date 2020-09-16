1516500e022699b61e7a8999c1cc9445629f2edc
Paul LE COZ <plc@openfire.fr>
2020-09-16 09:06:38 +0200

    of_account_tax : Modification de la méthode map_tax pour compatibilité avec le e-commerce

--- of_account_tax/models/of_account_tax.py
+++ of_account_tax/models/of_account_tax.py
@@ -42,10 +42,11 @@ class AccountFiscalPosition(models.Model):
 
     @api.multi
     def map_tax(self, taxes, product=None, partner=None):
-        if not self:
-            raise UserError(u"Veuillez renseigner une position fiscale")
-        self.ensure_one()
-        if not taxes:
+        if not self._context.get('website_id', False):
+            if not self:
+                raise UserError(u"Veuillez renseigner une position fiscale")
+            self.ensure_one()
+        if len(self) == 1 and not taxes:
             taxes = self.default_tax_ids
         return super(AccountFiscalPosition, self).map_tax(taxes, product=product, partner=partner)
 
