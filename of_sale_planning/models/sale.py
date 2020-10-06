9eb243f62f11e0bbe6e90918d7ed17feedff95c7
Stan Heurtault <heusta@openfire.fr>
2020-10-06 10:22:06 +0200

    of_sale_planning :  - Changement du calcul de durée prévisonnelle sur ligne de commande pour prendre en compte les composants de kits.  - Charger la durée prévisionnelle d'un article de modèle de devis au moment de la sélection du modèle dans le devis.
    modifications

--- of_sale_planning/models/sale.py
+++ of_sale_planning/models/sale.py
@@ -20,22 +20,47 @@ class SaleOrderLine(models.Model):
     of_duration = fields.Float(string=u"Durée de pose prévisionnelle")
 
     @api.multi
-    @api.onchange('product_id')
-    def product_id_change(self):
-        res = super(SaleOrderLine, self).product_id_change()
-        if self.product_id:
+    def calculate_duration(self):
+        if self.of_is_kit and self.kit_id:
+            kit_lines = self.kit_id.kit_line_ids
+            total_duration = 0.0
+            for kit_line in kit_lines:
+                original_uom = kit_line.product_id.uom_id
+                current_uom = kit_line.product_uom_id
+                qty = kit_line.qty_per_kit
+                product_duration = kit_line.product_id.of_duration_per_unit
+                total_duration += (product_duration * current_uom._compute_quantity(qty, original_uom, round=False))
+            self.of_duration = total_duration
+        elif self.product_id:
             original_uom = self.product_id.uom_id
             current_uom = self.product_uom
             self.of_duration = self.product_id.of_duration_per_unit * \
                 current_uom._compute_quantity(self.product_uom_qty, original_uom, round=False)
 
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super(SaleOrderLine, self).product_id_change()
+        self.calculate_duration()
         return res
 
     @api.onchange('product_uom', 'product_uom_qty')
     def product_uom_change(self):
         super(SaleOrderLine, self).product_uom_change()
-        if self.product_id:
-            original_uom = self.product_id.uom_id
-            current_uom = self.product_uom
-            self.of_duration = self.product_id.of_duration_per_unit * \
-                current_uom._compute_quantity(self.product_uom_qty, original_uom, round=False)
+        self.calculate_duration()
+
+    @api.onchange('of_is_kit')
+    def _onchange_of_is_kit(self):
+        super(SaleOrderLine, self)._onchange_of_is_kit()
+        self.calculate_duration()
+
+    @api.onchange('kit_id')
+    def _onchange_kit_id(self):
+        super(SaleOrderLine, self)._onchange_of_is_kit()
+        self.calculate_duration()
+
+    @api.model
+    def _new_line_for_template(self, data):
+        new_line = super(SaleOrderLine, self)._new_line_for_template(data)
+        new_line.calculate_duration()
+        return new_line
