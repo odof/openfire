43af4026fab5b6768216ee8b50e685098b2fbc5c
Gaël FRASSON <fragae@openfire.fr>
2020-11-17 14:24:36 +0100

    of_label: initialisation du module: ajout code SerpentCS pour sauvegarde

---
+++ of_label/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+{
+    "name": "Mass Label Reporting",
+    "version": "10.0.1.0.0",
+    "author": 'Serpent Consulting Services Pvt. Ltd.',
+    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
+    "category": "Tools",
+    "license": "AGPL-3",
+    "website": "http://www.serpentcs.com",
+    "summary": "Generate customised labels of any document",
+    "description": """
+    The video : https://www.youtube.com/watch?v=Fps5FWfcLwo
+    """,
+    'depends': ['report'],
+    'data': [
+        'data/report_paperformat.xml',
+        'security/of_label.brand.csv',
+        'security/of_label.config.csv',
+        'security/ir.model.access.csv',
+        'views/label_config_view.xml',
+        'views/label_print_view.xml',
+        'views/label_size_data.xml',
+        'wizard/label_print_wizard_view.xml',
+        'views/label_report.xml',
+        'report/dynamic_label.xml'
+    ],
+    'uninstall_hook': 'uninstall_hook',
+    'images': ['static/description/Label.png'],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ of_label/models/label_config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# 1:  imports of odoo
+from odoo import models, fields
+
+
+class LabelMain(models.Model):
+    _name = 'of_label.brand'
+    _rec_name = 'brand_name'
+
+    brand_name = fields.Char('Name', size=64, index=True)
+    label_config_ids = fields.One2many('of_label.config', 'label_main_id',
+                                       'Label Config')
+
+
+class LabelConfig(models.Model):
+    _name = 'of_label.config'
+
+    name = fields.Char("Name", size=64, required=True, index=True)
+    height = fields.Float("Height (in mm)", required=True)
+    width = fields.Float("Width (in mm)", required=True)
+    top_margin = fields.Float("Top Margin (in mm)", default=0.0)
+    bottom_margin = fields.Float("Bottom Margin  (in mm)", default=0.0)
+    left_margin = fields.Float("Left Margin (in mm)", default=0.0)
+    right_margin = fields.Float("Right Margin (in mm)", default=0.0)
+    cell_spacing = fields.Float("Cell Spacing", default=1.0)
+    label_main_id = fields.Many2one('of_label.brand', 'Label')
