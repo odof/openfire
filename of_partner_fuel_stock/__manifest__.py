6a99f66cf63427d8ae7abe764405c5ad3800a751
Paul LE COZ <plc@openfire.fr>
2020-11-30 15:58:11 +0100

    of_partner_fuel_stock : T1303 - Nouveau module sur l'état de stock de combustible pour les clients

---
+++ of_partner_fuel_stock/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': u"OpenFire / État de stock de combustible pour les clients",
+    'version': "10.0.1.0.0",
+    'author': "OpenFire",
+    'license': "",
+    'category': "OpenFire",
+    'description': u"""
+Module d'état de stock de combustible pour les clients
+======================================================
+
+""",
+    'website': "www.openfire.fr",
+    'depends': [
+        'of_base',
+        'of_external',
+        'of_sale',
+        'stock',
+        'product',
+    ],
+    'data': [
+        'reports/external_report_templates.xml',
+        'reports/stock_report_templates.xml',
+        'security/ir.model.access.csv',
+        'views/of_partner_fuel_stock_views.xml',
+        'views/product_views.xml',
+        'views/sale_views.xml',
+        'views/stock_views.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ of_partner_fuel_stock/models/sale.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        """
+        Pour les commandes qui ont au moins une ligne marquée comme stockée et retirée à la demande :
+            - On divise le BL
+            - On incrémente le stock de combustible
+        """
+        res = super(SaleOrder, self).action_confirm()
+        for order in self.filtered(lambda o: o.order_line.filtered(lambda l: l.of_storage)):
+            # Division du BL
+            for initial_picking in order.picking_ids.filtered(lambda p: p.state not in ['cancel', 'done']):
+                order_lines = initial_picking.move_lines.mapped('procurement_id').mapped(
+                    lambda p: p.sale_line_id or p.of_sale_comp_id.kit_id.order_line_id)
+                storage_order_lines = order_lines.filtered(lambda l: l.of_storage)
+                other_order_lines = order_lines.filtered(lambda l: not l.of_storage)
+                if storage_order_lines and other_order_lines:
+                    # On divise le BL en 2
+                    storage_move_lines = storage_order_lines.mapped('procurement_ids').mapped('move_ids')
+                    other_move_lines = other_order_lines.mapped('procurement_ids').mapped('move_ids')
+                    # On copie le BL
+                    new_picking = initial_picking.copy()
+                    new_picking.of_storage = True
+                    for line in storage_move_lines:
+                        # On supprime la ligne du BL initial
+                        line_to_delete = initial_picking.move_lines.filtered(
+                            lambda l: l.procurement_id == line.procurement_id)
+                        line_to_delete.action_cancel()
+                        line_to_delete.unlink()
+                    for line in other_move_lines:
+                        # On supprime la ligne du nouveau BL
+                        line_to_delete = new_picking.move_lines.filtered(
+                            lambda l: l.procurement_id == line.procurement_id)
+                        line_to_delete.action_cancel()
+                        line_to_delete.unlink()
+                    initial_picking.action_assign()
+                    new_picking.action_assign()
+                elif storage_order_lines:
+                    initial_picking.of_storage = True
+            # Stock de combustible
+            for order_line in order.order_line.filtered(lambda l: l.of_storage):
+                product_info_list = []
+                if not order_line.of_is_kit:
+                    product_info_list.append((order_line.product_id, order_line.product_uom_qty))
+                else:
+                    for kit_line in order_line.kit_id.kit_line_ids:
+                        if kit_line.product_id.type == 'consu':
+                            product_info_list.append((kit_line.product_id,
+                                                      kit_line.qty_per_kit * order_line.product_uom_qty))
+                for product, product_qty in product_info_list:
+                    fuel_stock = self.env['of.res.partner.fuel.stock'].\
+                        search([('partner_id', '=', order.partner_id.id), ('product_id', '=', product.id)],
+                               limit=1)
+                    if fuel_stock:
+                        fuel_stock.ordered_qty = fuel_stock.ordered_qty + product_qty
+                    else:
+                        self.env['of.res.partner.fuel.stock'].create({'partner_id': order.partner_id.id,
+                                                                      'product_id': product.id,
+                                                                      'ordered_qty': product_qty})
+        return res
+
+    @api.multi
+    def action_cancel(self):
+        # Gestion du stock de combustible
+        for order in self:
+            if order.state == 'sale' and order.order_line.filtered(lambda l: l.of_storage):
+                for order_line in order.order_line.filtered(lambda l: l.of_storage):
+                    product_info_list = []
+                    if not order_line.of_is_kit:
+                        product_info_list.append((order_line.product_id, order_line.product_uom_qty))
+                    else:
+                        for kit_line in order_line.kit_id.kit_line_ids:
+                            if kit_line.product_id.type == 'consu':
+                                product_info_list.append((kit_line.product_id,
+                                                          kit_line.qty_per_kit * order_line.product_uom_qty))
+                    for product, product_qty in product_info_list:
+                        fuel_stock = self.env['of.res.partner.fuel.stock'].search(
+                            [('partner_id', '=', order.partner_id.id), ('product_id', '=', product.id)],
+                            limit=1)
+                        if fuel_stock:
+                            fuel_stock.ordered_qty = fuel_stock.ordered_qty - product_qty
+        return super(SaleOrder, self).action_cancel()
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    of_storage = fields.Boolean(string=u"Stockage et retrait à la demande des articles")
+
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super(SaleOrderLine, self).product_id_change()
+        if self.product_id.of_storage:
+            self.of_storage = True
+        return res
