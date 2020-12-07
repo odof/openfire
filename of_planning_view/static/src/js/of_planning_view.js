c1307966e14edecf1ed631cddaf5e64cafc6f013
Gaël FRASSON <fragae@openfire.fr>
2020-12-07 17:58:24 +0100

    [FIX] of_planning_view: correction de l'event on_mouseout des créneaux dispo qui fonctionnait mal sur certains navigateurs

--- of_planning_view/static/src/js/of_planning_view.js
+++ of_planning_view/static/src/js/of_planning_view.js
@@ -1504,7 +1504,9 @@ var PlanningCreneauDispo = Widget.extend({
      */
     on_mouseout: function (ev) {
         ev.preventDefault();
-        this.$el.find('.of_planning_creneau_dispo').outerHeight(this.hauteur);
+        if (!this.$el.is(":hover")) {
+            this.$el.find('.of_planning_creneau_dispo').outerHeight(this.hauteur);
+        }
     },
     /**
      *  Ouvre le pop-up de sélection de secteur
