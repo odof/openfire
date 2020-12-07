71fd53a3def198e1b15e6521028f4b8d03ed0a7f
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2020-12-07 17:39:29 +0100

    of_account : Ajout d'un outil de lettrage dans le formulaire des paiements, à la manière de celui présent dans le formulaire des factures.

--- of_account/__manifest__.py
+++ of_account/__manifest__.py
@@ -29,8 +29,12 @@ Module de comptabilité OpenFire.
     ],
     'demo_xml': [],
     'data': [
+        'views/of_account.xml',
         'views/of_account_view.xml',
         'wizards/wizard_edit_export_view.xml'
+    ],
+    'qweb': [
+        "static/src/xml/account_payment.xml",
     ],
     'installable': True,
 }
