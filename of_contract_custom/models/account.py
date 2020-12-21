13c1057291cf9210c98f6a92d525df4813590f2a
Stan Heurtault <heusta@openfire.fr>
2020-12-21 18:00:26 +0100

    T1695 début de la tache (dev non fini donc commenté) T1681 correction tache

--- of_contract_custom/models/account.py
+++ of_contract_custom/models/account.py
@@ -19,6 +19,7 @@ class OfAccountInvoice(models.Model):
     of_contract_id = fields.Many2one('of.contract', string="(OF) Contrat")
     of_contract_period = fields.Char(string=u"Période du contrat", compute='_compute_of_contract_period')
     of_intervention_id = fields.Many2one('of.planning.intervention', string="RDV d'intervention")
+    # of_alert_contract_date = fields.Text(string="Alerte date", compute="_compute_of_alert_contract_date")
     # of_address_id = fields.Many2one('res.partner', string="Adresse d'intervention", compute='_compute_of_address_id')
 
     @api.depends('of_contract_id', 'date_invoice', 'of_intervention_id')
@@ -46,6 +47,13 @@ class OfAccountInvoice(models.Model):
                         period_start = date + relativedelta(months=months, days=1)
                         invoice.of_contract_period = "%s - %s" % (format_date(fields.Date.to_string(period_start), lang),
                                                                   format_date(invoice.date_invoice, lang))
+
+    # @api.depends('of_contract_id', 'invoice_line_ids', 'invoice_line_ids.of_contract_line_id', 'invoice_date')
+    # def _compute_of_alert_contract_date(self):
+    #     for invoice in self:
+    #         lines = invoice.invoice_line_ids.filtered(lambda il: il.of_contract_line_id and
+    #                                                              il.of_contract_line_id.frequency_type == 'date')
+    #         if lines:
 
     # @api.depends('invoice_line_ids', 'invoice_line_ids.of_contract_line_id', 'invoice_line_ids.of_contract_line_id.address_id')
     # def _compute_of_address_id(self):
