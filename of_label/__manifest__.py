ff29bd0b576f7f2268f6ddf8fd00ca92d1435788
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2020-12-31 16:56:31 +0100

    of_label : Correction du descriptif du module + affichage du chemin complet de l'emplacement dans le modèle d'étiquette par défaut.

--- of_label/__manifest__.py
+++ of_label/__manifest__.py
@@ -9,26 +9,26 @@
     "license": "AGPL-3",
     "website": "http://www.serpentcs.com",
     "summary": "Generate customised labels of any document",
-    "description": """
+    "description": u"""
     Seul le compte admin peut accéder à la configuration des étiquettes. Configuration > Technique > Impression d'étiquettes
-    
+
     Label Configuration: les différents patrons d'étiquettes (exemple OpenFire - 2 x 2 paysage)
-    
+
     Label Print: les différents contenus d'étiquettes (exemple Étiquette de stock 2x2)
-    
+
     Une fois le module installé vous pouvez créer des actions contextuelles pour vos impression d'étiquettes.
-    Pour ce faire, dans l'étiquettes que vous voulez utiliser > Onglet Advance > cliquer sur "Add sidebar button".
-    Vous pouvez ensuite imprimer des étiquettes dans l'objet concerné depuis le bouton d'actions
-    
+    Pour ce faire, dans l'étiquette que vous voulez utiliser > Onglet Advance > cliquer sur "Add sidebar button".
+    Vous pouvez ensuite imprimer des étiquettes dans l'objet concerné depuis le bouton d'actions.
+
     Exemple: Après avoir installé ce module, je veux utiliser les étiquettes de stock fournies par défaut.
     Je vais dans Configuration > Technique > Impression d'étiquettes > Label Print et je choisis "étiquette de stock 2x2".
     Je clique sur le bouton "Add sidebar button" dans l'onglet "Advance".
     Je peux désormais aller dans un BL ou BR et cliquer sur Action > Label (étiquette de stock 2x2).
-    Je choisis le nombre de copies de chaques lignes et les lignes à imprimer et clique sur "PRINT".
+    Je choisis le nombre de copies de chaque ligne et les lignes à imprimer et clique sur "PRINT".
     Et voilà! j'ai un PDF avec mes étiquettes de stock!
-    
+
     The video : https://www.youtube.com/watch?v=Fps5FWfcLwo
-    """,
+""",
     'depends': [
         'of_base',
         'report',
