1f81355f96449d290ba29e638f38d939a839dfb8
Stan Heurtault <heusta@openfire.fr>
2021-01-12 17:41:19 +0100

    of_sale_kanban : Ajout d'un module pour gérer les étapes kanban des devis/commandes.

---
+++ of_sale_kanban/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    OpenFire
+#    Version OF10.0
+#
+#    Module conçu et développé par OpenFire SAS
+#
+#    Compatible avec Odoo 10 Community Edition
+#
+##############################################################################
+
+{
+    'name' : u"OpenFire / Ventes - vue kanban",
+    'version' : "10.0.1.0.0",
+    'license': '',
+    'author' : "OpenFire",
+    'website' : "www.openfire.fr",
+    'category': "Module OpenFlam",
+    'summary': u"Personnalisation des ventes OpenFire",
+    'description': u"""
+
+Module OpenFire / Ventes - vue kanban
+=====================================
+
+Modification de la vue kanban des devis/commandes + ajout d'un champ étapes kanban
+
+""",
+    'depends' : [
+        "sale",
+    ],
+    'external_dependancies': {
+        'python': ['pdfminer', 'pypdftk', 'pyPdf'],
+    },
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/of_sale_kanban_view.xml',
+        'data/of_sale_kanban_data.xml',
+        ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
