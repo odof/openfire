4c9be13ce69ab63f0049d21285870e11f69686e1
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2021-01-29 10:14:36 +0100

    of_datastore_connector of_datastore_supplier : Séparation de la classe abstraite du connecteur dans un module dédié.

---
+++ of_datastore_connector/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': u"OpenFire / Connecteur Odoo",
+    'version': "10.0.1.0.0",
+    'author': "OpenFire",
+    'website': "http://www.openfire.fr",
+    'category': "Openfire",
+    'description': u"""
+Module OpenFire de connexion à une base Odoo.
+=============================================
+
+Ajout d'une classe abstraite permettant de se connecter à une autre base odoo.
+""",
+    'depends': [],
+    'external_dependencies': {
+        'python': [
+            'openerplib'
+        ],
+    },
+    'init_xml': [],
+    'demo_xml': [],
+    'data': [],
+    'installable': True,
+}
