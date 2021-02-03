5267d57ab242574cbc3793d8e3417944449ba2e7
Paul LE COZ <plc@openfire.fr>
2021-02-03 18:09:57 +0100

    of_sale : T1853 - Ajout du prix d'achat des articles dans la vue "Bons de commande" pour les membres des groupes Ventes > Gestionnaire ET Marge > Responsable
    of_access_control : T1853 - Modification des groupes pouvant visualiser le champ "Prix d'achat" : Ventes > Responsable ET Marge > Responsable
    
    of_base : T1853 - Surcharge de la méthode standard 'user_has_groups' pour permettre l'utilisation de '+' (i.e. doit appartenir à tous les groupes)

--- of_base/models/of_base.py
+++ of_base/models/of_base.py
@@ -3,6 +3,64 @@
 from odoo import models, api, tools, fields, SUPERUSER_ID, _
 from odoo.exceptions import AccessError
 from odoo.tools import OrderedSet
+from odoo.models import BaseModel
+
+
+@api.model
+def user_has_groups(self, groups):
+    """Return true if the user is member of at least one of the groups in
+    ``groups``, and is not a member of any of the groups in ``groups``
+    preceded by ``!``. Typically used to resolve ``groups`` attribute in
+    view and model definitions.
+
+    OpenFire Addition: allow use of '+' in the group list to specify
+    several groups user must belong to.
+
+    :param str groups: comma-separated list of fully-qualified group
+        external IDs, e.g., ``base.group_user,base.group_system``,
+        optionally preceded by ``!``
+    :return: True if the current user is a member of one of the given groups
+        not preceded by ``!`` and is not member of any of the groups
+        preceded by ``!``
+    """
+    from odoo.http import request
+    user = self.env.user
+
+    has_groups = []
+    not_has_groups = []
+    for group_ext_id in groups.split(','):
+        group_ext_id = group_ext_id.strip()
+        if group_ext_id[0] == '!':
+            not_has_groups.append(group_ext_id[1:])
+        else:
+            has_groups.append(group_ext_id)
+
+    for group_ext_id in not_has_groups:
+        if group_ext_id == 'base.group_no_one':
+            # check: the group_no_one is effective in debug mode only
+            if user.has_group(group_ext_id) and request and request.debug:
+                return False
+        else:
+            if user.has_group(group_ext_id):
+                return False
+
+    for group_ext_id in has_groups:
+        for of_group_ext_id in group_ext_id.split('+'):
+            of_group_ext_id = of_group_ext_id.strip()
+            if of_group_ext_id == 'base.group_no_one':
+                # check: the group_no_one is effective in debug mode only
+                if not user.has_group(of_group_ext_id) or not request or not request.debug:
+                    break
+            else:
+                if not user.has_group(of_group_ext_id):
+                    break
+        else:
+            return True
+
+    return not has_groups
+
+
+BaseModel.user_has_groups = user_has_groups
 
 
 class IrModelFields(models.Model):
