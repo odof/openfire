1c2677896ceca6a046ea51a46b41228260ab710c
Paul LE COZ <plc@openfire.fr>
2021-02-12 09:19:35 +0100

    of_questionnaire : T1456 - Ajout des questions conditionnelles, des types photo et dessin, et de la possibilité d'ajouter une photo à tout type de question

--- of_questionnaire/wizards/of_questionnaire_wizard.py
+++ of_questionnaire/wizards/of_questionnaire_wizard.py
@@ -1,26 +1,48 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools.safe_eval import safe_eval
+
 
 class RepondreQuestionnaireWizard(models.TransientModel):
     _name = 'of.repondre.questionnaire.wizard'
 
-    question_ids = fields.Many2many('of.planning.intervention.question', relation="of_repondre_questionnaire_question_rel")
+    question_ids = fields.Many2many(
+        'of.planning.intervention.question', relation="of_repondre_questionnaire_question_rel")
     question_id = fields.Many2one('of.planning.intervention.question')
-    answer_type = fields.Selection([('bool', "Oui/Non"),
-                                    ('text', "Texte libre"),
-                                    ('one', u"Plusieurs choix, une seule réponse possible"),
-                                    ('list', u"plusieurs choix, plusieurs réponses possibles")], compute="_compute_type", store=True)
+    answer_type = fields.Selection(
+        [('bool', "Oui/Non"),
+         ('text', "Texte libre"),
+         ('one', u"Plusieurs choix, une seule réponse possible"),
+         ('list', u"plusieurs choix, plusieurs réponses possibles"),
+         ('photo', u"Photo"),
+         ('drawing', u"Dessin")], compute="_compute_type", store=True)
     question = fields.Char(related='question_id.name')
     answer_bool = fields.Selection([('Oui', 'Oui'), ('Non', 'Non')], string=u"Réponse")
     answer_id = fields.Many2one('of.questionnaire.line.reponse', string=u"Réponse")
-    answer_ids = fields.Many2many('of.questionnaire.line.reponse', relation="of_repondre_questionnaire_reponse_rel", string=u"Réponses")
+    answer_ids = fields.Many2many(
+        'of.questionnaire.line.reponse', relation="of_repondre_questionnaire_reponse_rel", string=u"Réponses")
     answer_text = fields.Text(string=u"Réponse")
+    attachment_answer = fields.Binary(string=u"Fichier réponse", attachment=True)
+    attachment_answer_name = fields.Char(string=u"Nom du fichier réponse")
+    additional_attachment_answer = fields.Boolean(
+        string=u"Fichier réponse supplémentaire", compute='_compute_additional_attachment_answer')
+    attachment_required = fields.Boolean(string=u"Fichier réponse obligatoire", compute='_compute_attachment_required')
 
     @api.depends('question_id')
     def _compute_type(self):
         for reponse in self:
             reponse.answer_type = reponse.question_id.answer_type
+
+    @api.depends('question_id')
+    def _compute_additional_attachment_answer(self):
+        for rec in self:
+            rec.additional_attachment_answer = rec.question_id.photo
+
+    @api.depends('question_id')
+    def _compute_attachment_required(self):
+        for rec in self:
+            rec.attachment_required = rec.question_id.photo_required
 
     @api.model
     def _convert_question_answer(self, question):
@@ -42,6 +64,11 @@ class RepondreQuestionnaireWizard(models.TransientModel):
                                                   ('name', '=', r)], limit=1)
                     reponses |= reponse
             result['answer_ids'] = [(6, 0, reponses._ids)]
+
+        if question.answer_type in ('photo', 'drawing') or question.photo:
+            result['attachment_answer_name'] = question.attachment_answer_name
+            result['attachment_answer'] = question.attachment_answer
+
         return result
 
     @api.model
@@ -52,6 +79,10 @@ class RepondreQuestionnaireWizard(models.TransientModel):
             rec['question_id'] = question_ids[0][1]
             rec['question_ids'] = question_ids[1:]
             question = self.env['of.planning.intervention.question'].browse(question_ids[0][1])
+            if question.definitive_answer:
+                rec.update(self._convert_question_answer(question))
+        elif 'question_id' in rec:
+            question = self.env['of.planning.intervention.question'].browse(rec['question_id'])
             if question.definitive_answer:
                 rec.update(self._convert_question_answer(question))
         return rec
@@ -65,11 +96,42 @@ class RepondreQuestionnaireWizard(models.TransientModel):
             self.question_id.write({'definitive_answer': self.answer_id.name})
         elif self.question_id.answer_type == 'list':
             self.question_id.write({'definitive_answer': ', '.join([answer.name for answer in self.answer_ids])})
+        elif self.question_id.answer_type in ('photo', 'drawing'):
+            self.question_id.write({'definitive_answer': self.attachment_answer_name})
+
+        if self.question_id.answer_type in ('photo', 'drawing') or (self.additional_attachment_answer and
+                                                                    self.attachment_answer):
+            self.question_id.write({'attachment_answer_name': self.attachment_answer_name,
+                                    'attachment_answer': self.attachment_answer})
+
+        if self.question_id.condition_unmet:
+            self.question_id.write({'condition_unmet': False})
+
         return self.next_question()
 
     def next_question(self):
         self.question_id = self.question_ids and self.question_ids[0] or False
         self.question_ids -= self.question_id
+        # On teste s'il y a des conditions
+        if self.question_id.condition:
+            ctx = {}
+            all_questions = self.env['of.planning.intervention.question'].search(
+                [('intervention_id', '=', self.question_id.intervention_id.id)])
+            for question in all_questions:
+                ctx[question.id_code] = question.definitive_answer
+            condition_code = self.question_id.condition_code
+            condition_code = condition_code.replace(' = ', ' == ')
+            condition_code = condition_code.replace(' et ', ' and ')
+            condition_code = condition_code.replace(' ou ', ' or ')
+            condition_res = safe_eval(condition_code, ctx)
+            if not condition_res:
+                # Si la condition n'est pas respectée, on vide la réponse potentielle de la question
+                # et on passe à la question suivante
+                self.question_id.write({'condition_unmet': True,
+                                        'definitive_answer': False,
+                                        'attachment_answer_name': False,
+                                        'attachment_answer': False})
+                return self.next_question()
         if self.question_id:
             self.write(self._convert_question_answer(self.question_id))
             return {"type": "ir.actions.do_nothing"}
