a6ddaabf05637cdb2dfaeb4f53a02e6ef393472a
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2021-04-21 13:42:57 +0200

    of_account : T2052 - A la saisie du parenaire dans la 1ère ligne d'une pièce comptable, le compte de client est rempli si le journal n'est pas un journal d'achat.

--- of_account/models/of_account.py
+++ of_account/models/of_account.py
@@ -278,12 +278,12 @@ class AccountMoveLine(models.Model):
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
         if self.partner_id and not self.account_id and not self._context.get('line_ids', False):
-            if self.journal_id.type == 'sale':
-                # Pour un journal de vente on prend le compte de tiers client.
-                self.account_id = self.partner_id.property_account_receivable_id
-            elif self.journal_id.type == 'purchase':
+            if self.journal_id.type == 'purchase':
                 # Pour un journal d'achat on prend le compte de tiers fournisseur.
                 self.account_id = self.partner_id.property_account_payable_id
+            else:
+                # Pour tout autre journal on prend le compte de tiers client.
+                self.account_id = self.partner_id.property_account_receivable_id
 
     @api.multi
     def write(self, vals):
