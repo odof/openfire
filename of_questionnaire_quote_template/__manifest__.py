470172aa35f58feb9ed3990ce62919b21972431a
Gaël <gf@openfire.eu>
2021-05-14 17:49:46 +0200

    T2167 - of_questionnaire_quote_template : Initialisation du module, faisant le lien entre les modèles d'intervention, leur questionnaire et les modèles de devis

---
+++ of_questionnaire_quote_template/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    OpenFire
+#    Version OF10.0
+#
+#    Module conçu et développé par OpenFire SAS
+#
+#    Compatible avec Odoo 10 Community Edition
+#    Copyright © 2004-2016 Odoo S.A. License GNU LGPL
+#
+##############################################################################
+
+{
+    'name': "OpenFire / Questionnaire + Modèles de devis",
+    'version': "10.0.1.0.0",
+    'author': "OpenFire",
+    'website': "www.openfire.fr",
+    'category': "questionnaire",
+    'summary': u"Ajout des modèles d'intervention dans les modèles de devis",
+    'description': u"""
+Module OpenFire - Questionnaire Modèle de devis
+===============================================
+Ce module ajoute les modèles d'interventiosn dans les modèles de devis
+
+Fonctionnalités
+---------------
+ - Ajout d'un onglet "Questionnaire" dans les modèles de devis, contenant le questionnaire du modèle d'intervention lié.
+ - Au moment de créer un RDV depuis une CC qui contient un modèle de devis, charge le modèle d'intervention s'il y a lieu. Ainsi que le questionnaire lié.
+""",
+    'depends': [
+        'of_questionnaire',
+        'of_sale_quote_template',
+    ],
+    'data': [
+        'views/of_questionnaire_quote_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
