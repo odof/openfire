c87e905fc1ccbcf4515816bad59592ed15b19b94
Gaël <gf@openfire.eu>
2021-06-22 17:06:16 +0200

    T2180 - of_company_multi_logos : Ajout fonctionnalité d'affichage de logos au dessus du pied de page ou dans le coin bas gauche

--- of_company_multi_logos/models/of_company_multi_logos.py
+++ of_company_multi_logos/models/of_company_multi_logos.py
@@ -2,9 +2,39 @@
 
 from odoo import api, fields, models, _
 
+
 class ResCompany(models.Model):
     _inherit = "res.company"
+
     of_logo_ids = fields.Many2many('of.company.multi.logos', 'company_logo', 'company_id', 'logo_id', string='Logos')
+    of_logo_footer = fields.Boolean(
+        string=u"Logos en pied de page", compute='_compute_of_logo_footer', store=True,
+        help=u"Cette société utilise des logos à positionner juste au dessus du pied de page")
+
+    @api.depends('of_logo_ids')
+    def _compute_of_logo_footer(self):
+        for company in self:
+            company.of_logo_footer = any(
+                [name.startswith(u"Logo_footer") for name in company.of_logo_ids.mapped('name')])
+
+    @api.multi
+    def _write(self, vals):
+        res = super(ResCompany, self)._write(vals)
+        if 'of_logo_footer' in vals:
+            # Utiliser le format de papier spécial
+            if vals['of_logo_footer']:
+                paperformat = self.env.ref('of_company_multi_logos.paperformat_euro_of_logo_footer')
+            # Utiliser le format de papier standard
+            else:
+                paperformat = self.env.ref('report.paperformat_euro')
+            self.write({'paperformat_id': paperformat.id})
+        return res
+
+    def get_logo_footer(self):
+        self.ensure_one()
+        if not self.of_logo_footer:
+            return []
+        return [l.logo for l in self.of_logo_ids.filtered(lambda s: s.name.startswith(u"Logo_footer"))]
 
     @api.multi
     def getLogo(self, name):
@@ -13,6 +43,7 @@ class ResCompany(models.Model):
             if logos.name == name:
                 return logos.logo
         return False
+
 
 class OfCompanyMultiLogos(models.Model):
     _name = "of.company.multi.logos"
