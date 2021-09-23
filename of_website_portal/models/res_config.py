e33540a0ca3175bd632f39edb66ce417292bb568
Archibald JEGO <aj@openfire.fr>
2021-09-23 17:45:41 +0200

    of_website_portal: T2395 - Configuration automatique à l'installation du module, réglage des problèmes de public user affecter par l'affichage des taxes, disponibilité des bon de livraison dans mon compte.

--- of_website_portal/models/res_config.py
+++ of_website_portal/models/res_config.py
@@ -1,6 +1,13 @@
 from odoo import fields, models
 
 
+class SaleConfigSettings(models.TransientModel):
+    _inherit = 'sale.config.settings'
+
+    group_show_price_subtotal = fields.Boolean(group='base.group_portal,base.group_user')
+    group_show_price_total = fields.Boolean(group='base.group_portal,base.group_user')
+
+
 class WebsiteConfigSettings(models.TransientModel):
     _inherit = 'website.config.settings'
 
