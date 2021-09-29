fd0da9a4c024a556ae96ade078494d08603e56cc
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2021-09-29 11:50:19 +0200

    of_datastore_product of_datastore_supplier : Nettoyage.

--- of_datastore_supplier/__manifest__.py
+++ of_datastore_supplier/__manifest__.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 
 {
-    "name" : "OpenFire / Base centrale des articles",
-    "version" : "10.0.1.0.0",
-    "author" : "OpenFire",
-    "website" : "http://www.openfire.fr",
-    "category" : "OpenFire",
-    "description": u"""
+    'name': "OpenFire / Base centrale des articles",
+    'version': "10.0.1.0.0",
+    'author': "OpenFire",
+    'website': "http://www.openfire.fr",
+    'category': "OpenFire",
+    'description': u"""
 Module OpenFire pour bases de centralisation des tarifs.
 ========================================================
 
 Ajoute un champ de notes de mise à jour dans les marques.
 """,
-    "depends" : ['of_product', 'of_product_brand', 'of_import'],
-    "init_xml" : [],
-    "demo_xml" : [],
-    "data" : [
+    'depends': ['of_product', 'of_product_brand', 'of_import'],
+    'data': [
         'views/of_datastore_supplier_views.xml',
     ],
-    "installable": True,
+    'installable': True,
 }
