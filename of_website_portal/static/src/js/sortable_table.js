3ac3ebedce5e246e5de895f47c2b22cdead60b52
Archibald JEGO <aj@openfire.fr>
2021-10-04 15:17:33 +0200

    of_website_portal: T2497 - Ajout de traductions + correction de mise en forme de barre de recherche du filtre de liste livraison du portail

--- of_website_portal/static/src/js/sortable_table.js
+++ of_website_portal/static/src/js/sortable_table.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
     $('#delivery_table').DataTable({
         "paging":           false,
         "info":             false,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/fr_fr.json"
+        }
     });
 } );
