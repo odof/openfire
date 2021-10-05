5e79b79f1934a9c980b277721fa69df9307f06f7
Paul <plc@openfire.fr>
2021-10-05 11:19:14 +0200

    of_sale_planning : Correction de la fonction de calcul da la date prévue des picking -> ne pas prendre en compte la date prévue des mouvements si des RDV sont liés ou si une commande avec date de pose prévisionnelle est liée

---
+++ of_sale_planning/models/stock.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.one
+    @api.depends('move_lines.date_expected', 'of_intervention_ids', 'of_intervention_ids.date', 'sale_id',
+                 'sale_id.of_date_de_pose')
+    def _compute_dates(self):
+        res = super(StockPicking, self)._compute_dates()
+        if self.of_intervention_ids:
+            last_inter = self.of_intervention_ids.sorted('date')[-1]
+            self.min_date = last_inter.date
+        elif self.sale_id and self.sale_id.of_date_de_pose:
+            self.min_date = self.sale_id.of_date_de_pose
+        return res
