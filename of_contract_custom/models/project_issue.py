447c0f624f2af6dab8b13385e72dd5299f3fe1eb
Gaël <gf@openfire.eu>
2021-10-12 15:09:06 +0200

    T2356 SQL ok, reste à gérer les étapes kanban

--- of_contract_custom/models/project_issue.py
+++ of_contract_custom/models/project_issue.py
@@ -20,108 +20,177 @@ class ProjectIssue(models.Model):
         Pour les SAV sans DI -> créé une DI avec toutes les informations du SAV
         """
         if not self.env['ir.config_parameter'].sudo().get_param('of_migration_sav_di'):
+            _logger.info(u"OPENFIRE : début de migration des SAV dans les DI (T2356)")
             sav_obj = self.env['project.issue']
             service_obj = self.env['of.service']
-            planning_tag_obj = self.env['of.planning.tag']
+            cr = self.env.cr
             sav_type = self.env.ref('of_contract_custom.of_contract_custom_type_sav')
             sav_tache  = self.env['of.planning.tache'].search([('name', 'ilike', 'SAV')], limit=1)
             if not sav_tache:
-                _logger.error(u"OPENFIRE ERROR : impossible de migrer les SAV dans les DI, tâche 'SAV' inexistante")
-                return
-            dans_7_jours_str = fields.Date.to_string(fields.Date.from_string(fields.Date.today()) + timedelta(days=1))
-            error_dict = {}
+                sav_tache  = self.env['of.planning.tache'].search([('name', 'ilike', '%SAV%')], limit=1)
+                if not sav_tache:
+                    _logger.error(u"OPENFIRE ERROR : impossible de migrer les SAV dans les DI, tâche 'SAV' inexistante")
+                    return
 
-            def get_vals(sav, di):
-                # concaténer diffŕenets champs dans la description
-                note = di and di.note or sav.description or u""
-                note += sav.of_intervention and u"\nNature de l'intervention: %s" % sav.of_intervention or u""
-                note += sav.of_piece_commande and u"\nPièces à commander: %s" % sav.of_piece_commande or u""
-                note += u"\nnº SAV ancienne version: %s" % sav.of_code
-                if sav.product_name_id and not sav.of_produit_installe_id:
-                    note += u"\nArticle associé ancienne version: %s" % sav.product_name_id.display_name
-                # récupérer les étiquettes de la DI si existante, ajouter les étiquettes du SAV
-                # les project.tags sont migrées dans les of.planning.tags dans l'auto_init et donc
-                # avant l'exécution de cette fonction
-                di_tags = di and di.tag_ids or planning_tag_obj
-                sav_tags = planning_tag_obj.search([('name', 'in', sav.tag_ids.mapped('name'))])
-                tag_ids = (di_tags | sav_tags).ids
-                # prendre les RDV des SAV, les ajouter à la DI si besoin
-                intervention_ids = sav.interventions_liees and sav.interventions_liees.ids or []
-                # peupler le nouveau champ payer_mode
-                payer_mode = False
-                if sav.of_payant_client and sav.of_payant_fournisseur:
-                    payer_mode = 'retailer'
-                elif sav.of_payant_client and not sav.of_payant_fournisseur:
-                    payer_mode = 'client'
-                elif not sav.of_payant_client and sav.of_payant_fournisseur:
-                    payer_mode = 'manufacturer'
-                # s'assurer que la date de début et de fin de planif soient cohérentes
-                date_next = di and di.date_next or sav.date or fields.Date.today()
-                date_fin = di and di.date_fin or sav.date_deadline or dans_7_jours_str
-                if date_fin < date_next:
-                    date_fin = fields.Date.to_string(fields.Date.from_string(date_next) + timedelta(days=7))
-
-                di_vals = {
-                    #'titre': di and di.titre or sav.name,
-                    #'active': di and di.active or sav_sans.active,
-                    #'partner_id': di and di.partner_id.id or sav.partner_id.id,
-                    #'address_id': di and di.address_id and di.address_id.id or sav.partner_id.id,
-                    #'company_id': di and di.company_id and di.company_id.id or sav.company_id and sav.company_id.id,
-                    #'note': note,
-                    'tag_ids': [(6, 0, tag_ids)],
-                    #'priority': di and di.priority or sav.priority,
-                    #'user_id': di and di.user_id and di.user_id.id or sav.user_id and sav.user_id.id,
-                    'date_next': date_next,
-                    'date_fin': date_fin,
-                    'intervention_ids': [(4, i_id, 0) for i_id in intervention_ids],
-                    #'of_categorie_id': sav.of_categorie_id and sav.of_categorie_id.id,
-                    #'of_canal_id': sav.of_canal_id and sav.of_canal_id.id,
-                    #'parc_installe_id': di and di.parc_installe_id and di.parc_installe_id.id or
-                    #                    sav.of_produit_installe_id and sav.of_produit_installe_id.id,
-                    #'sav_id': sav.id,
-                    #'payer_mode': payer_mode,
-                    'type_id': sav_type.id,
-                    'tache_id': sav_tache.id,
-                }
-                return di_vals
             # Ne pas toucher aux SAV déjà migrés
             # Mettre à jour les DI des SAV avec DI, créer des DI pour les autres
             sav_avec_di = service_obj.search([('sav_id', '!=', False)]).mapped('sav_id')\
                 .filtered(lambda s: not s.is_migrated)
-            sav_sans_di = sav_obj.search([('is_migrated', '=', False)]) - sav_avec_di
+            # ne pas essayer de migrer les SAV sans partenaire
+            sav_sans_di = sav_obj.search([('is_migrated', '=', False), ('partner_id', '!=', False)]) - sav_avec_di
 
-            for sav_avec in sav_avec_di:
-                try:
-                    di = service_obj.search([('sav_id', '=', sav_avec.id)], limit=1)
-                    vals = get_vals(sav_avec, di)
-                    di.write(vals)
-                    sav_avec.is_migrated = True
-                except Exception as e:
-                    k = e.message or e.name
-                    if not error_dict.get(k):
-                        error_dict[k] = []
-                    error_dict[k].append(sav_avec.id)
-            for sav_sans in sav_sans_di:
-                if not sav_sans.partner_id:
-                    continue
-                try:
-                    vals = get_vals(sav_sans, False)
-                    service_obj.create(vals)
-                    sav_sans.is_migrated = True
-                except Exception as e:
-                    k = e.message or e.name
-                    if not error_dict.get(k):
-                        error_dict[k] = []
-                    error_dict[k].append(sav_sans.id)
+            if sav_avec_di:
+                _logger.info(u"OPENFIRE : mise à jour des DI liées à un SAV")
+                cr.execute("""UPDATE of_service os SET
+    titre = COALESCE ( os.titre, pi.name ),
+    active = COALESCE ( os.active, pi.active ),
+    partner_id = COALESCE ( os.partner_id, pi.partner_id ),
+    address_id = COALESCE ( os.address_id, pi.partner_id ),
+    company_id = COALESCE ( os.company_id, pi.company_id ),
+    priority = COALESCE ( os.priority, pi.priority ),
+    user_id = COALESCE ( os.user_id, pi.user_id ),
+    of_categorie_id = COALESCE ( os.of_categorie_id, pi.of_categorie_id ),
+    of_canal_id = COALESCE ( os.of_canal_id, pi.of_canal_id ),
+    parc_installe_id = COALESCE ( os.parc_installe_id, pi.of_produit_installe_id ),
+    note = COALESCE ( os.note, pi.description
+        || (CASE WHEN pi.of_intervention IS NOT NULL 
+            THEN '
+Nature de l''intervention: ' || pi.of_intervention ELSE '' END)
+        || (CASE WHEN pi.of_piece_commande IS NOT NULL
+            THEN '
+Pièces à commander: ' || pi.of_piece_commande ELSE '' END)
+        || '
+nº SAV ancienne version: ' || pi.of_code
+        || (CASE WHEN pi.product_name_id IS NOT NULL AND pi.of_produit_installe_id IS NULL 
+            THEN '
+Article associé ancienne version: ['
+                || (SELECT pp1.default_code FROM product_product pp1 WHERE pp1.id = pi.product_name_id) 
+                || '] '
+                || (SELECT pt.name
+                    FROM product_product pp2 JOIN product_template pt ON pt.id = pp2.product_tmpl_id
+                    WHERE pp2.id = pi.product_name_id) ELSE '' END )),
+    payer_mode = COALESCE ( os.payer_mode,
+                            CASE
+                                WHEN pi.of_payant_client AND pi.of_payant_fournisseur THEN 'retailer'
+                                WHEN pi.of_payant_client THEN 'client'
+                                WHEN pi.of_payant_fournisseur THEN 'manufacturer'
+                                ELSE NULL
+                            END),
+    type_id = %s
+FROM project_issue pi
+WHERE os.sav_id = pi.id AND pi.id in %s""", (sav_type.id, sav_avec_di._ids))
 
-            if error_dict:
-                _logger.error(u"OPENFIRE ERROR : migration des SAV dans les DI (T2356)")
-                for kew in error_dict:
-                    _logger.error(u"erreur pour les ids %s" % error_dict[kew])
-                    _logger.error(u"%s" % kew)
-            else:
-                _logger.info(u"OPENFIRE SUCCÈS : migration des SAV dans les DI (T2356)")
-                self.env['ir.config_parameter'].sudo().set_param('of_migration_sav_di', True)
+            _logger.info(u"OPENFIRE : création de DI pour les SAV sans DI")
+            cr.execute("""INSERT INTO of_service (
+    create_uid,
+    create_date,
+    write_uid,
+    write_date,
+    base_state,
+    titre,
+    active,
+    partner_id,
+    address_id,
+    company_id,
+    priority,
+    user_id,
+    of_categorie_id,
+    of_canal_id,
+    parc_installe_id,
+    sav_id,
+    name,
+    note,
+    payer_mode,
+    date_next,
+    date_fin,
+    tache_id,
+    duree,
+    type_id
+)
+(
+SELECT
+    1,
+    now(),
+    1,
+    now(),
+    'calculated',
+    pi.name,
+    pi.active,
+    pi.partner_id,
+    pi.partner_id,
+    pi.company_id,
+    pi.priority,
+    pi.user_id,
+    pi.of_categorie_id,
+    pi.of_canal_id,
+    pi.of_produit_installe_id,
+    pi.id,
+    (SELECT rp.name || ' ' || rp.zip FROM res_partner rp WHERE rp.id = pi.partner_id)
+        || ' ' || (SELECT opt.name FROM of_planning_tache opt WHERE opt.id = %s),
+    pi.description
+        || (CASE WHEN pi.of_intervention IS NOT NULL
+            THEN '
+Nature de l''intervention: ' || pi.of_intervention ELSE '' END)
+        || (CASE WHEN pi.of_piece_commande IS NOT NULL
+            THEN '
+Pièces à commander: ' || pi.of_piece_commande ELSE '' END)
+        || '
+nº SAV ancienne version: ' || pi.of_code
+        || (CASE WHEN pi.product_name_id IS NOT NULL AND pi.of_produit_installe_id IS NULL
+            THEN '
+Article associé ancienne version: [' 
+                || (SELECT pp1.default_code FROM product_product pp1 WHERE pp1.id = pi.product_name_id)
+                || '] '
+                || (SELECT pt.name
+                    FROM product_product pp2
+                    JOIN product_template pt ON pt.id = pp2.product_tmpl_id
+                    WHERE pp2.id = pi.product_name_id) ELSE '' END),
+    CASE
+        WHEN pi.of_payant_client AND pi.of_payant_fournisseur THEN 'retailer'
+        WHEN pi.of_payant_client THEN 'client'
+        WHEN pi.of_payant_fournisseur THEN 'manufacturer'
+        ELSE NULL
+    END AS payer_mode,
+    CASE WHEN pi.date IS NOT NULL THEN pi.date::date ELSE CURRENT_DATE END AS date_next,
+    CASE
+        WHEN pi.date IS NULL AND pi.date_deadline IS NULL THEN CURRENT_DATE + integer '7'
+        WHEN pi.date_deadline IS NULL OR pi.date_deadline < pi.date THEN pi.date::date + integer '7'
+        ELSE pi.date_deadline::date
+    END AS date_fin,
+    %s,
+    %s,
+    %s
+FROM project_issue pi
+WHERE pi.id in %s
+)""", (sav_tache.id, sav_tache.id, sav_tache.duree, sav_type.id, sav_sans_di._ids))
+
+            # noter tous les SAV avec partenaire comme "migrés"
+            (sav_avec_di | sav_sans_di).write({'is_migrated': True})
+
+            _logger.info(u"OPENFIRE : rattacher aux DI les étiquettes en provenance des SAV")
+            cr.execute("""INSERT INTO of_service_of_planning_tag_rel (service_id, tag_id)
+SELECT os.id, opt.id
+FROM of_service os
+JOIN project_issue_project_tags_rel pipt ON pipt.project_issue_id = os.sav_id
+JOIN project_tags pt ON pt.id = pipt.project_tags_id
+JOIN of_planning_tag opt ON UPPER(opt.name) = UPPER(pt.name)
+ON CONFLICT DO NOTHING""")
+
+            _logger.info(u"OPENFIRE : rattacher aux DI les RDV liés à des SAV")
+            cr.execute("""UPDATE of_planning_intervention opi
+SET service_id = os.id
+FROM project_issue pi
+JOIN of_service os ON os.sav_id = pi.id
+WHERE opi.sav_id = pi.id""")
+
+            _logger.info(u"OPENFIRE : calculer les champs difficiles à traduire en SQL pour les DI nouvellement créées")
+            # calculer l'état des DI nouvellement créées. Leur affecter un numéro. En python car compliqué en SQL
+            service_new = service_obj.search([('sav_id', 'in', sav_sans_di.ids)])
+            service_new._compute_state_poncrec()
+            service_obj._init_number()
+
+            _logger.info(u"OPENFIRE SUCCÈS : migration des SAV dans les DI (T2356)")
+            self.env['ir.config_parameter'].sudo().set_param('of_migration_sav_di', True)
 
     @api.multi
     def _compute_of_a_programmer_count(self):
@@ -171,7 +240,7 @@ class ProjectIssue(models.Model):
 
 
 class ProjectTags(models.Model):
-    _inherit = "project.tags"
+    _inherit = 'project.tags'
 
     @api.model_cr_context
     def _auto_init(self):
@@ -184,19 +253,10 @@ class ProjectTags(models.Model):
         if not self.env['ir.config_parameter'].sudo().get_param('of_migration_sav_di'):
             # Créer les étiquettes de planning. Attention aux doublons!
             # Ne pas créer si il y a une étiquette planning du même nom
+            # on ne rattache pas ici les étiquettes nouvellement crées, on le fera dans la méthode migrer_sav_di
             cr.execute("INSERT INTO of_planning_tag "
                        "(name, color, create_uid, write_uid, create_date, write_date, active) "
                        "SELECT pt.name, pt.color, pt.create_uid, pt.write_uid, pt.create_date, pt.write_date, true "
                        "FROM project_tags AS pt "
                        "WHERE UPPER(pt.name) NOT IN (SELECT UPPER(name) FROM of_planning_tag);")
-            # Connecter les étiquettes de planning aux services existants qui possèdent un SAV
-            # en fonction des étiquettes de projet
-            cr.execute(
-                "INSERT INTO of_service_of_planning_tag_rel (service_id, tag_id) "
-                "SELECT os.id, opt.id "
-                "FROM of_service os "
-                "JOIN project_issue_project_tags_rel pipt ON pipt.project_issue_id = os.sav_id "
-                "JOIN project_tags pt ON pt.id = pipt.project_tags_id "
-                "JOIN of_planning_tag opt ON UPPER(opt.name) = UPPER(pt.name) "
-                "ON CONFLICT DO NOTHING;")
         return res
