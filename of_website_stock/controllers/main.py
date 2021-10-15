5dcb2ef9c8acd7f8496bc1dbd740f5f5815f7179
Archibald JEGO <aj@openfire.fr>
2021-10-15 09:39:53 +0200

    of_website_stock: T2496 - Disponibilité des stocks en ligne of_website_stock: T2649 - Gestion des délai de livraison en ligne

---
+++ of_website_stock/controllers/main.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from odoo import http, fields, SUPERUSER_ID, tools, _
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from datetime import datetime
+
+
+class WebsiteSaleStockNotify(http.Controller):
+
+    @http.route('/website_sale_stock_notify/notify', type='json', auth="public", methods=['POST'], website=True)
+    def notify(self, product_id, email, **post):
+        cr, uid, context = request.cr, request.uid, request.context
+        notification = request.env['of.website.stock.notify']
+        request.env.uid = 1
+        if uid != request.website.user_id.id:
+            user_brw = request.env['res.users'].browse(uid)
+            notification_ids = notification.search([('product_id', '=', int(product_id)), ('email', '=', email)])
+            if not notification_ids:
+                notification.create(
+                    {'partner_id': user_brw.partner_id.id, 'product_id': product_id, 'email': email})
+
+        else:
+            notification_ids = notification.search([('product_id', '=', int(product_id)), ('email', '=', email)])
+            if not notification_ids:
+                notification.create({'product_id': product_id, 'email': email})
+
+        return True
+
+
+class WebsiteSaleDelivery(WebsiteSale):
+
+    @http.route(['/shop/payment'], type='http', auth="public", website=True)
+    def payment(self, **post):
+        order = request.website.sale_get_order()
+        requested_date = post.get('requested_date')
+        if requested_date:
+            requested_date = datetime.strptime(requested_date, '%d/%m/%Y')
+            if order:
+                order.requested_date = requested_date
+                return request.redirect("/shop/payment")
+
+        for line in order.order_line:
+            line.customer_lead = line.product_id.sale_delay
+
+        order._compute_website_commitment_date()
+
+        return super(WebsiteSaleDelivery, self).payment(**post)
---
+++ of_website_stock/models/sale.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api, _
+from datetime import timedelta
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    website_commitment_date = fields.Datetime(string='Website Commitment Date',
+        help="Date by which the products are sure to be delivered. This is a date that you can "
+             "promise to the customer, based on the Product Lead Times.")
+
+    def _compute_website_commitment_date(self):
+        """Compute the website commitment date, only called on /shop/payment from the website"""
+
+        # On récupère le site web
+        website = self.env['website'].search([])[0]
+        of_website_security_lead = float(website.get_of_website_security_lead()) or 0
+
+        for order in self:
+            dates_list = []
+            order_datetime = fields.Datetime.from_string(order.date_order)
+            # Avec le test sur sale_ok, on exclue la ligne qui corrrespond à la livraison
+            for line in order.order_line.filtered(lambda x: x.state != 'cancel' and x.product_id.sale_ok is True):
+
+                # On calcul le product_quantity en fonction de la configuration on_hand/forecast
+                product_quantity = line.product_id.qty_available
+                if website.get_website_config() == 'forecast':
+                    product_quantity += - line.product_id.outgoing_qty + line.product_id.incoming_qty
+
+                # On prend le max entre customer_lead et of_website_security_lead
+                days = max(line.customer_lead, of_website_security_lead) or 0.0
+
+                # Si article indisponible, on rajoute le délai fournisseur et la marge de sécurité
+                if not product_quantity > 0:
+                    days += (website.company_id.security_lead + line.product_id._select_seller(
+                        quantity=line.product_qty, uom_id=line.product_uom).delay) \
+                            or 0.0
+
+                dt = order_datetime + timedelta(days=days)
+                dates_list.append(dt)
+
+            if dates_list:
+                # On détermine la commitment_date en fonction de la picking policy
+                commit_date = min(dates_list) if order.picking_policy == 'direct' else max(dates_list)
+                order.website_commitment_date = fields.Datetime.to_string(commit_date)
+
+    # Add a depends on picking_policy
+    @api.depends('date_order', 'picking_policy', 'order_line.customer_lead')
+    def _compute_commitment_date(self):
+        """Compute the commitment date"""
+        for order in self:
+            dates_list = []
+            order_datetime = fields.Datetime.from_string(order.date_order)
+
+            # Si c'est une commande website, le website_commitment_date est défini et vient remplir le commitment_date
+            if order.website_commitment_date:
+                order.commitment_date = order.website_commitment_date
+            else:
+                for line in order.order_line.filtered(lambda x: x.state != 'cancel'):
+                    dt = order_datetime + timedelta(days=line.customer_lead or 0.0)
+                    dates_list.append(dt)
+                if dates_list:
+                    commit_date = min(dates_list) if order.picking_policy == 'direct' else max(dates_list)
+                    order.commitment_date = fields.Datetime.to_string(commit_date)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.depends('product_id')
+    def _compute_product_id_set_customer_lead(self):
+        self.customer_lead = self.product_id.sale_delay
