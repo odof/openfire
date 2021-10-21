9a42b54d00117299c6da59f1308695f919f55e18
Archibald JEGO <aj@openfire.fr>
2021-10-21 17:23:59 +0200

    of_liste_choix: T2353 - Ajout d'un rapport devis sans prix of_conditionnement: T2353 - Ajout de champs quantité brute et coef pour calculer automatiquement la quantité commandée

--- of_conditionnement/models/of_conditionnement.py
+++ of_conditionnement/models/of_conditionnement.py
@@ -21,6 +21,14 @@ class SaleOrderLine(models.Model):
     of_display_required_units = fields.Boolean(string=u"Affichage des informations de conditionnement")
     of_force_qty = fields.Boolean(string=u"Forcer la quantité")
     of_display_force_qty = fields.Boolean(string=u"Affichage du paramètre pour forcer les quantités")
+    of_packaging_unit_number = fields.Float(
+        string=u"Unité de conditionnement", related='product_id.of_packaging_unit', readonly=True)
+    of_product_qty_brut = fields.Float(
+        string=u"Quantité brute", digits=(12,2),
+        help=u"L'Unité de Quantité(s) commandée(s) doit être le m² pour que cela corresponde")
+    of_coefficient_marge = fields.Float(
+        string=u"Coefficient (en %)", digits=(12,2),
+        help=u"Le coefficient s’applique à la quantité brute pour avoir une marge d’erreur ou de perte.")
 
     @api.multi
     @api.depends('product_id', 'product_uom_qty')
@@ -51,18 +59,63 @@ class SaleOrderLine(models.Model):
         return res
 
     @api.multi
+    @api.onchange('of_packaging_unit_number','of_product_qty_brut','of_coefficient_marge')
+    def _onchange_chantier(self):
+        if not self.of_force_qty:
+            product_uom_qty = 0.0
+            if self.product_id and self.of_packaging_unit_number and self.of_product_qty_brut:
+                product_uom_qty = self.of_packaging_unit_number * self.of_product_qty_brut
+                if self.of_coefficient_marge:
+                    product_uom_qty *= 1 + (self.of_coefficient_marge/100)
+            if product_uom_qty:
+                self.product_uom_qty = product_uom_qty
+
+            # Mise à jour de la description
+            surface_str = u"Quantité brute : %s %s" % (self.of_product_qty_brut, self.product_id.of_packaging_type)
+            coef_str = u"Marge appliquée : %s%s" % (self.of_coefficient_marge, "%")
+            old_desc_lines = self.name and self.name.splitlines() or []
+            new_desc_lines = []
+            for i, desc_line in enumerate(old_desc_lines):
+                if u"Quantité brute : " in desc_line or u"Marge appliquée : " in desc_line:
+                    continue
+                new_desc_lines.append(desc_line)
+            else:
+                new_desc_lines.append(surface_str)
+                new_desc_lines.append(coef_str)
+            self.name = "\n".join(new_desc_lines)
+
+    @api.multi
     @api.onchange('product_uom_qty')
     def _onchange_product_uom_qty(self):
         res = super(SaleOrderLine, self)._onchange_product_uom_qty()
         if self.product_id and self.product_id.of_packaging_unit and self.product_uom_qty:
-            required_units = int(math.ceil(round(self.product_uom_qty / self.product_id.of_packaging_unit, 3)))
-            self.of_display_required_units = True
+
             if not self.of_force_qty:
+                product_uom_qty = 0.0
+                if self.of_product_qty_brut:
+                    product_uom_qty = self.of_packaging_unit_number * self.of_product_qty_brut
+                    if self.of_coefficient_marge:
+                        product_uom_qty *= 1 + (self.of_coefficient_marge/100)
+                if product_uom_qty:
+                    self.product_uom_qty = product_uom_qty
+
+                required_units = int(math.ceil(round(self.product_uom_qty / self.product_id.of_packaging_unit, 3)))
+                self.of_display_required_units = True
                 self.product_uom_qty = required_units * self.product_id.of_packaging_unit
+
+            else:
+                # Si of_force_qty, on supprime les informations de quantités brutes et coef de la description
+                old_desc_lines = self.name and self.name.splitlines() or []
+                new_desc_lines = []
+                for i, desc_line in enumerate(old_desc_lines):
+                    if u"Quantité brute : " in desc_line or u"Marge appliquée : " in desc_line:
+                        continue
+                    new_desc_lines.append(desc_line)
+                self.name = "\n".join(new_desc_lines)
 
             # Mise à jour de la description
             conditionnement_str = u"Conditionnement : " + self.of_packaging_unit
-            desc_lines = self.name.splitlines()
+            desc_lines = self.name and self.name.splitlines() or []
             for i, desc_line in enumerate(desc_lines):
                 if u"Conditionnement : " in desc_line:
                     desc_lines[i] = conditionnement_str
---
+++ of_liste_choix_kit/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": u"OpenFire / Module de lient entre Liste de choix et kits",
+    "version": "10.0.1.0.0",
+    "author": "OpenFire",
+    "description": u"""
+Gestion des listes de choix
+===========================
+- Surcharge pour gérer les kits dans l'impression liste de choix
+""",
+    "website": "www.openfire.fr",
+    "depends": [
+        'of_liste_choix',
+        'of_kit',
+    ],
+    "category": "OpenFire",
+    "data": [
+        'report/of_liste_choix_kit_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
