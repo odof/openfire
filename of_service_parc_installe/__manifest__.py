c973bdf2f7114c9635086b15cc5b2398ff8613f5
Paul <plc@openfire.fr>
2021-11-09 11:14:51 +0100

    of_contract_custom, of_service, etc : T2427 - Déplacement des DI de of_contract_custom vers of_service et corrections diverses

--- of_service_parc_installe/__manifest__.py
+++ of_service_parc_installe/__manifest__.py
@@ -20,9 +20,11 @@ Module de lien entre service, interventions et parc installé
         'of_planning_tournee',
         'of_account',
     ],
-    'data' : [
+    'data': [
+        'data/of_service_parc_installe_data.xml',
         'report/of_service_parc_installe_fiche_intervention.xml',
         'report/of_planning_rapport_intervention.xml',
+        'report/of_service_parc_installe_demande_intervention_templates.xml',
         'views/of_service_parc_installe_view.xml',
         'views/of_planning_intervention_template_views.xml',
         'wizard/of_wizard_invoice_to_parc_installe_view.xml'
