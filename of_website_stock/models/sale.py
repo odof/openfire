ba5c241f80a7ec86a422569d116de258212877d3
Archibald JEGO <aj@openfire.fr>
2021-11-12 16:56:12 +0100

    of_website_stock: T2649 - Correction de bug
    of_website_stoc, of_website_sample_stock: T2649 - Correction pep8, traductions, correction du requested_date dans vue panier paiement
    
    of_website_stock: T2649 - Simplication du calcul de website_commitment_date, traductions

--- of_website_stock/models/sale.py
+++ of_website_stock/models/sale.py
@@ -1,49 +1,57 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api, _
-from datetime import timedelta
+from datetime import datetime, date, timedelta
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    website_commitment_date = fields.Datetime(string='Website Commitment Date',
-        help="Date by which the products are sure to be delivered. This is a date that you can "
-             "promise to the customer, based on the Product Lead Times.")
+    website_commitment_date = fields.Datetime(
+        string='Website Commitment Date', help="Date by which the products are sure to be delivered."
+        "This is a date that you can promise to the customer, based on the Product Lead Times.")
 
     def _compute_website_commitment_date(self):
         """Compute the website commitment date, only called on /shop/payment from the website"""
 
         # On récupère le site web
         website = self.env['website'].search([])[0]
+        of_delivery_management = website.get_of_delivery_management()
         of_website_security_lead = float(website.get_of_website_security_lead()) or 0
+        datetime_now = fields.Datetime.now()
 
         for order in self:
+
+            if not of_delivery_management:
+                order.website_commitment_date = False
+                return
+
             dates_list = []
-            order_datetime = fields.Datetime.from_string(order.date_order)
+
+            # On prend la date max entre la date de la comande et la date d'aujourd'hui
+            if order.date_order:
+                order_datetime = max(
+                    [fields.Datetime.from_string(order.date_order), fields.Datetime.from_string(datetime_now)])
+            else:
+                order_datetime = fields.Datetime.from_string(datetime_now)
+
             # Avec le test sur sale_ok, on exclue la ligne qui corrrespond à la livraison
             for line in order.order_line.filtered(lambda x: x.state != 'cancel' and x.product_id.sale_ok is True):
 
-                # On calcul le product_quantity en fonction de la configuration on_hand/forecast
-                product_quantity = line.product_id.qty_available
-                if website.get_website_config() == 'forecast':
-                    product_quantity += - line.product_id.outgoing_qty + line.product_id.incoming_qty
-
-                # On prend le max entre customer_lead et of_website_security_lead
-                days = max(line.customer_lead, of_website_security_lead) or 0.0
-
-                # Si article indisponible, on rajoute le délai fournisseur et la marge de sécurité
-                if not product_quantity > 0:
-                    days += (website.company_id.security_lead + line.product_id._select_seller(
-                        quantity=line.product_qty, uom_id=line.product_uom).delay) \
-                            or 0.0
-
-                dt = order_datetime + timedelta(days=days)
+                # On récupère la date de livraison pour cette ligne
+                dt = line.get_delivery_date()
                 dates_list.append(dt)
 
             if dates_list:
                 # On détermine la commitment_date en fonction de la picking policy
                 commit_date = min(dates_list) if order.picking_policy == 'direct' else max(dates_list)
                 order.website_commitment_date = fields.Datetime.to_string(commit_date)
+
+                # Si la requested_date n'est plus à jour, on la met à jour
+                if order.requested_date:
+                    order.requested_date = fields.Datetime.to_string(max([
+                        fields.Datetime.from_string(order.requested_date),
+                        fields.Datetime.from_string(order.website_commitment_date)
+                    ]))
 
     # Add a depends on picking_policy
     @api.depends('date_order', 'picking_policy', 'order_line.customer_lead')
@@ -98,6 +106,32 @@ class SaleOrderLine(models.Model):
     @api.depends('product_id')
     def _compute_product_id_set_customer_lead(self):
         self.customer_lead = self.product_id.sale_delay
+
+    def get_quantity_available(self):
+        # On récupère le site web
+        website = self.env['website'].search([])[0]
+
+        # On calcul le product_quantity en fonction de la configuration on_hand/forecast
+        quantity_available = self.product_id.qty_available
+        if website.get_website_config() == 'forecast':
+            quantity_available += - self.product_id.outgoing_qty + self.product_id.incoming_qty
+
+        return quantity_available or 0
+
+    def get_days_of_delay(self):
+        # On récupère le site web
+        website = self.env['website'].search([])[0]
+
+        # On calcul le days_of_delay en fonction de la quantity_available
+        days_of_delay = float(max(self.customer_lead, website.get_of_website_security_lead()) or 0.0)
+        if self.product_uom_qty > self.get_quantity_available():
+            days_of_delay += (website.company_id.security_lead or 0.0) + (self.product_id._select_seller(quantity=self.product_qty, uom_id=self.product_uom).delay or 0.0)
+
+        return days_of_delay or 0.0
+
+    def get_delivery_date(self):
+        # On calcul le delivery_date en fonction du days_of_delay
+        return (datetime.now() + timedelta(days=self.get_days_of_delay()))
 
 
 class ProductTemplate(models.Model):
