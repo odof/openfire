724e2c7e54f8c5a7c2a7e5200e32da9b90abd12b
Archibald JEGO <aj@openfire.fr>
2021-12-03 18:20:01 +0100

    of_sale, of_sale_ofwebsite_sale : T2343 - Corrections diverses

--- of_sale/models/of_sale.py
+++ of_sale/models/of_sale.py
@@ -727,27 +727,27 @@ class Report(models.Model):
         if report_name in allowed_reports:
             # On ajoute au besoin les documents joint
             model = self.env[allowed_reports[report_name]].browse(docids)[0]
+            fd, order_pdf = tempfile.mkstemp()
+            os.write(fd, result)
+            os.close(fd)
+            file_paths = [order_pdf]
+
             mails_data = model._detect_doc_joint()
 
-            if mails_data:
-                fd, order_pdf = tempfile.mkstemp()
-                os.write(fd, result)
+            for mail_data in mails_data:
+                fd, mail_pdf = tempfile.mkstemp()
+                os.write(fd, base64.b64decode(mail_data))
                 os.close(fd)
-                file_paths = [order_pdf]
+                file_paths.append(mail_pdf)
 
-                if report_name == 'sale.report_saleorder':
-                    for attachment in model.order_line.mapped('sale_product_attachment_ids'):
-                        fd, attachment_pdf = tempfile.mkstemp()
-                        os.write(fd, base64.b64decode(attachment.datas))
-                        os.close(fd)
-                        file_paths.append(attachment_pdf)
-
-                for mail_data in mails_data:
-                    fd, mail_pdf = tempfile.mkstemp()
-                    os.write(fd, base64.b64decode(mail_data))
+            if report_name == 'sale.report_saleorder' and self.user_has_groups('of_sale.group_of_sale_print_attachment'):
+                for attachment in model.order_line.mapped('of_product_attachment_ids'):
+                    fd, attachment_pdf = tempfile.mkstemp()
+                    os.write(fd, base64.b64decode(attachment.datas))
                     os.close(fd)
-                    file_paths.append(mail_pdf)
+                    file_paths.append(attachment_pdf)
 
+            if file_paths:
                 result_file_path = self.env['report']._merge_pdf(file_paths)
                 try:
                     result_file = file(result_file_path, "rb")
@@ -845,8 +845,8 @@ class SaleOrderLine(models.Model):
 
     of_date_tarif = fields.Date(string="Date du tarif", related="product_id.date_tarif", readonly=True)
     of_obsolete = fields.Boolean(string=u"Article obsolète", related="product_id.of_obsolete", readonly=True)
-    sale_product_image_ids = fields.Many2many('sale.product.image', string='Images')
-    sale_product_attachment_ids = fields.Many2many("ir.attachment", string="Documents joints")
+    of_product_image_ids = fields.Many2many('of.product.image', string='Images')
+    of_product_attachment_ids = fields.Many2many("ir.attachment", string="Documents joints")
 
     @api.model_cr_context
     def _auto_init(self):
@@ -982,15 +982,15 @@ class SaleOrderLine(models.Model):
                     self.layout_category_id = self.product_id.categ_id.of_layout_id
             if self.env.user.has_group('of_sale.group_of_sale_multiimage'):
                 if self.env.user.has_group('of_sale.group_of_sale_print_multiimage'):
-                    if self.product_id.product_tmpl_id.sale_product_image_ids:
-                        self.sale_product_image_ids = self.product_id.product_tmpl_id.sale_product_image_ids
+                    if self.product_id.product_tmpl_id.of_product_image_ids:
+                        self.of_product_image_ids = self.product_id.product_tmpl_id.of_product_image_ids
                 if self.env.user.has_group('of_sale.group_of_sale_print_attachment'):
                     attachment_ids = self.env['ir.attachment']\
                         .search([('res_model', '=', 'product.template'),
                                  ('res_id', '=', self.product_id.product_tmpl_id.id),
                                  ('mimetype', '=', 'application/pdf')])
                     if attachment_ids:
-                        res['domain']['sale_product_attachment_ids'] = [('id', 'in', attachment_ids.ids)]
+                        res['domain']['of_product_attachment_ids'] = [('id', 'in', attachment_ids.ids)]
 
         return res
 
@@ -1092,6 +1092,15 @@ class SaleOrderLine(models.Model):
                             u"""Veuillez annuler vos modifications.""")
         return super(SaleOrderLine, self).unlink()
 
+    @api.model
+    def create(self, vals):
+        res = super(SaleOrderLine, self).create(vals)
+
+        if 'of_product_image_ids' in vals.keys() and vals['of_product_image_ids'] and not res.of_product_image_ids:
+            res.with_context(already_tried=True).of_product_image_ids = vals['of_product_image_ids']
+
+        return res
+
     @api.multi
     def write(self, vals):
         """
@@ -1107,6 +1116,11 @@ class SaleOrderLine(models.Model):
             locked_invoice_lines = line.mapped('invoice_lines').filtered(lambda l: l.of_is_locked)
             if locked_invoice_lines and blocked and not force:
                 raise UserError(u"""Cette ligne ne peut être modifiée : %s""" % line.name)
+
+        if 'already_tried' not in self._context:
+            if 'of_product_image_ids' in vals.keys() and vals['of_product_image_ids'] and not self.of_product_image_ids:
+                self.with_context(already_tried=True).of_product_image_ids = vals['of_product_image_ids']
+
         return super(SaleOrderLine, self).write(vals)
 
     @api.multi
@@ -1780,7 +1794,7 @@ class PurchaseOrderLine(models.Model):
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    sale_product_image_ids = fields.One2many('sale.product.image', 'product_tmpl_id', string='Sale Images')
+    of_product_image_ids = fields.One2many('of.product.image', 'product_tmpl_id', string=u'Images')
 
     @api.multi
     def action_view_sales(self):
@@ -1841,9 +1855,9 @@ class ProductProduct(models.Model):
         return res
 
 
-class SaleProductImage(models.Model):
-    _name = 'sale.product.image'
-    _description = 'Images to display in Sale Order'
+class OfProductImage(models.Model):
+    _name = 'of.product.image'
+    _description = 'Product Images'
 
     name = fields.Char('Name')
     image = fields.Binary('Image', attachment=True)
