a1c6ff86e86d4a9da8b2a1aa2755f48d63ebb3e4
Stan Heurtault <heusta@openfire.fr>
2021-12-10 12:26:56 +0100

    of_datastore_product : T2106 - Ajout d'un bouton permettant de voir les stocks d'une base fournisseur.
    of_datastore_supplier : T2106
     - Marques : Ajout d'un champ pointant sur un emplacement de stock pour permettre la visualisation sur base cliente.
     - Articles : Ajout d'un champ information de stock.

--- of_datastore_supplier/models/of_datastore_supplier.py
+++ of_datastore_supplier/models/of_datastore_supplier.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class OfProductBrand(models.Model):
@@ -11,8 +11,34 @@ class OfProductBrand(models.Model):
         help=u"Ce champ est à destination des distributeurs et permet de transmettre des informations "
              u"sur l'état des tarifs")
 
+    datastore_location_id = fields.Many2one(
+        comodel_name='stock.location', string="Diffuser le stock", domain="[('usage', '=', 'internal')]")
+    datastore_stock_user_ids = fields.Many2many(comodel_name='res.users', string="Restreindre par client")
+
+    @api.model
+    def of_access_stocks(self, brand_id):
+        current_uid = self.env.uid
+        brand = self.search([('id', '=', brand_id)])
+        if not brand or not brand.datastore_location_id:
+            return False
+        return not brand.datastore_stock_user_ids or current_uid in brand.datastore_stock_user_ids._ids
+
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     of_template_image = fields.Binary(related='product_tmpl_id.image', string=u"Image du modèle")
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    of_stock_informations = fields.Text(string="Informations de stock")
+
+    @api.multi
+    def of_datastore_get_quantities(self):
+        self.ensure_one()
+        product = self.with_context(location=self.brand_id.datastore_location_id.id)
+        return\
+            product.qty_available - product.outgoing_qty,\
+            product.of_stock_informations or "Aucune information"
