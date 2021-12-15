4563523a9dfe531b8aa22ddc2ec1d677e9c97ae7
Archibald JEGO <aj@openfire.fr>
2021-12-15 15:29:40 +0100

    of_mode_paiement: T2763 - Ajout d'une séquence RUM, génération du code RUM avec contrainte d'unicité. Ajout d'une vérification SEPA sur les factures. of_service_mode_paiement: T2763 - Création d'un module intermédiaire entre of_service et of_mode_paiement. Ajout d'une vérification SEPA sur les DI.

--- of_mode_paiement/__manifest__.py
+++ of_mode_paiement/__manifest__.py
@@ -6,24 +6,26 @@
 ##############################################################################
 
 {
-    'name' : "OpenFire / Modes de paiement",
-    'version' : "10.0.1.0.0",
-    'author' : "OpenFire",
-    'website' : "www.openfire.fr",
-    'category' : "Generic Modules/Sales & Purchases",
+    'name': "OpenFire / Modes de paiement",
+    'version': "10.0.1.0.0",
+    'author': "OpenFire",
+    'website': "www.openfire.fr",
+    'category': "Generic Modules/Sales & Purchases",
     'description': """
     - Ajout compte bancaire dans les modes de paiement
     - Règlement factures par LCR et prélèvement SEPA par échange de données informatisées (EDI)
     """,
-    'depends' : [
+    'depends': [
         'account',
         'of_account_payment_mode',
     ],
-    'init_xml' : [
+    'init_xml': [
     ],
-    'data' : [
+    'data': [
+        'data/ir_sequence_data.xml',
         'security/ir.model.access.csv',
         'views/of_mode_paiement_edi_view.xml',
+        'wizard/of_wizard_generer_code_rum_view.xml',
     ],
     'installable': True,
 }
---
+++ of_mode_paiement/wizard/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import of_wizard_generer_code_rum
