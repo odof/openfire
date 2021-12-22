a840a8d0970fb15145e4cdd3b50baa360fa60367
Paul <plc@openfire.fr>
2021-12-22 11:01:17 +0100

    of_service : T2818 - Ajout d'un menu propre aux DI de type "Entretien - Maintenance" et d'un menu de configuration pour les étapes de DI of_service_parc_installe : T2818 - Ajout d'un menu propre aux DI de type "SAV" et d'un champ 'state' sur les étapes de DI pour calcul des temps de traitement des SAV

--- of_service/models/of_service.py
+++ of_service/models/of_service.py
@@ -612,10 +612,12 @@ class OfService(models.Model):
 
     @api.model
     def _read_group_stage_ids(self, stages, domain, order):
-        """
-        Surcharge de la fonction pour afficher toutes les étapes existantes sur vue kanban
-        """
-        return stages.search([], order=order)
+        res = stages.search([], order=order)
+        if self._context.get('of_kanban_steps') == 'Maintenance':
+            ce_type = self.env.ref('of_service.of_service_type_maintenance', raise_if_not_found=False)
+            if ce_type:
+                return res.filtered(lambda s: ce_type.id in s.type_ids.ids)
+        return res
 
     # Actions
 
