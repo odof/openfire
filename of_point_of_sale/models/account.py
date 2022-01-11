9fc0916356b28d521d5643fdeb62d6fd80f2bc28
Archibald JEGO <aj@openfire.fr>
2022-01-11 10:01:43 +0100

    of_point_of_sale : T2845     - Correction de l'affectation des comptes en fonction des taxes pour les commandes sans facture     - Sélection automlatique d'un client lorsqu'on clique sur le bouton facture du PdV

--- of_point_of_sale/models/account.py
+++ of_point_of_sale/models/account.py
@@ -10,6 +10,32 @@ class AccountJournal(models.Model):
         comodel_name='of.account.payment.mode', string=u"Mode de paiement associé au journal pour le point de vente")
 
 
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    @api.model
+    def create(self, values):
+        # Si le context check_account_tax est présent
+        if self._context.get('check_account_tax'):
+
+            # On regarde si un account_id peut être redéfinie par ses taxes
+            if values.get('account_id') and values.get('tax_ids'):
+
+                income_account = values.get('account_id')
+                account = self.env['account.account'].browse(income_account)
+                tax_ids = values.get('tax_ids')
+
+                for tax_id in tax_ids[0][2]:
+                    # On récupère la taxe et le compte
+                    tax = self.env['account.tax'].browse(tax_id)
+
+                    # On regarde si le compte doit être réaffecté à un nouveau compte et on le rajoute dans values
+                    income_account = tax.map_account(account).id
+                    values['account_id'] = income_account
+
+        return super(AccountMoveLine, self).create(values)
+
+
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
---
+++ of_point_of_sale/models/pos_config_settings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class PosConfigSettings(models.TransientModel):
+    _inherit = 'pos.config.settings'
+
+    default_invoice_customer_id = fields.Many2one('res.partner', string=u"Client facture par défaut")
+
+    @api.multi
+    def set_default_default_invoice_customer_id(self):
+        self.env['ir.values'].set_default(
+            'pos.config.settings', 'default_invoice_customer_id',
+            self.default_invoice_customer_id.id)
