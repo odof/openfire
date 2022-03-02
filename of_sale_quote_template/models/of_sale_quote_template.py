3828780d16e32302ae22abad35ece439989feccf
Archibald JEGO <aj@openfire.fr>
2022-03-02 17:45:54 +0100

    Ajustements

--- of_sale_quote_template/models/of_sale_quote_template.py
+++ of_sale_quote_template/models/of_sale_quote_template.py
@@ -686,7 +686,7 @@ class OfSaleOrderLayoutCategory(models.Model):
         model_obj = self.env[model]
         hex_color = model_obj.get_color_section()
         rgb_hex = [hex_color[x:x + 2] for x in [1, 3, 5]]
-        new_rgb_int = [int(hex_value, 16) + (self.depth * 42) for hex_value in rgb_hex]
+        new_rgb_int = [int(hex_value, 16) + ((self.depth - 2) * 35) for hex_value in rgb_hex]
         new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int]  # make sure new values are between 0 and 255
         # hex() produces "0x88", we want just "88"
         return "#" + "".join([hex(i)[2:] for i in new_rgb_int])
