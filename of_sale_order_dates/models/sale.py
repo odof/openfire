936e901120d4af72f616487be62a04389630daba
Paul <plc@openfire.fr>
2022-03-07 15:28:09 +0100

    of_sale_order_dates : T2117 - Correction du calcul du champ "Semaine demandée"

--- of_sale_order_dates/models/sale.py
+++ of_sale_order_dates/models/sale.py
@@ -3,6 +3,7 @@
 from odoo import api, fields, models
 
 from datetime import datetime
+import pytz
 
 
 class SaleOrder(models.Model):
@@ -15,7 +16,11 @@ class SaleOrder(models.Model):
         for order in self:
             if order.requested_date:
                 requested_year = fields.Date.from_string(order.requested_date).year
-                requested_week = datetime.strptime(order.requested_date, "%Y-%m-%d %H:%M:%S").date().isocalendar()[1]
+                requested_date = datetime.strptime(order.requested_date, "%Y-%m-%d %H:%M:%S")
+                requested_date_utc = pytz.utc.localize(requested_date)
+                tz = pytz.timezone(self.env.user.tz or "Europe/Paris")
+                requested_date_tz = requested_date_utc.astimezone(tz)
+                requested_week = requested_date_tz.date().isocalendar()[1]
                 order.of_requested_week = "%s - S%02d" % (requested_year, requested_week)
             else:
                 order.of_requested_week = ""
