7b540ccf307d41d95abd0c3a4796ef4fc7efdf73
Paul <plc@openfire.fr>
2022-03-15 17:52:19 +0100

    of_crm : T3339 - Correction du calcul du reste à facturer des lignes de commande

--- of_crm/models/sale_order.py
+++ of_crm/models/sale_order.py
@@ -160,10 +160,11 @@ class SaleOrderLine(models.Model):
     of_amount_to_invoice = fields.Float(
         string=u"Reste à facturer en €", compute="_compute_of_amount_to_invoice", store=True)
 
-    @api.depends('product_uom_qty', 'qty_invoiced', 'price_unit')
+    @api.depends('product_uom_qty', 'qty_invoiced', 'price_unit', 'discount')
     def _compute_of_amount_to_invoice(self):
         for line in self:
-            line.of_amount_to_invoice = (line.product_uom_qty - line.qty_invoiced) * line.price_unit
+            line.of_amount_to_invoice = (line.product_uom_qty - line.qty_invoiced) * line.price_unit * \
+                (1 - (line.discount or 0.0) / 100.0)
 
 
 class AccountInvoice(models.Model):
