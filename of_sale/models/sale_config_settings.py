4a5d73810b1d7f112e79bca9ce348543c3b06d1e
Maxime JACQUET <mj@openfire.fr>
2022-05-05 17:32:42 +0200

    of_sale, etc. : T2360 - [IMP/ADD/DEL] Disposition des settings de vente
    * of_sale :
    
     - [ADD] Ajout d'un wizard spécifique pour la disposition des champs
       du rapport de la commande client.
     - [DEL/ADD] Suppression des champs pour les paramètres du rapport PDF de
       l'objet/vue de sale.config.settings pour les déplacer dans le
       wizard.
     - [ADD] Ajout de traductions
    
    * of_analytique, of_crm, of_purchase, of_sale, of_sale_stock,
      of_quote_template:
    
     - [IMP/ADD] Modification des xpath, des groupes, des champs,
      des libellés pour réorganiser la vue des Paramètres de vente.
    
     - [ADD/IMP] Également ajouté/mis à jour les fichiers de traductions.
    
    * of_sale_validity : Ajout d'un module de lien spécifique entre
     "sale_validity" et "of_sale" pour modification de la vue de
     configuration.
    
    * of_sale_comment_template :
    
     - Ajout d'un module de lien spécifique entre 'sale_comment_template' et
      'of_sale' car certains développements OF spécifiques sont effectués
      dans un module OCA.
      Et nous avons dû modifier la vue donc nous avons créé le module
      'of_sale_comment_template'.
      À l'avenir, il pourrait être bon que tous les développements
      effectués dans le module OCA soient déplacés dans le nouveau.

--- of_sale/models/sale_config_settings.py
+++ of_sale/models/sale_config_settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 
 
 class OFSaleConfiguration(models.TransientModel):
@@ -54,93 +54,6 @@ class OFSaleConfiguration(models.TransientModel):
         help=u"Afficher les messages d'avertissement de stock ?"
     )
 
-    pdf_display_product_ref_setting = fields.Boolean(
-        string=u"(OF) Réf. produits", required=True, default=False,
-        help=u"Afficher les références produits dans les rapports PDF ?"
-    )
-
-    pdf_date_validite_devis = fields.Boolean(
-        string=u"(OF) Date validité devis", required=True, default=False,
-        help=u"Afficher la date de validité dans le rapport PDF des devis ?"
-    )
-
-    pdf_vt_pastille = fields.Boolean(
-        string=u"(OF) Date VT pastille", required=True, default=False,
-        help=u"Afficher la date de visite technique dans une pastille dans le rapport PDF des devis ?"
-    )
-
-    pdf_hide_global_address_label = fields.Boolean(
-        string=u"(OF) Libellé adresse de livraison et facturation", required=True, default=False,
-        help=u'Masquer le libellé "Adresse de livraison et de facturation" dans le rapport PDF des devis ?')
-
-    pdf_masquer_pastille_commercial = fields.Boolean(
-        string=u"(OF) Masquer pastille commercial", required=True, default=False,
-        help=u"Masquer la pastille commercial dans les rapports PDF ?"
-    )
-
-    pdf_mail_commercial = fields.Boolean(
-        string=u"(OF) pastille commercial", required=True, default=False,
-        help=u"Afficher l'email dans la pastille commercial des rapports PDF ?"
-    )
-
-    pdf_masquer_pastille_payment_term = fields.Boolean(
-        string=u"(OF) Masquer pastille conditions de règlement", required=True, default=False,
-        help=u"Masquer la pastille conditions de règlement dans les rapports PDF ?"
-    )
-
-    pdf_adresse_nom_parent = fields.Boolean(
-        string=u"(OF) Nom parent contact", required=True, default=False,
-        help=u"Afficher le nom du 'parent' du contact au lieu du nom du contact dans les rapport PDF ?"
-    )
-    pdf_adresse_civilite = fields.Boolean(
-        string=u"(OF) Civilités", required=True, default=False,
-        help=u"Afficher la civilité dans les rapport PDF ?"
-    )
-    pdf_adresse_telephone = fields.Selection(
-        [
-            (1, u"Afficher dans l'encart d'adresse principal"),
-            (2, u"Afficher dans une pastille d'informations complémentaires"),
-            (3, u"Afficher dans l'encart d'adresse principal et dans une pastille d'informations complémentaires")
-        ], string=u"(OF) Téléphone",
-        help=u"Où afficher le numéro de téléphone dans les rapport PDF ? Ne rien mettre pour ne pas afficher."
-    )
-    pdf_adresse_mobile = fields.Selection(
-        [
-            (1, u"Afficher dans l'encart d'adresse principal"),
-            (2, u"Afficher dans une pastille d'informations complémentaires"),
-            (3, u"Afficher dans l'encart d'adresse principal et dans une pastille d'informations complémentaires")
-        ], string=u"(OF) Mobile",
-        help=u"Où afficher le numéro de téléphone mobile dans les rapport PDF ? Ne rien mettre pour ne pas afficher."
-    )
-    pdf_adresse_fax = fields.Selection(
-        [
-            (1, u"Afficher dans l'encart d'adresse principal"),
-            (2, u"Afficher dans une pastille d'informations complémentaires"),
-            (3, u"Afficher dans l'encart d'adresse principal et dans une pastille d'informations complémentaires")
-        ], string="(OF) Fax",
-        help=u"Où afficher le fax dans les rapport PDF ? Ne rien mettre pour ne pas afficher."
-    )
-    pdf_adresse_email = fields.Selection(
-        [
-            (1, u"Afficher dans l'encart d'adresse principal"),
-            (2, u"Afficher dans une pastille d'informations complémentaires"),
-            (3, u"Afficher dans l'encart d'adresse principal et dans une pastille d'informations complémentaires")
-        ], string="(OF) E-mail",
-        help=u"Où afficher l'adresse email dans les rapport PDF ? Ne rien mettre pour ne pas afficher."
-    )
-    pdf_afficher_multi_echeances = fields.Boolean(
-        string=u"(OF) Multi-échéances", required=True, default=False,
-        help=u"Afficher les échéances multiples dans les rapports PDF ?"
-    )
-    of_color_bg_section = fields.Char(
-        string="(OF) Couleur fond titres section",
-        help=u"Choisissez un couleur de fond pour les titres de section", default="#F0F0F0"
-    )
-    of_color_font = fields.Char(
-        string="(OF) Couleur police titre section",
-        help=u"Choisissez un couleur pour les titres de section", default="#000000"
-    )
-
     of_position_fiscale = fields.Boolean(string="(OF) Position fiscale")
     of_allow_quote_addition = fields.Boolean(string=u"(OF) Devis complémentaires")
 
@@ -155,15 +68,14 @@ class OFSaleConfiguration(models.TransientModel):
 
     group_of_sale_multiimage = fields.Selection([
         (0, 'One image per product'),
-        (1, 'Several images per product')
-        ], string='(OF) Multi Images', implied_group='of_sale.group_of_sale_multiimage',
+        (1, 'Several images per product')],
+        string='(OF) Multi Images', implied_group='of_sale.group_of_sale_multiimage',
         group='base.group_portal,base.group_user,base.group_public')
 
     of_sale_print_multiimage_level = fields.Selection([
         (0, 'Do not print'),
         (1, 'Print on each line'),
-        (2, 'Print on appendix')
-        ], string='(OF) Print product images on Sale Order')
+        (2, 'Print on appendix')], string='(OF) Print product images on Sale Order')
     group_of_sale_print_one_image = fields.Boolean(
         'Print on each line', implied_group='of_sale.group_of_sale_print_one_image',
         group='base.group_portal,base.group_user,base.group_public')
@@ -173,57 +85,22 @@ class OFSaleConfiguration(models.TransientModel):
 
     group_of_sale_print_attachment = fields.Selection([
         (0, 'Do not print'),
-        (1, 'Print on appendix')
-        ], string='(OF) Print product attachments on Sale Order',
+        (1, 'Print on appendix')], string='(OF) Print product attachments on Sale Order',
         implied_group='of_sale.group_of_sale_print_attachment',
         group='base.group_portal,base.group_user,base.group_public')
 
     of_invoice_grouped = fields.Selection(selection=[
         (0, 'Groupement par partenaire + devise'),
-        (1, 'Groupement par commande'),
-        ], string=u"(OF) Facturation groupée")
+        (1, 'Groupement par commande'), ], string=u"(OF) Facturation groupée")
 
     sale_show_tax = fields.Selection(selection_add=[('both', 'Afficher les sous-totaux HT (B2B) et TTC (B2C)')])
 
-    of_pdf_taxes_display = fields.Boolean(
-        string=u"(OF) Détails des taxes", help=u"Afficher le tableau de détail des taxes dans les rapports PDF")
-
     of_propagate_payment_term = fields.Boolean(
-        string=u"(OF) Propager les conditions de règlement dans la facture",
+        string=u"(OF) Terms of payment",
         help=u"Si décoché, les conditions de règlement ne sont pas propagées aux factures", default=True)
 
     of_sale_order_margin_control = fields.Boolean(
         string=u"(OF) Contrôle de marge", help=u"Activer le contrôle de marge à la validation des commandes")
-
-    @api.multi
-    def set_pdf_adresse_nom_parent_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_adresse_nom_parent', self.pdf_adresse_nom_parent)
-
-    @api.multi
-    def set_pdf_adresse_civilite_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_adresse_civilite', self.pdf_adresse_civilite)
-
-    @api.multi
-    def set_pdf_adresse_telephone_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_adresse_telephone', self.pdf_adresse_telephone)
-
-    @api.multi
-    def set_pdf_adresse_mobile_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_adresse_mobile', self.pdf_adresse_mobile)
-
-    @api.multi
-    def set_pdf_adresse_fax_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_adresse_fax', self.pdf_adresse_fax)
-
-    @api.multi
-    def set_pdf_adresse_email_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_adresse_email', self.pdf_adresse_email)
 
     @api.multi
     def set_stock_warning_defaults(self):
@@ -231,59 +108,9 @@ class OFSaleConfiguration(models.TransientModel):
             'sale.config.settings', 'stock_warning_setting', self.stock_warning_setting)
 
     @api.multi
-    def set_pdf_display_product_ref_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_display_product_ref_setting', self.pdf_display_product_ref_setting)
-
-    @api.multi
-    def set_pdf_date_validite_devis_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_date_validite_devis', self.pdf_date_validite_devis)
-
-    @api.multi
-    def set_pdf_vt_pastille_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_vt_pastille', self.pdf_vt_pastille)
-
-    @api.multi
-    def set_pdf_hide_global_address_label_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_hide_global_address_label', self.pdf_hide_global_address_label)
-
-    @api.multi
-    def set_pdf_masquer_pastille_commercial(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_masquer_pastille_commercial', self.pdf_masquer_pastille_commercial)
-
-    @api.multi
-    def set_pdf_mail_commercial_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_mail_commercial', self.pdf_mail_commercial)
-
-    @api.multi
-    def set_pdf_masquer_pastille_payment_term(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_masquer_pastille_payment_term', self.pdf_masquer_pastille_payment_term)
-
-    @api.multi
     def set_of_deposit_product_categ_id_defaults(self):
         return self.env['ir.values'].sudo().set_default(
             'sale.config.settings', 'of_deposit_product_categ_id_setting', self.of_deposit_product_categ_id_setting.id)
-
-    @api.multi
-    def set_of_color_bg_section_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'of_color_bg_section', self.of_color_bg_section)
-
-    @api.multi
-    def set_of_color_font_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'of_color_font', self.of_color_font)
-
-    @api.multi
-    def set_pdf_afficher_multi_echeances_defaults(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'pdf_afficher_multi_echeances', self.pdf_afficher_multi_echeances)
 
     @api.multi
     def set_of_position_fiscale(self):
@@ -321,22 +148,17 @@ class OFSaleConfiguration(models.TransientModel):
             self.update({
                 'group_show_price_total': False,
                 'group_show_price_subtotal': True,
-                })
+            })
         elif self.sale_show_tax == "total":
             self.update({
                 'group_show_price_total': True,
                 'group_show_price_subtotal': False,
-                })
+            })
         else:
             self.update({
                 'group_show_price_total': True,
                 'group_show_price_subtotal': True,
-                })
-
-    @api.multi
-    def set_of_pdf_taxes_display(self):
-        return self.env['ir.values'].sudo().set_default(
-            'sale.config.settings', 'of_pdf_taxes_display', self.of_pdf_taxes_display)
+            })
 
     @api.multi
     def set_of_propagate_payment_term(self):
@@ -347,3 +169,13 @@ class OFSaleConfiguration(models.TransientModel):
     def set_of_sale_order_margin_control(self):
         return self.env['ir.values'].sudo().set_default(
             'sale.config.settings', 'of_sale_order_margin_control', self.of_sale_order_margin_control)
+
+    @api.multi
+    def action_printings_params(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'of.sale.wizard.set.printing.params',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'target': 'new'
+        }
