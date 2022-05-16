8f172018b58ba4ad77006b750750179e6c4e38ee
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2022-05-16 10:44:05 +0200

    of_service : T3517 - [FIX] Erreur bloquante à la planification d'intervention.

--- of_service/models/of_service.py
+++ of_service/models/of_service.py
@@ -1469,7 +1469,7 @@ class OFPlanningIntervention(models.Model):
                     line_vals.append((0, 0, line.prepare_intervention_line_vals()))
                 self.line_ids = line_vals
             # self._origin contient les valeurs de l'enregistrement en BDD
-            if self._origin and self._origin.service_id \
+            if hasattr(self, '_origin') and self._origin.service_id \
                     and (self._origin.service_id.note or u"") in (self._origin.description_interne or u""):
                 # Si la description de l'ancienne DI est encore présente, la retirer
                 descritab = self._origin.description_interne.split(self._origin.service_id.note or u"")
