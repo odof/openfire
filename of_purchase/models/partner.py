f0d75b424638a490b7c0370f39eab84eaeb84163
Maxime JACQUET <mj@openfire.fr>
2022-06-16 16:28:57 +0200

    of_purchase : T3701 - [CLEAN/DEL]
    Sépération des classes en plusieurs fichiers
    Retrait du _auto_init sur les coefficiant d'achat
     "property_of_purchase_coeff"

---
+++ of_purchase/models/partner.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    of_br_count = fields.Integer(string=u"Réceptions", compute="_compute_of_br_count")
+
+    def _compute_of_br_count(self):
+        picking_obj = self.env['stock.picking']
+        for partner in self:
+            partner.of_br_count = picking_obj.search_count(
+                [('of_customer_id', '=', partner.id), ('of_location_usage', '=', 'supplier')])
+
+    @api.multi
+    def _purchase_invoice_count(self):
+        super(ResPartner, self)._purchase_invoice_count()
+        all_partners = self.search([('id', 'child_of', self.ids)])
+        all_partners.read(['parent_id'])
+
+        # On réapplique la deuxième étape de la fonction pour ajouter les avoir fournisseurs
+        supplier_invoice_groups = self.env['account.invoice'].read_group(
+            domain=[('partner_id', 'in', all_partners.ids),
+                    ('type', '=', 'in_refund')],
+            fields=['partner_id'], groupby=['partner_id']
+        )
+        for group in supplier_invoice_groups:
+            partner = self.browse(group['partner_id'][0])
+            while partner:
+                if partner in self:
+                    partner.supplier_invoice_count += group['partner_id_count']
+                partner = partner.parent_id
+
+    @api.multi
+    def action_view_picking(self):
+        action = self.env.ref('of_purchase.of_purchase_open_picking').read()[0]
+        action['domain'] = [('of_customer_id', 'in', self._ids)]
+        return action
---
+++ of_purchase/models/stock.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    of_customer_id = fields.Many2one('res.partner', string="Client")
+    of_customer_shipping_id = fields.Many2one('res.partner', string=u"Adresse de Livraison du Client")
+    of_customer_shipping_city = fields.Char(
+        related='of_customer_shipping_id.city', string=u"Ville", store=True, readonly=True)
+    of_customer_shipping_zip = fields.Char(
+        related='of_customer_shipping_id.zip', string=u"Code Postal", store=True, readonly=True)
+    partner_shipping_id = fields.Many2one('res.partner', string=u"Adresse de Livraison du Partenaire")
+    partner_shipping_city = fields.Char(
+        related='partner_shipping_id.city', string=u"Ville", store=True, readonly=True)
+    partner_shipping_zip = fields.Char(
+        related='partner_shipping_id.zip', string=u"Code Postal", store=True, readonly=True)
+    # Permet de cacher le champ of_customer_id si pas sur BR
+    of_location_usage = fields.Selection(related="location_id.usage")
+    of_user_id = fields.Many2one(comodel_name='res.users', string="Responsable technique")
+
+    @api.onchange('of_customer_id')
+    def _onchange_of_customer_id(self):
+        self.ensure_one()
+        addresses = self.of_customer_id.address_get(['delivery'])
+        self.of_customer_shipping_id = addresses['delivery']
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        self.ensure_one()
+        addresses = self.partner_id.address_get(['delivery'])
+        self.partner_shipping_id = addresses['delivery']
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _get_new_picking_values(self):
+        res = super(StockMove, self)._get_new_picking_values()
+        if isinstance(res, dict):
+            responsable = self.procurement_id.sale_line_id.order_id.of_user_id
+            if responsable:
+                res['of_user_id'] = responsable.id
+        return res
