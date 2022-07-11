614ee02632564d69302ac21e96703266dc8be258
Archibald JEGO <aj@openfire.fr>
2022-07-11 16:07:00 +0200

    of_website_sale: T4031 - [FIX] Renvoi un message d'erreur lorsqu'on essaye de publier un article avec ID négatif (article TC) sur le site web

--- of_website_sale/models/product.py
+++ of_website_sale/models/product.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     website_name = fields.Char(string=u"Dénomination site web")
+
+    @api.multi
+    def website_publish_button(self):
+        self.ensure_one()
+        if self.id < 0:
+            raise UserError(u"Vous ne pouvez publier un article centralisé qui n'est pas importé, "
+                            u"importez l'article avant de le publier sur le site internet")
+        else:
+            return super(ProductTemplate, self).website_publish_button()
