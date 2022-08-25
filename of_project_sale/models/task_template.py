f36294b1d9a28e910d33f9db78068e9fa69929a4
Archibald JEGO <aj@openfire.fr>
2022-08-25 12:19:18 +0200

    of_project, of_project_sale: T4375 - [FIX] Reprise de champs lors de la création d'un RDV depuis la tâche of_project_sale: T4375 - [IMP] Ajout d'un champ Assigné à dans le modèle de tâche

--- of_project_sale/models/task_template.py
+++ of_project_sale/models/task_template.py
@@ -1,12 +1,25 @@
 # -*- coding: utf8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
+from odoo.tools.safe_eval import safe_eval
 
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     of_planning_tache_id = fields.Many2one(comodel_name='of.planning.tache', string=u"Tâche du RDV")
+
+    @api.multi
+    def action_view_interventions(self):
+        res = super(ProjectTask, self).action_view_interventions()
+        if len(self._ids) == 1 and self.of_planning_tache_id:
+            context = safe_eval(res['context'])
+            context.update({
+                'default_tache_id': self.of_planning_tache_id.id,
+            })
+            res['context'] = context
+        return res
 
 
 class OFProjectTaskTemplate(models.Model):
@@ -17,3 +30,4 @@ class OFProjectTaskTemplate(models.Model):
     duration = fields.Float(string=u"Durée", digits=(6, 2))
     product_tmpl_id = fields.Many2one(comodel_name='product.template', string=u"Article", index=True)
     planning_tache_id = fields.Many2one(comodel_name='of.planning.tache', string=u"Tâche du RDV")
+    user_id = fields.Many2one(comodel_name='res.users', string=u"Assigné à")
