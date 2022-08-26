ddab03c4a2778ff8f3e1cfecedb984cb41117869
Archibald JEGO <aj@openfire.fr>
2022-08-26 09:26:23 +0200

    of_stock: T4236 - [IMP] Amélioration de la remise à 0 des quantités de l'inventaire

--- of_stock/__manifest__.py
+++ of_stock/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "OpenFire / Stock",
-    "version": "10.0.1.0.0",
-    "author": "OpenFire",
+    'name': u"OpenFire / Stock",
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
-    'category': 'OpenFire',
-    "description": """
+    'author': u"OpenFire",
+    'category': u"OpenFire",
+    'website': u"www.openfire.fr",
+    'description': u"""
 Extension OpenFire du module stock
 ==================================
 
@@ -14,7 +15,6 @@ Extension OpenFire du module stock
 - Ajout de l'id (non modifiable) et d'un champ note dans les lignes d'inventaire.
 - Édition des lignes d'inventaire par le haut.
 """,
-    "website": "www.openfire.fr",
     "depends": [
         "stock",
         "of_external",
@@ -27,6 +27,7 @@ Extension OpenFire du module stock
         'wizards/stock_backorder_confirmation_views.xml',
         'wizards/stock_immediate_transfer_views.xml',
         'wizards/of_specific_delivery_report_wizard_views.xml',
+        'wizards/of_stock_inventory_reset_qty.xml',
         'reports/of_specific_delivery_report.xml',
         'reports/of_valued_delivery_report.xml',
     ],
