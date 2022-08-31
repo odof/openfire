36587cac0260c91610d01895b2d0bd324557e210
Archibald JEGO <aj@openfire.fr>
2022-08-31 16:11:41 +0200

    of_sale_quote_template: T2946 - [IMP] Actualisation de la description des lignes de devis lors de la modification d'un article

--- of_sale_quote_template/models/__init__.py
+++ of_sale_quote_template/models/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import of_sale_quote_tmpl_activity
 from . import of_sale_quote_template
 from . import sale_config_settings
+from . import product
