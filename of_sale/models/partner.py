12ec8088e10be8ea0f2ac6ce85e296cfa9714a69
Archibald JEGO <aj@openfire.fr>
2022-09-06 15:04:11 +0200

    of_sale, of_purchase, of_crm: T4217 - [IMP] Prise en compte des contacts archibés dans le calcul du nombre de CC/CF lié à un contact of_sale_custom_workflow: T4217 - [IMP] Prise en compte des commandes clôturées dans le nombre de commande partenaire

--- of_sale/models/partner.py
+++ of_sale/models/partner.py
@@ -48,3 +48,19 @@ class ResPartner(models.Model):
             partner.of_is_warn = True
         partners_left = self - has_warn
         super(ResPartner, partners_left)._compute_of_is_warn()
+
+    def _compute_sale_order_count(self):
+        # retrieve all children partners and prefetch 'parent_id' on them
+        all_partners = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
+        all_partners.read(['parent_id'])
+
+        sale_order_groups = self.env['sale.order'].read_group(
+            domain=[('partner_id', 'in', all_partners.ids)],
+            fields=['partner_id'], groupby=['partner_id']
+        )
+        for group in sale_order_groups:
+            partner = self.browse(group['partner_id'][0])
+            while partner:
+                if partner in self:
+                    partner.sale_order_count += group['partner_id_count']
+                partner = partner.parent_id
