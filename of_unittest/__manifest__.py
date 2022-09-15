ea8869665f287e6bb70c1a1f78f22234320fdc07
Maxime JACQUET <mj@openfire.fr>
2022-09-15 09:26:10 +0200

    Base module pour les tests

---
+++ of_unittest/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'OpenFire / Tests unitaires',
+    'version': '10.0.1.0.0',
+    'author': 'OpenFire',
+    'license': 'AGPL-3',
+    'website': 'www.openfire.fr',
+    'category': 'Generic Modules/Formation Tests unitaires',
+    'description': u"""
+Module de démo pour la formation sur les tests unitaires
+""",
+    'depends': [
+        'of_base',
+        'of_sale',
+        'of_crm',
+    ],
+    'demo_xml': [],
+    'data': [],
+    'installable': True,
+}
---
+++ of_unittest/models/sale.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from datetime import datetime
+from odoo import api, models, fields
+
+
+def is_weekday(order_date):
+    return (0 <= order_date.weekday() < 5)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    weekday_sms_sent = fields.Boolean(string='Weekday SMS sent')
+
+    @api.multi
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        for rec in self:
+            rec.weekday_sms_sent = bool(is_weekday(datetime.now()))
+        return res
