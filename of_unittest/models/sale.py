e21ee8e9687b726e6dcdf93382bec19cc7c976a5
Maxime JACQUET <mj@openfire.fr>
2022-09-15 09:41:49 +0200

    Ajout external service confirmation commande

--- of_unittest/models/sale.py
+++ of_unittest/models/sale.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import time
 from datetime import datetime
 from odoo import api, models, fields
 
@@ -17,6 +18,15 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         """ Surcharge de la fonction d'origine pour ajouter un appel à un service web sensible. """
         res = super(SaleOrder, self).action_confirm()
+        self._send_sale_to_external_service()
         for rec in self:
             rec.weekday_sms_sent = bool(is_weekday(datetime.now()))
         return res
+
+    @api.multi
+    def _send_sale_to_external_service(self):
+        """ Envoie les informations de la commande à un service web externe. """
+        for rec in self:
+            for i in range(0, 5):
+                print "Attention, je suis une fonction sensible à ne pas appeler dans un test unitaire"
+                time.sleep(1)
