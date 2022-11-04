03d815401790b8bae29c48716fb1608247824ee4
Gaël <fragae@openfire.fr>
2022-11-04 15:02:49 +0100

    of_kit, of_sale_quote_template_kit : T4540 - [CLEAN] mise aux normes des fichiers pythons

--- of_sale_quote_template_kit/__manifest__.py
+++ of_sale_quote_template_kit/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 {
-    "name": "OpenFire / Modèle de devis avec kit",
-    "version": "10.0.1.0.0",
-    "author": "OpenFire",
+    'name': u"OpenFire / Modèle de devis avec kit",
+    'version': '10.0.1.0.0',
+    'author': u"OpenFire",
     'license': '',
-    "category": "OpenFire",
-    "description": u"""
+    'category': u"OpenFire",
+    'description': u"""
 Modèle de devis OpenFire
 ========================
 Ce module modifie les modèles de devis OpenFire pour fonctionner avec les kits.
@@ -16,12 +16,12 @@ Fonctionnalités
 ----------------
  - Ajout de champs dans les lignes de modèles pour fonctionner avec les kits
 """,
-    "website": "www.openfire.fr",
-    "depends": [
-        "of_sale_quote_template",
-        "of_kit",
+    'website': 'www.openfire.fr',
+    'depends': [
+        'of_sale_quote_template',
+        'of_kit',
     ],
-    "data": [
+    'data': [
         'views/of_sale_quote_template_views_kit.xml',
     ],
     'installable': True,
