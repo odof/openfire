49775d52cbffcb541eb507e8d0cccdf5c99abb9d
Stanislav HEURTAULT <sh@openfire.fr>
2022-11-04 13:34:19 +0000

    10.0-sh-of_base : T4606 - [IMP] Cacher les menus liés au groupe spécial caché.

---
+++ of_base/models/ir_ui_menu.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# 1: imports of python lib
+# 2: imports of odoo
+from odoo import api, models, SUPERUSER_ID
+# 3: imports from odoo modules
+# 4: local imports
+# 5: Import of unknown third party lib
+
+
+class IrUiMenu(models.Model):
+    _inherit = 'ir.ui.menu'
+
+    @api.model
+    def _visible_menu_ids(self, debug=False):
+        hidden_group = self.env.ref('of_base.of_group_hidden', raise_if_not_found=False)
+        menus_ids = super(IrUiMenu, self)._visible_menu_ids(debug=debug)
+        if hidden_group and self._uid != SUPERUSER_ID:
+            # remove all menus that are specified in the group's menu items
+            return menus_ids - set(hidden_group.menu_access.ids)
+        return menus_ids
