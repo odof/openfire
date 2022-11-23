26bab425b21be9a801b5b7e018652d25dcb661bc
Maxime JACQUET <mj@openfire.fr>
2022-11-23 15:41:56 +0100

    of_parc_installe : T4615 - [IMP] Nom du produit parcs installés
    Mise en place d'une option pour raccourcir le nom des produits affichés
     lors de la création d'un parc installé depuis la commande client

--- of_parc_installe/models/product_product.py
+++ of_parc_installe/models/product_product.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 from odoo import models, api
 
+LIMIT_CHAR = 120
+
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     @api.multi
     def name_get(self):
-        if self._context.get('show_only_name', False):
-            result = []
-            for product in self:
-                result.append((product.id, '%s' % product.name))
-            return result
-        return super(ProductProduct, self).name_get()
+        products_names = super(ProductProduct, self).name_get()
+        if not self._context.get('show_shorten_name', False):
+            return products_names
+        limit_char = self._context.get('show_shorten_name', LIMIT_CHAR)
+        if limit_char > 0:
+            return [(product_name[0], '%s...' % product_name[1][:int(limit_char)]) for product_name in products_names]
+        return products_names
