61e9444417f5ed2312d374442b7d2f88b78feed4
Maxime JACQUET <mj@openfire.fr>
2022-11-30 12:54:07 +0100

    of_sale: T4906 - [ADD/IMP] Signature vendeur sur devis

---
+++ of_sale/models/res_users.py
@@ -0,0 +1,11 @@
+
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    of_vendor_signature = fields.Binary(string="Salesman signature in sales report")
