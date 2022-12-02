1bad278e61c7b9b746d37280cf8d39cbcedd1499
Willy MERIEAU <wm@openfire.fr>
2022-12-02 15:48:46 +0000

    of_planning : T4627 - [IMP] RDV totalement facturé sans facturation

--- of_planning/__manifest__.py
+++ of_planning/__manifest__.py
@@ -14,7 +14,7 @@
 
 {
     'name': u"OpenFire / Planning",
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'author': u"OpenFire",
     'website': u"www.openfire.fr",
     'category': u"Generic Modules",
