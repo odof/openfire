a3aeb27993dfe09931578ae54f5b482a8b36f24c
Maxime JACQUET <mj@openfire.fr>
2022-12-28 14:53:51 +0100

    of_sale_commission : T5297 - Cleaning

--- of_sale_commission/models/of_sale_profcommi.py
+++ of_sale_commission/models/of_sale_profcommi.py
@@ -1,44 +1,21 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api, SUPERUSER_ID
-from odoo.exceptions import UserError
-
-
-class OFSaleRegcommi(models.Model):
-    _name = "of.sale.regcommi"
-    _description = u"Règles de commissions"
-
-    name = fields.Char(string=u"Libellé")
-    code = fields.Text(string="Code")
-
-    _sql_constraints = [
-        ('name_id_unique', 'unique (name)', u"Le libellé est déjà utilisé !"),
-    ]
-
-    @api.model
-    def create(self, vals):
-        if self._uid != SUPERUSER_ID:
-            raise UserError(u"Seul l'administrateur a le droit de créer des règles !")
-        return super(OFSaleRegcommi, self).create(vals)
-
-    @api.multi
-    def write(self, vals):
-        if self._uid != SUPERUSER_ID:
-            raise UserError(u"Seul l'administrateur a le droit de modifier des règles !")
-        return super(OFSaleRegcommi, self).write(vals)
+from odoo import models, fields, api
 
 
 class OFSaleProfcommi(models.Model):
-    _name = "of.sale.profcommi"
-    _description = "Profils de commissions"
+    _name = 'of.sale.profcommi'
+    _description = u"Profils de commissions"
 
-    name = fields.Char(string="Profil")
-    taux_commi = fields.Float(string="Taux global", digits=(16, 2), default=4)
-    taux_acompte = fields.Float(string="Indice acompte", default=50)
+    name = fields.Char(string=u"Profil")
+    taux_commi = fields.Float(string=u"Taux global", digits=(16, 2), default=4)
+    taux_acompte = fields.Float(string=u"Indice acompte", default=50)
     taux_solde = fields.Float(string=u"Indice règlement", compute='_compute_taux_solde')
-    profcommi_line_ids = fields.One2many('of.sale.profcommi.line', 'profcommi_id', string="Commissions")
-    user_ids = fields.One2many('res.users', 'of_profcommi_id', string="Utilisateurs", readonly=True)
+    profcommi_line_ids = fields.One2many(
+        comodel_name='of.sale.profcommi.line', inverse_name='profcommi_id', string=u"Commissions")
+    user_ids = fields.One2many(
+        comodel_name='res.users', inverse_name='of_profcommi_id', string=u"Utilisateurs", readonly=True)
 
     @api.depends('taux_acompte')
     def _compute_taux_solde(self):
@@ -51,13 +28,13 @@ class OFSaleProfcommi(models.Model):
 
     @api.multi
     def get_taux_commi(self, line):
-        """
+        u"""
         Recupere le taux de commissionnement pour un profil de commission et une ligne
         type definit le type de ligne : 'acompte' pour order_line, 'solde' ou 'avoir' pour invoice_line
         """
         self.ensure_one()
         taux = self.taux_commi
-        product = line.product_id  # A utiliser dans les règles de commissionnement (regcommi_id.code)
+        product = line.product_id  # noqa: F841. A utiliser dans les règles de commissionnement (regcommi_id.code).
         cond = False
         for prof_line in self.profcommi_line_ids:
             if prof_line.type == 'commission':
@@ -69,16 +46,16 @@ class OFSaleProfcommi(models.Model):
 
 
 class OFSaleProfcommiLine(models.Model):
-    _name = "of.sale.profcommi.line"
+    _name = 'of.sale.profcommi.line'
     _description = "Ligne de Profil Commissions"
-    _order = "type,sequence"
+    _order = 'type, sequence'
 
     name = fields.Char(string=u"Libellé", required=True)
-    profcommi_id = fields.Many2one('of.sale.profcommi', string="Profil", ondelete='cascade')
-    regcommi_id = fields.Many2one('of.sale.regcommi', string=u"Règle", required=True, ondelete='restrict')
+    profcommi_id = fields.Many2one(comodel_name='of.sale.profcommi', string=u"Profil", ondelete='cascade')
+    regcommi_id = fields.Many2one(comodel_name='of.sale.regcommi', string=u"Règle", required=True, ondelete='restrict')
     sequence = fields.Integer(string=u"Séquence")
-    taux_commi = fields.Float(string="Pourcentage", digits=(16, 2), default=4)
+    taux_commi = fields.Float(string=u"Pourcentage", digits=(16, 2), default=4)
     type = fields.Selection(
-        [('commission', "Taux de Commission"), ('acompte', "Premier Versement")], string="Type", required=True,
+        [('commission', u"Taux de Commission"), ('acompte', u"Premier Versement")], string=u"Type", required=True,
         default='commission'
     )
