bfdef0f46fe4b1d721f6ce12177cdb7a600fd732
Willy Merieau <wm@openfire.fr>
2023-01-11 13:43:51 +0100

    of_sale_type : T6008 - [IMP] Type de devis dans les lignes de CC et FC

--- of_sale_type/models/account.py
+++ of_sale_type/models/account.py
@@ -9,6 +9,12 @@ class AccountInvoice(models.Model):
 
     of_sale_type_id = fields.Many2one(comodel_name='of.sale.type', string="Sale order type")
 
+    class AccountInvoiceLine(models.Model):
+        _inherit = 'account.invoice.line'
+
+        of_sale_type_id = fields.Many2one(related='invoice_id.of_sale_type_id', string="Sale order type", readonly=True,
+                                          store=True)
+
     def _get_refund_common_fields(self):
         common_fields = ['of_sale_type_id']
         return (super(AccountInvoice, self)._get_refund_common_fields() or []) + common_fields
