e394cba47e6aed499b40b8470ff3e0bced559e74
Stan Heurtault <heusta@openfire.fr>
2023-01-24 10:44:23 +0100

    of_document : T5247 - [IMP] Modification du domaine pour le smart button Documents des sale.order

--- of_document/models/sale.py
+++ of_document/models/sale.py
@@ -7,24 +7,31 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    of_dms_file_count = fields.Integer(string="Documents", compute='_compute_dms_file')
+    of_dms_file_count = fields.Integer(string="Documents", compute='_compute_of_dms_file_count')
 
     @api.multi
-    def _compute_dms_file(self):
+    def get_dms_file_domain(self):
+        self.ensure_one()
+        return [
+            '|', '|', '&', ('of_related_model', '=', 'sale.order'), ('of_related_id', '=', self.id),
+            '&', ('of_related_model', '=', 'account.invoice'), ('of_related_id', 'in', self.invoice_ids.ids),
+            '&', ('of_related_model', '=', 'of.planning.intervention'),
+            ('of_related_id', 'in', self.intervention_ids.ids)
+        ]
+
+    @api.multi
+    def _compute_of_dms_file_count(self):
         for order in self:
-            order.of_dms_file_count = self.env['muk_dms.file'].search_count(
-                [('of_related_model', '=', 'sale.order'), ('of_related_id', '=', order.id)])
+            domain = order.get_dms_file_domain()
+            order.of_dms_file_count = self.env['muk_dms.file'].search_count(domain)
 
     @api.multi
     def action_view_dms_files(self):
         self.ensure_one()
-        if self.of_dms_file_count:
-            order_files = self.env['muk_dms.file'].search(
-                [('of_related_model', '=', 'sale.order'), ('of_related_id', '=', self.id)])
-            action = self.env.ref('muk_dms.action_dms_file').read()[0]
-            action['views'] = [
-                (self.env.ref('muk_dms.view_dms_file_kanban').id, 'kanban'),
-                (self.env.ref('muk_dms.view_dms_file_tree').id, 'tree'),
-                (self.env.ref('muk_dms.view_dms_file_form').id, 'form')]
-            action['domain'] = [('id', 'in', order_files.ids)]
-            return action
+        action = self.env.ref('muk_dms.action_dms_file').read()[0]
+        action['views'] = [
+            (self.env.ref('muk_dms.view_dms_file_kanban').id, 'kanban'),
+            (self.env.ref('muk_dms.view_dms_file_tree').id, 'tree'),
+            (self.env.ref('muk_dms.view_dms_file_form').id, 'form')]
+        action['domain'] = self.get_dms_file_domain()
+        return action
