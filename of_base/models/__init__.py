1e4b27813bfcfc808a8ae3541d6022703dafb075
Maxime JACQUET <mj@openfire.fr>
2023-02-03 15:39:44 +0100

    of_base : [ADD] CRON de notification éditeur
    Ajout de la possibilité de désactiver l'action planifiée de notification
     éditeur en fonction d'un paramètre système pour les environnements
     de développement / preproduction.

--- of_base/models/__init__.py
+++ of_base/models/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import base
+from . import update
 from . import base_config_settings
 from . import res_company
 from . import ir_mail_server
