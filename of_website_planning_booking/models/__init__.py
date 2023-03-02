a5db62734a61bd2d2e908b2380e065cdba742cfc
Stan Heurtault <heusta@openfire.fr>
2023-03-02 16:01:41 +0100

    of_website_planning_booking : T6784  - [IMP] Ajout d'une nouvelle granularité de planification Manuelle.  - [IMP] Ajout du choix de créneaux sur les taches.  - [IMP] Prise de RDV en ligne en fonction des créneaux de planification manuel.
    of_calendar, of_planning, of_planning_tournee : T6784
     - [IMP] Modification pour permettre l'utilisation de la nouvelle granularité de planification

--- of_website_planning_booking/models/__init__.py
+++ of_website_planning_booking/models/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import partner
 from . import planning
 from . import product
 from . import website
 from . import tour
+from . import of_horaire
