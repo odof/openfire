30e68726f9204731b9a9c1de30afc79aef7f8f02
Paul <plc@openfire.fr>
2023-03-17 17:47:21 +0100

    of_calculation : T5518 - [ADD] Nouvelle application regroupant les calculs
    of_calculation_heat_loss : T5518 - [ADD] Ajout du calcul de déperdition de chaleur

---
+++ of_calculation/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': u"OpenFire / Calculs",
+    'version': "10.0.1.0.0",
+    'author': "OpenFire",
+    'description': u"""
+Module de l'application Calcul
+==============================
+""",
+    'website': "www.openfire.fr",
+    'depends': [],
+    'category': "OpenFire",
+    'data': [
+        'views/of_calculation_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
