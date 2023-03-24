b2a901d72449dca99bb80b9dd4673742902c916c
Maxime JACQUET <mj@openfire.fr>
2023-03-24 16:27:52 +0100

    of_product_brand : [MIG] Migration 16.0
    (from openfire@f8a0879da854947585e8ea25f42ec859f2fc058f)

--- of_product_brand/report/of_sale_report.py
+++ of_product_brand/report/of_sale_report.py
@@ -155,6 +155,7 @@ class SaleReport(models.Model):
             display_margin_percent = depends_mapping_margin.get('of_margin_percentage')
             if all(f'{val}:sum' in fields for val in display_margin_percent):
                 for entry in res:
-                    entry[display_margin_percent[0]] = \
-                        entry[display_margin_percent[1]] * 100 / (entry[display_margin_percent[2]] or 1)
+                    if entry[display_margin_percent[1]] is not None and entry[display_margin_percent[2]] is not None:
+                        entry[display_margin_percent[0]] = \
+                            entry[display_margin_percent[1]] * 100 / (entry[display_margin_percent[2]] or 1)
         return res
