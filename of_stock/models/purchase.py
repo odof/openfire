e6439606d8a7fc3b51ce1cd346fb56910acabcd7
Paul <plc@openfire.fr>
2023-04-17 15:34:15 +0200

    of_stock : T11732 - [IMP] Séparation de la partie pé-assignation des numéros de série et création d'une action dédiée

--- of_stock/models/purchase.py
+++ of_stock/models/purchase.py
@@ -8,29 +8,35 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     @api.multi
-    def button_confirm(self):
-        res = super(PurchaseOrder, self).button_confirm()
+    def button_approve(self):
+        res = super(PurchaseOrder, self).button_approve()
         company_ids = self.env['ir.values'].get_default(
             'stock.config.settings', 'of_serial_management_company_ids') or []
-        orders_with_serial_management = self.filtered(
-            lambda o: o.company_id.id in company_ids and o.state == 'purchase')
+        orders_with_serial_management = self.filtered(lambda o: o.company_id.id in company_ids)
         orders_with_serial_management.generate_serial_number()
+        orders_with_serial_management.preassign_serial_number()
         return res
 
     @api.multi
     def action_generate_serial_number(self):
         company_ids = self.env['ir.values'].get_default(
             'stock.config.settings', 'of_serial_management_company_ids') or []
+        orders_with_serial_management = self.filtered(lambda o: o.company_id.id in company_ids)
+        orders_with_serial_management.generate_serial_number()
+
+    @api.multi
+    def action_preassign_serial_number(self):
+        company_ids = self.env['ir.values'].get_default(
+            'stock.config.settings', 'of_serial_management_company_ids') or []
         orders_with_serial_management = self.filtered(
             lambda o: o.company_id.id in company_ids and o.state == 'purchase')
-        orders_with_serial_management.generate_serial_number()
+        orders_with_serial_management.preassign_serial_number()
 
     @api.multi
     def generate_serial_number(self):
         production_lot_obj = self.env['stock.production.lot']
         sequence_obj = self.env['ir.sequence']
         barcode_nomenclature_obj = self.env['barcode.nomenclature']
-        operation_lot_obj = self.env['stock.pack.operation.lot']
 
         # On parcourt les articles des commandes avec suivi par numéro de série
         for order in self:
@@ -44,15 +50,7 @@ class PurchaseOrder(models.Model):
 
                 number = qty_todo - qty_done
 
-                # On récupère l'opération de réception liée à l'article en vue de l'attribution des lots générés.
-                # Plusieurs lignes de commande avec un même article ne créées qu'une operation.
-                pack_operation = order.mapped(
-                    'order_line.move_ids.linked_move_operation_ids.operation_id').filtered(
-                    lambda o: o.product_id.id == product.id)
-                if len(pack_operation) > 1:
-                    pack_operation = pack_operation[0]
-
-                    # On crée autant de numéro de série que de quantité non traitée sur la ligne
+                # On crée autant de numéro de série que de quantité non traitée sur la ligne
                 while number > 0:
                     next_by_code = sequence_obj.next_by_code('stock.lot.serial')
                     name = '%s %s %s' % (
@@ -60,18 +58,43 @@ class PurchaseOrder(models.Model):
                             next_by_code,
                             order.partner_id and order.partner_id.name or '')
                     ean13 = barcode_nomenclature_obj.sudo().sanitize_ean("%0.13s" % next_by_code)
-                    production_lot = production_lot_obj.create({
+                    production_lot_obj.create({
                         'name': name,
                         'product_id': product.id,
                         'of_internal_serial_number': ean13,
                     })
-
-                    # On attribue le lot à l'opération associée
-                    if pack_operation:
-                        operation_lot_obj.create({
-                            'operation_id': pack_operation.id,
-                            'qty': 0,
-                            'qty_todo': 1,
-                            'lot_id': production_lot.id,
-                        })
                     number -= 1
+
+    @api.multi
+    def preassign_serial_number(self):
+        production_lot_obj = self.env['stock.production.lot']
+        operation_lot_obj = self.env['stock.pack.operation.lot']
+
+        # On parcourt les articles des commandes avec suivi par numéro de série
+        for order in self:
+            for product in order.mapped('order_line.product_id').filtered(lambda p: p.tracking == 'serial'):
+                # On récupère l'opération de réception liée à l'article en vue de l'attribution des lots.
+                # Plusieurs lignes de commande avec un même article ne créées qu'une operation.
+                pack_operation = order.mapped(
+                    'order_line.move_ids.linked_move_operation_ids.operation_id').filtered(
+                    lambda o: o.product_id.id == product.id)
+
+                if pack_operation:
+                    if len(pack_operation) > 1:
+                        pack_operation = pack_operation[0]
+
+                    # On recherche les numéros de série qui ont été générés pour cet article et cette commande
+                    production_lots = production_lot_obj.search(
+                        [('name', 'ilike', order.name), ('product_id', '=', product.id)])
+
+                    for lot in production_lots:
+                        # On vérifie que ce lot n'est pas déjà assigné à cette opération
+                        if not operation_lot_obj.search(
+                                [('lot_id', '=', lot.id), ('operation_id', '=', pack_operation.id)]):
+                            # On attribue le lot à l'opération associée
+                            operation_lot_obj.create({
+                                'operation_id': pack_operation.id,
+                                'qty': 0,
+                                'qty_todo': 1,
+                                'lot_id': lot.id,
+                            })
