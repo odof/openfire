d255a1b9a67c9bc93d06a4ef106f2d9fd9827af6
Maxime JACQUET <mj@openfire.fr>
2023-04-26 17:45:06 +0200

    of_product_brand : [FIX] Expected singleton sur account.move.line

--- of_product_brand/models/account_move_line.py
+++ of_product_brand/models/account_move_line.py
@@ -20,31 +20,35 @@ class AccountInvoiceLine(models.Model):
     @api.depends('product_id', 'journal_id')
     def _compute_name(self):
         super()._compute_name()
-        line_name = self.name
-        if self.product_id.brand_id.use_brand_description_sale:
-            # Recalcul du libelllé de la ligne
-            line_name = self.product_id.name_get()[0][1]
-            # Only inline templates are supported here.
-            # ie: {{ description_sale and '\n' + description_sale or '' }}
-            brand_desc = self.env['mail.template']._render_template(
-                self.product_id.brand_id.description_sale, 'product.product', [self.product_id.id], post_process=False
-            )[self.product_id.id]
-            line_name += '\n%s' % brand_desc
-        if self.product_id.brand_id.show_in_sales:
-            # Ajout de la marque dans le descriptif de l'article
-            brand_code = f'{self.product_id.brand_id.name} - '
-            # This regex will match the first occurrence of "[XXXX]" in the product's name as the default code
-            # and will capture the rest of the string.
-            # So if the product's name is "[XXXX] This is the product's [YYYY] name" it will capture groups
-            # as : "[XXXX] " and "This is the product's [YYYY] name"
-            regex = r"(^\[\S+\] )(.*)"
-            if re.search(regex, line_name) is not None:
-                # product's name is required so we should always have 2 groups here
-                subst = f"\\g<1>{brand_code}\\g<2>"
-                line_name = re.sub(regex, subst, line_name, 0, re.MULTILINE)
-            else:
-                line_name = brand_code + line_name
-        self.name = line_name
+        for line in self:
+            line_name = line.name
+            if line.product_id.brand_id.use_brand_description_sale:
+                # Recalcul du libelllé de la ligne
+                line_name = line.product_id.name_get()[0][1]
+                # Only inline templates are supported here.
+                # ie: {{ description_sale and '\n' + description_sale or '' }}
+                brand_desc = self.env['mail.template']._render_template(
+                    line.product_id.brand_id.description_sale,
+                    'product.product',
+                    [line.product_id.id],
+                    post_process=False,
+                )[line.product_id.id]
+                line_name += '\n%s' % brand_desc
+            if line.product_id.brand_id.show_in_sales:
+                # Ajout de la marque dans le descriptif de l'article
+                brand_code = f'{line.product_id.brand_id.name} - '
+                # This regex will match the first occurrence of "[XXXX]" in the product's name as the default code
+                # and will capture the rest of the string.
+                # So if the product's name is "[XXXX] This is the product's [YYYY] name" it will capture groups
+                # as : "[XXXX] " and "This is the product's [YYYY] name"
+                regex = r"(^\[\S+\] )(.*)"
+                if re.search(regex, line_name) is not None:
+                    # product's name is required so we should always have 2 groups here
+                    subst = f"\\g<1>{brand_code}\\g<2>"
+                    line_name = re.sub(regex, subst, line_name, 0, re.MULTILINE)
+                else:
+                    line_name = brand_code + line_name
+            line.name = line_name
 
     def _write(self, vals):
         for field in vals:
