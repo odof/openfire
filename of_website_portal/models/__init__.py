e865a874f92d2f208479718a1a6ff66c31ba930c
Archibald JEGO <aj@openfire.fr>
2023-05-02 10:39:03 +0200

    of_website_portal : T4480
     - [IMP] Permettre la création automatique d'utilisateur portail
    
    of_website_portal_custom_workflow : T4480
     - [ADD] Module de lien entre of_website_portal et of_sale_custom_workflow

--- of_website_portal/models/__init__.py
+++ of_website_portal/models/__init__.py
@@ -7,3 +7,4 @@ from . import sale_order
 from . import res_config
 from . import res_users
 from . import website
+from . import res_partner
--- of_website_portal/models/sale_order.py
+++ of_website_portal/models/sale_order.py
@@ -9,3 +9,16 @@ class SaleConfiguration(models.TransientModel):
 
     group_show_price_subtotal = fields.Boolean(group='base.group_portal,base.group_user')
     group_show_price_total = fields.Boolean(group='base.group_portal,base.group_user')
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        create_portal_users = self.env['ir.values'].get_default('base.config.settings', 'of_create_portal_users')
+        if create_portal_users == 'order_validation':
+            self.mapped('partner_id')._create_portal_user()
+        return res
+
