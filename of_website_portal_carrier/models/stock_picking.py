15269f455916a0ec10d415081412c52719bc6542
Stan Heurtault <heusta@openfire.fr>
2023-05-09 18:00:42 +0200

    of_website_portal_carrier : T12963 - [IMP] Ajout d'un délai pour la validation de BR depuis le portail.

--- of_website_portal_carrier/models/stock_picking.py
+++ of_website_portal_carrier/models/stock_picking.py
@@ -1,10 +1,48 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 from odoo import models, api, fields
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    of_validated_by_carrier = fields.Boolean(string="Validé par le transporteur")
+    of_validated_by_carrier = fields.Boolean(string=u"Validé par le transporteur")
+    of_carrier_validation_date = fields.Datetime(string=u"Validé par le transporteur le")
+    of_need_backorder = fields.Boolean(string=u"Créer le reliquat")
+    of_error_message = fields.Text(string=u"Message d'erreur")
+
+    @api.model
+    def validate_picking_from_carriers(self):
+        backorder_confirmation_obj = self.env['stock.backorder.confirmation']
+        delay = self.env['ir.values'].sudo().get_default('website.config.settings', 'of_picking_rollback_delay')
+        time_for_domain = fields.Datetime.to_string(datetime.now() - relativedelta(minutes=delay))
+        domain = [('state', 'not in',['done', 'cancel']), ('of_carrier_validation_date', '<=', time_for_domain)]
+        pickings_done = self
+        pickings_to_validate = self.search(domain)
+        for picking in pickings_to_validate:
+            # catching exceptions just in case
+            try:
+                action = picking.do_new_transfer()
+                if action and action.get('res_id'):
+                    wizard = backorder_confirmation_obj.browse(action['res_id'])
+                    if picking.of_need_backorder:
+                        wizard.process()
+                    else:
+                        wizard.process_cancel_backorder()
+                pickings_done += picking
+            except Exception as e:
+                continue
+        if pickings_done:
+            pickings_done.write({'of_validated_by_carrier': True})
+
+    @api.multi
+    def write(self, vals):
+        # Vider le message d'erreur si il y en a un
+        if 'of_error_message' not in vals and self.mapped('of_error_message'):
+            vals['of_error_message'] = False
+        return super(StockPicking, self).write(vals)
