c4320e79ab03aadfa261b1a538dc0fc796770aaa
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2023-05-11 09:39:53 +0200

    of_sale_norme : [ADD] Version 10.0
    (from openfire@00cbe956d1cd406a0199c507ef285bc81ab93c87)

---
+++ of_sale_norme/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "OpenFire / Normes",
+    "version": "10.0.1.0.0",
+    "author": "OpenFire",
+    'category': 'OpenFire modules',
+    'summary': 'Normes',
+    'license': 'AGPL-3',
+    "description": """
+Normes des produits vendus OpenFire
+====================================
+
+- Création de l'objet 'Norme' et des vues associées
+- Ajout des champs 'norme_id' et 'description_norme' dans les articles
+- Report du champ 'description_norme' dans les lignes de devis et de factures si elle est active et que la case 'Afficher en impression' est cochée
+
+Objet Norme
+-----------
+
+Cet objet contient 4 champs:
+
+- Code (name): le code de la norme
+- Libellé (libelle): une brève description de la norme
+- Description (description): une description détaillée de la norme
+- Afficher en impression (display_docs): cocher cette case pour ajouter la description de la norme dans les devis et factures
+
+Il est possible d'archiver une norme.
+
+La description d'une norme est copiée dans l'article quand elle lui est associée. Elle peut ensuite être modifiée article par article.
+
+Quand la description d'une norme est modifiée, met à jour le champ 'description_norme' des articles associés. Les articles dont la description norme a été modifiée ne sont pas mis à jour.
+""",
+    "website": "www.openfire.fr",
+    "depends": [
+        "sale",
+        "of_product",
+    ],
+    "category": "OpenFire",
+    "data": [
+        'views/of_sale_norme_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
