cdb57b8b254cf289f43b2131800e40d827817280
Paul <plc@openfire.fr>
2023-05-18 22:22:11 +0200

    of_website_portal_carrier : [IMP/FIX] Ajout de la pagination et correction du tri par date de livraison pour la liste des BR transporteur

--- of_website_portal_carrier/static/src/js/sortable_table.js
+++ of_website_portal_carrier/static/src/js/sortable_table.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('#receipt_table').DataTable({
-        "paging":           false,
         "info":             false,
         "language": {
             "url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/fr_fr.json"
