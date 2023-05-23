6052711bc96f658a2c2dc82f0b9d131f5dbe14bb
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2023-05-23 16:45:33 +0200

    of_sale_order_dates : [MIG] Migration 16.0

--- of_sale_order_dates/__manifest__.py
+++ of_sale_order_dates/__manifest__.py
@@ -1,16 +1,23 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
-    'name': "OpenFire / Dates on Sale Orders",
+    'name': "OpenFire / Dates sur les commandes de vente",
     'version': '16.0.1.0.0',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
     'category': "OpenFire",
+    'summary': "Dates sur les commandes de vente",
+    'description': "",
     'depends': [
         'of_sale_report_setting',
     ],
     'data': [
-        'reports/sale_report_templates.xml',
         'views/res_config_settings_views.xml',
         'views/sale_order_views.xml',
+        'reports/sale_report_templates.xml',
     ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
