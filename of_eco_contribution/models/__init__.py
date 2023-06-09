d2a8f1816e18e1ded4267b5b5142342b6a31afff
Stan Heurtault <heusta@openfire.fr>
2023-06-09 15:32:53 +0200

    of_eco_contribution : T13243  - [IMP] Ajout de la sélection de l'éco-contribution depuis les articles.  - [IMP] Ajout de l'éco-contribution aux impressions de devis/commandes/factures.  - [IMP] Création de la pièce comptable de facture prend en compte le compte comptable des éco-organismes.
    of_kit, of_sale : T13243
     - [IMP] Ajout d'attributs name sur les rapports.

--- of_eco_contribution/models/__init__.py
+++ of_eco_contribution/models/__init__.py
@@ -3,3 +3,6 @@
 
 # from . import res_config_settings
 from . import of_eco_contribution
+from . import product
+from . import sale_order
+from . import account_invoice
---
+++ of_eco_contribution/models/sale_order.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import api, models, fields, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    of_total_eco_contribution = fields.Float(
+        string=u"Dont éco-contribution PMCB", compute='_compute_of_total_eco_contribution')
+
+    @api.depends('order_line.of_total_eco_contribution')
+    def _compute_of_total_eco_contribution(self):
+        for record in self:
+            record.of_total_eco_contribution = sum(record.mapped('order_line.of_total_eco_contribution'))
+
+    @api.multi
+    def recompute_eco_contribution(self):
+        self_filtered = self.filtered(lambda r: r.state in ('draft', 'sent'))
+        if not self_filtered:
+            return
+        lines = self_filtered.mapped('order_line')
+        kit_lines = lines.mapped('kit_id.kit_line_ids')
+        kit_lines._recompute_todo(kit_lines._fields['of_total_eco_contribution'])
+        kit_lines.recompute()
+        lines._recompute_todo(lines._fields['of_total_eco_contribution'])
+        lines.recompute()
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    of_total_eco_contribution = fields.Float(
+        string=u"Montant éco-contribution", compute='_compute_of_total_eco_contribution', store=True)
+
+    @api.depends('product_id', 'product_uom_qty', 'product_uom', 'kit_id.of_total_eco_contribution', 'of_is_kit')
+    def _compute_of_total_eco_contribution(self):
+        for record in self:
+            if record.of_is_kit:
+                record.of_total_eco_contribution = record.kit_id.of_total_eco_contribution
+            elif record.product_id.of_eco_contribution_id:
+                contribution = record.product_id.of_eco_contribution_id
+                original_uom = record.product_id.uom_id
+                current_uom = record.product_uom
+                qty = current_uom._compute_quantity(record.product_uom_qty, original_uom, round=False)
+                if contribution.type == 'ton':
+                    record.of_total_eco_contribution = qty * contribution.price * \
+                                                       record.product_id.weight / 1000.0
+                else:
+                    record.of_total_eco_contribution = qty * contribution.price
+
+
+class OfSaleOrderKit(models.Model):
+    _inherit = 'of.saleorder.kit'
+
+    of_total_eco_contribution = fields.Float(
+        string=u"Montant éco-contribution", compute='_compute_of_total_eco_contribution', store=True)
+
+    @api.depends('kit_line_ids.of_total_eco_contribution')
+    def _compute_of_total_eco_contribution(self):
+        for record in self:
+            record.of_total_eco_contribution = sum(record.mapped('kit_line_ids.of_total_eco_contribution'))
+
+
+class OfSaleOrderKitLine(models.Model):
+    _inherit = 'of.saleorder.kit.line'
+
+    of_total_eco_contribution = fields.Float(
+        string=u"Montant éco-contribution", compute='_compute_of_total_eco_contribution', store=True)
+
+    @api.depends('product_id', 'qty_per_kit', 'product_uom_id', 'kit_id.order_line_id.product_uom_qty')
+    def _compute_of_total_eco_contribution(self):
+        for record in self:
+            if record.product_id.of_eco_contribution_id:
+                contribution = record.product_id.of_eco_contribution_id
+                original_uom = record.product_id.uom_id
+                current_uom = record.product_uom_id
+                base_qty = record.qty_per_kit * record.kit_id.order_line_id.product_uom_qty
+                qty = current_uom._compute_quantity(base_qty, original_uom, round=False)
+                if contribution.type == 'ton':
+                    record.of_total_eco_contribution = qty * contribution.price * \
+                                                       record.product_id.weight / 1000.0
+                else:
+                    record.of_total_eco_contribution = qty * contribution.price
