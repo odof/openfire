cbcbdd7ffe3142faf2d9e2f579f2bcb93f7f1b37
Maxime JACQUET <mj@openfire.fr>
2023-06-15 15:44:27 +0200

    of_base : [MIG] Migration 16.0
    (from openfire@e5effb9e8711d97c51f7ce6d6cb4f59060b1d132)

--- of_base/models/res_partner_title.py
+++ of_base/models/res_partner_title.py
@@ -5,5 +5,7 @@ from odoo import fields, models
 
 class ResPartnerTitle(models.Model):
     _inherit = 'res.partner.title'
+    _order = 'sequence, name'
 
+    sequence = fields.Integer(string="Sequence", default=1, help="Used to order titles. Lower is better.")
     of_used_for_phone = fields.Boolean(string="Used for phone numbers", default=True)
