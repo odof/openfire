a7ca4bf61be69eba22746a0389ef3952c7883e73
Archibald JEGO <aj@openfire.fr>
2023-06-19 18:19:05 +0200

    of_sale_stock: T5089 - [IMP] Ajout du lient CF sur les mouvements de stock CC

--- of_sale_stock/hooks/__init__.py
+++ of_sale_stock/hooks/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
-from . import wizard
-from . import hooks
+from . import of_sale_stock_hook
