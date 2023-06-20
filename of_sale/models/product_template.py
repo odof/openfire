a57a42d6124cba40c9af1418487b91b01135ead9
Maxime JACQUET <mj@openfire.fr>
2023-06-20 14:56:44 +0200

    of_sale : [MIG] Migration 16.0
    (from openfire@2ff27d108a86ed2f52242891fafbdc92df2e9c75)

--- of_sale/models/product_template.py
+++ of_sale/models/product_template.py
@@ -1,10 +1,41 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import _, api, models
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        admins = self.env.ref('base.user_root') | self.env.ref('base.user_admin')
+        if self._uid not in admins.ids:
+            deposit_categ_id = (
+                self.env['ir.config_parameter'].sudo().get_param('of.sale.of_deposit_product_categ_id', False)
+            )
+            if deposit_categ_id := deposit_categ_id and int(deposit_categ_id) or False:
+                deposit_categories = any(vals['categ_id'] == deposit_categ_id for vals in vals_list)
+                if deposit_categories:
+                    raise UserError(
+                        _("Only the administrator has the right to place items in the down payment category.")
+                    )
+        return super().create(vals_list)
+
+    def write(self, vals):
+        admins = self.env.ref('base.user_root') | self.env.ref('base.user_admin')
+        if self._uid not in admins.ids:
+            deposit_categ_id = (
+                self.env['ir.config_parameter'].sudo().get_param('of.sale.of_deposit_product_categ_id', False)
+            )
+            deposit_categ_id = deposit_categ_id and int(deposit_categ_id) or False
+            if deposit_categ_id and vals.get('categ_id') == deposit_categ_id:
+                raise UserError(_("Only the administrator has the right to place items in the down payment category."))
+        if self.with_context(prefetch_fields=False).search(
+            [('id', 'in', self.ids), ('categ_id', '=', deposit_categ_id)], limit=1
+        ):
+            raise UserError(_("Only the administrator has the right to modify deposit items."))
+        return super().write(vals)
 
     def unlink(self):
         deleted_ids = self.ids
