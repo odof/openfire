393ff8e6639c2e6306c29ea9854e5b2741c62799
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2023-06-27 16:15:19 +0200

    of_account_invoice_report : [ADD] Version 10.0
    (from acbb4244302383b40558743704614056f8012e2b)

---
+++ of_account_invoice_report/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'OpenFire / Impression factures',
+    'author': 'OpenFire',
+    'version': '10.0.1.0.0',
+    'category': 'OpenFire modules',
+    'summary': 'Affichage des paiements dans les factures',
+    'description': """
+Module de factures :
+
+- Personnalisation des rapports de factures
+
+- Ajout des paiements dans les rapports de factures
+    """,
+    'website': 'openfire.fr',
+    'depends': [
+        'of_account',
+    ],
+    'data': [
+        'data/of_account_invoice_report_data.xml',
+        'report/of_invoice_report_templates.xml',
+        'views/of_account_invoice_report_views.xml',
+        'views/report_overdue.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
