c8836de6eb0579d2ec6e1090b69fa34511858d6a
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2023-06-27 12:29:34 +0200

    of_base_multicompy : T13686 - [FIX] valeur d'inventaire sur lignes groupées dans l'inventaire à la date.

--- of_base_multicompany/models/__init__.py
+++ of_base_multicompany/models/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import of_base_multicompany
 from . import product
+from . import stock_history
 from . import stock_picking
