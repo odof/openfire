cab119f5238c0e98671d8aa54d2a016a44a73432
Stan Heurtault <heusta@openfire.fr>
2023-06-28 15:10:45 +0200

    of_website_portal_carrier : T13411 - [IMP] Ajout d'un paramètre pour la création de reliquat depuis le portail transporteur.

--- of_website_portal_carrier/models/res_config_settings.py
+++ of_website_portal_carrier/models/res_config_settings.py
@@ -13,12 +13,18 @@ class WebsiteConfigSettings(models.TransientModel):
         if not self.env['ir.values'].get_default('website.config.settings', 'of_picking_rollback_delay'):
             # par défaut 24h
             self.env['ir.values'].sudo().set_default('website.config.settings', 'of_picking_rollback_delay', 1440)
+        if not self.env['ir.values'].get_default('website.config.settings', 'of_picking_backorder'):
+            self.env['ir.values'].sudo().set_default('website.config.settings', 'of_picking_backorder', 'choice')
         return res
 
     of_picking_rollback_delay = fields.Integer(string=u"(OF) Délais de modification des BR")
+    of_picking_backorder = fields.Selection(selection=[
+        ('choice', u"Choix"),
+        ('auto', u"Automatique"),
+        ], string=u"(OF) Création de reliquat", default='choice', required=True)
 
     @api.onchange('of_picking_rollback_delay')
-    def _onhcnage_of_picking_rollback_delay(self):
+    def _onchange_of_picking_rollback_delay(self):
         if self.of_picking_rollback_delay and self.of_picking_rollback_delay < 0:
             self.of_picking_rollback_delay = 0
 
@@ -26,3 +32,9 @@ class WebsiteConfigSettings(models.TransientModel):
     def set_of_picking_rollback_delay_defaults(self):
         return self.env['ir.values'].sudo().set_default(
             'website.config.settings', 'of_picking_rollback_delay', self.of_picking_rollback_delay)
+
+
+    @api.multi
+    def set_of_picking_backorder_defaults(self):
+        return self.env['ir.values'].sudo().set_default(
+            'website.config.settings', 'of_picking_backorder', self.of_picking_backorder)
