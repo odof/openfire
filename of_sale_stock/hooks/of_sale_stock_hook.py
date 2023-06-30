5d7f74268f08b8b3953aaa2e88742c996a25e25d
Paul <plc@openfire.fr>
2023-06-30 15:17:52 +0200

    of_sale_stock: T5089 - [FIX] Le lien CC/CF se fait maintenant à partir du procurement.order

--- of_sale_stock/hooks/of_sale_stock_hook.py
+++ of_sale_stock/hooks/of_sale_stock_hook.py
@@ -18,11 +18,10 @@ class OFSaleStockHook(models.AbstractModel):
                                                                 'int4'))
             cr.execute('ALTER TABLE "%s" ADD COLUMN "%s" %s' % ('stock_move', 'of_check', 'bool'))
 
-
     def _init_field_procurement_purchase(self):
         module_self = self.env['ir.module.module'].search(
             [('name', '=', 'of_sale_stock'), ('state', 'in', ['installed', 'to upgrade'])])
-        actions_todo = module_self and module_self.latest_version < '10.0.2.0.0' or False
+        actions_todo = module_self and module_self.latest_version < '10.0.3.0.0' or False
         if actions_todo:
             cr = self._cr
             # compute les champs of_procurement_purchase_line_id et of_check
@@ -30,24 +29,20 @@ class OFSaleStockHook(models.AbstractModel):
                 UPDATE stock_move AS sm
                 SET of_procurement_purchase_line_id = (
                     SELECT purchase_line_id
-                    FROM stock_move
+                    FROM procurement_order
                     WHERE move_dest_id = sm.id
                     LIMIT 1
-                );""")
+                )
+                WHERE state NOT IN ('done', 'cancel');""")
 
             cr.execute("""
-                UPDATE stock_move AS sm
-                SET of_check = TRUE
-                WHERE id IN (
-                    SELECT      sm1.id
-                    FROM        stock_move as sm1
-                    LEFT JOIN   stock_move sm2
-                    ON          sm1.of_procurement_purchase_line_id = sm2.purchase_line_id
-                    LEFT JOIN   stock_quant sq1
-                    ON          sq1.reservation_id = sm1.id
-                    LEFT JOIN   stock_quant_move_rel sqmr
-                    ON          sqmr.move_id = sm2.id
-                    WHERE       sm1.of_procurement_purchase_line_id is not null
-                    AND         sm2.id is not null
-                    AND         sq1.id is not null
-                );""")
+                UPDATE  stock_move                          SM1
+                SET     of_check                            = TRUE
+                FROM    stock_move                          SM2
+                ,       stock_quant                         SQ
+                ,       stock_quant_move_rel                SQMR
+                WHERE   SM1.of_procurement_purchase_line_id IS NOT NULL
+                AND     SM2.purchase_line_id                = SM1.of_procurement_purchase_line_id
+                AND     SQ.reservation_id                   = SM1.id
+                AND     SQMR.move_id                        = SM2.id
+                ;""")
