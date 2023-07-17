0f5ec9628b46803adfee888a3fbca7726f18d2e6
Archibald JEGO <aj@openfire.fr>
2023-07-17 18:17:43 +0200

    of_website_portal_supplier: T13841 - [ADD] Création du module portail fournisseur

---
+++ of_website_portal_supplier/models/stock_pack_operation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class StockPackOperation(models.Model):
+    _inherit = 'stock.pack.operation'
+
+    of_expected_shipment_date = fields.Date(string=u"Date d’expédition prévue", copy=False)
+    of_shipped_qty = fields.Float(string=u"Qté expédiée", copy=False)
