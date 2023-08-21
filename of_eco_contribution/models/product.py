5f63d74f7e7ab46ab97ae6929cf42cba072874fa
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2023-08-21 18:10:21 +0200

    of_eco_contribution : T14066 - Options d'ajout de l'éco-contribution dans le montant total des devis/commandes/factures.

--- of_eco_contribution/models/product.py
+++ of_eco_contribution/models/product.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo import models, fields
 
-from odoo import api, models, fields, _
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
