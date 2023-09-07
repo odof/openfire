4102eec3a4cd7904d116ceb0e93d14fefa09b2e6
Stan Heurtault <heusta@openfire.fr>
2023-09-07 09:46:28 +0200

    of_analyse_chantier :  - [IMP] Norme

--- of_analyse_chantier/__manifest__.py
+++ of_analyse_chantier/__manifest__.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
 
 {
-    "name": "OpenFire / Analyse de chantier",
-    "version": "10.0.1.0.0",
-    "author": "OpenFire",
+    'name': u"OpenFire / Analyse de chantier",
+    'version': '10.0.1.1.0',
+    'author': u"OpenFire",
     'license': '',
-    "category": "OpenFire",
-    "description": """
+    'category': 'OpenFire',
+    'description': u"""
 Analyse de chantier OpenFire
 ==========================
 
 
 """,
-    "website": "www.openfire.fr",
-    "depends": [
-        "of_purchase",
-        "of_sale",
-        "of_account",
-        "of_kit",
-        "of_planning",
-        "analytic"
+    'website': 'www.openfire.fr',
+    'depends': [
+        'of_purchase',
+        'of_sale',
+        'of_account',
+        'of_kit',
+        'of_planning',
+        'analytic'
     ],
-    "data": [
-        "views/of_analyse_chantier_views.xml",
-        "wizards/of_analyse_chantier_wizard_views.xml",
-        "security/of_analyse_chantier_security.xml",
-        "security/ir.model.access.csv",
+    'data': [
+        'security/of_analyse_chantier_security.xml',
+        'security/ir.model.access.csv',
+        'views/of_analyse_chantier_views.xml',
+        'wizards/of_analyse_chantier_wizard_views.xml',
         ],
     'installable': True,
     'application': False,
