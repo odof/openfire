2bb59612c01648f29994fb3e1c42133f7017492b
Paul <plc@openfire.fr>
2023-09-15 12:37:09 +0200

    of_stock : T4236 - [FIX] Correction de la création des lignes manquantes à 0 pour les inventaires à la date

--- of_stock/wizards/of_stock_inventory_create_missing_wizard.py
+++ of_stock/wizards/of_stock_inventory_create_missing_wizard.py
@@ -19,48 +19,124 @@ class OFStockInventoryCreateMissingWizard(models.TransientModel):
 
         locations = self.env['stock.location'].search([('id', 'child_of', [self.stock_inventory_id.location_id.id])])
 
-        if self.all_products:
-            # On prend tous les articles en stock
-            self.env.cr.execute(
-                """ SELECT      product_id
-                    ,           SUM(qty)        AS product_qty
-                    ,           location_id
-                    ,           lot_id          AS prod_lot_id
-                    ,           package_id
-                    ,           owner_id        AS partner_id
-                    FROM        stock_quant
-                    WHERE       location_id     IN %s
-                    AND         company_id      = %s
-                    GROUP BY    product_id
-                    ,           location_id
-                    ,           lot_id
-                    ,           package_id
-                    ,           owner_id
-                """, (tuple(locations.ids), self.stock_inventory_id.company_id.id,))
+        inventory_date = self.env['ir.values'].get_default('stock.config.settings', 'of_forcer_date_inventaire')
+
+        if not inventory_date:
+            if self.all_products:
+                # On prend tous les articles en stock
+                self.env.cr.execute(
+                    """ SELECT      product_id
+                        ,           SUM(qty)        AS product_qty
+                        ,           location_id
+                        ,           lot_id          AS prod_lot_id
+                        ,           package_id
+                        ,           owner_id        AS partner_id
+                        FROM        stock_quant
+                        WHERE       location_id     IN %s
+                        AND         company_id      = %s
+                        GROUP BY    product_id
+                        ,           location_id
+                        ,           lot_id
+                        ,           package_id
+                        ,           owner_id
+                    """, (tuple(locations.ids), self.stock_inventory_id.company_id.id,))
+            else:
+                # On filtre les articles en stock en fonction des catégories renseignées
+                self.env.cr.execute(
+                    """ SELECT      SQ.product_id
+                        ,           SUM(SQ.qty)         AS product_qty
+                        ,           SQ.location_id
+                        ,           SQ.lot_id           AS prod_lot_id
+                        ,           SQ.package_id
+                        ,           SQ.owner_id         AS partner_id
+                        FROM        stock_quant         SQ
+                        ,           product_product     PP
+                        ,           product_template    PT
+                        WHERE       SQ.location_id      IN %s
+                        AND         SQ.company_id       = %s
+                        AND         PP.id               = SQ.product_id
+                        AND         PT.id               = PP.product_tmpl_id
+                        AND         PT.categ_id         IN %s
+                        GROUP BY    SQ.product_id
+                        ,           SQ.location_id
+                        ,           SQ.lot_id
+                        ,           SQ.package_id
+                        ,           SQ.owner_id
+                    """, (tuple(locations.ids), self.stock_inventory_id.company_id.id,
+                          tuple(self.product_category_ids.ids),))
         else:
-            # On filtre les articles en stock en fonction des catégories renseignées
-            self.env.cr.execute(
-                """ SELECT      SQ.product_id
-                    ,           SUM(SQ.qty)         AS product_qty
-                    ,           SQ.location_id
-                    ,           SQ.lot_id           AS prod_lot_id
-                    ,           SQ.package_id
-                    ,           SQ.owner_id         AS partner_id
-                    FROM        stock_quant         SQ
-                    ,           product_product     PP
-                    ,           product_template    PT
-                    WHERE       SQ.location_id      IN %s
-                    AND         SQ.company_id       = %s
-                    AND         PP.id               = SQ.product_id
-                    AND         PT.id               = PP.product_tmpl_id
-                    AND         PT.categ_id         IN %s
-                    GROUP BY    SQ.product_id
-                    ,           SQ.location_id
-                    ,           SQ.lot_id
-                    ,           SQ.package_id
-                    ,           SQ.owner_id
-                """, (tuple(locations.ids), self.stock_inventory_id.company_id.id,
-                      tuple(self.product_category_ids.ids),))
+            query_in = """
+                SELECT
+                    stock_move.location_dest_id AS location_id,
+                    stock_move.product_id AS product_id,
+                    quant.qty AS quantity,
+                    quant.lot_id AS lot_id
+                FROM
+                    stock_quant as quant
+                JOIN
+                    stock_quant_move_rel ON stock_quant_move_rel.quant_id = quant.id
+                JOIN
+                    stock_move ON stock_move.id = stock_quant_move_rel.move_id
+                JOIN
+                    product_product ON product_product.id = stock_move.product_id
+                JOIN
+                    product_template ON product_template.id = product_product.product_tmpl_id
+                WHERE
+                    quant.qty>0
+                    AND quant.company_id = %(company_id)s
+                    AND stock_move.state = 'done'
+                    AND stock_move.date <= %(date)s
+                    AND stock_move.location_dest_id IN %(location_ids)s
+                    AND stock_move.location_dest_id != stock_move.location_id"""
+            query_out = """
+                SELECT
+                    stock_move.location_id AS location_id,
+                    stock_move.product_id AS product_id,
+                    - quant.qty AS quantity,
+                    quant.lot_id AS lot_id
+                FROM
+                    stock_quant as quant
+                JOIN
+                    stock_quant_move_rel ON stock_quant_move_rel.quant_id = quant.id
+                JOIN
+                    stock_move ON stock_move.id = stock_quant_move_rel.move_id
+                JOIN
+                    product_product ON product_product.id = stock_move.product_id
+                JOIN
+                    product_template ON product_template.id = product_product.product_tmpl_id
+                WHERE
+                    quant.qty>0
+                    AND quant.company_id = %(company_id)s
+                    AND stock_move.state = 'done'
+                    AND stock_move.date <= %(date)s
+                    AND stock_move.location_id IN %(location_ids)s
+                    AND stock_move.location_id != stock_move.location_dest_id"""
+
+            query_params = {
+                'company_id': self.stock_inventory_id.company_id.id,
+                'date': self.stock_inventory_id.date,
+                'location_ids': locations._ids,
+            }
+
+            if not self.all_products:
+                query_in += " AND product_template.categ_id IN %(categ_ids)s"
+                query_out += " AND product_template.categ_id IN %(categ_ids)s"
+                query_params['categ_ids'] = self.product_category_ids._ids
+
+            query = """
+                SELECT
+                    product_id,
+                    SUM(quantity) AS product_qty,
+                    location_id,
+                    lot_id AS prod_lot_id
+                FROM
+                    ((""" + query_in + """
+                    ) UNION ALL
+                    (""" + query_out + """
+                    )) AS foo
+                GROUP BY location_id, product_id, lot_id"""
+
+            self.env.cr.execute(query, query_params)
 
         data = self.env.cr.dictfetchall()
         vals = []
