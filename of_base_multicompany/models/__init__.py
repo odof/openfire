94dc5681a7c9377f0c3a30a0c01b7647054f53e4
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2023-09-26 09:45:48 +0200

    of_base_multicompany of_l10n_fr_certification : T14334 - [FIX] Paiements de type transfert. of_base_multicompany : Possibilité de sélectionner un compte de transfert de la société comptable. of_l10n_fr_certification :  Réparation d'erreur à la création de paiement sans partenaire.

--- of_base_multicompany/models/__init__.py
+++ of_base_multicompany/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import account_config_setings
 from . import of_base_multicompany
 from . import product
 from . import stock_history
