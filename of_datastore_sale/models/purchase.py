be063aac58eb4264919dbc749ac588efd056013a
Saif Eddine ISSAOUI <sei@openfire.fr>
2023-10-10 14:38:11 +0200

    of_datastore_sale : [FIX] Bug de confirmation de commande
    Ajout d'un controle sur le champ client pour éviter une erreur lors de la confirmation de commande s'il est vide.
    
    of_datastore_sale : [FIX] Bug de confirmation de commande
    
    Ajout d'un controle sur le champ client pour éviter une erreur lors de la confirmation de commande s'il est vide.

--- of_datastore_sale/models/purchase.py
+++ of_datastore_sale/models/purchase.py
@@ -9,7 +9,9 @@ class PurchaseOrder(models.Model):
     @api.multi
     def button_confirm(self):
         datastore_obj = self.env['of.datastore.sale']
-        datastore = datastore_obj.search([('partner_ids', 'in', [self.customer_id.id])])
+        datastore = datastore_obj
+        if self.customer_id:
+            datastore = datastore_obj.search([('partner_ids', 'in', [self.customer_id.id])])
         line_link = {}
         if datastore:
             for line in self.mapped('order_line'):
