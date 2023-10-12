8fa91456843e9e243ce4f3909389047501ce5e0f
Hounaida SALHI <hs@openfire.fr>
2023-10-12 14:48:34 +0000

    of_questionnaire_quote_template:T14235-[FIX,IMP]-Création auto de DI à la validation de devis

--- of_questionnaire_quote_template/models/of_questionnaire_quote_template.py
+++ of_questionnaire_quote_template/models/of_questionnaire_quote_template.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
 
@@ -15,25 +16,3 @@ class SaleQuoteTemplate(models.Model):
         comodel_name='of.questionnaire.line', related='of_questionnaire_id.line_ids',
         string=u"Questions", readonly=True)
 
-
-class OfPlanningIntervention(models.Model):
-    _inherit = "of.planning.intervention"
-
-    @api.model
-    def default_get(self, field_list):
-        result = super(OfPlanningIntervention, self).default_get(field_list)
-        # Si le RDV est lié à une commande, que cette commande possède un modèle de devis
-        # et que ce modèle de devis est lié à un modèle d'intervention, charger le modèle d'intervention
-        # les onchanges s'occuperont de charger la tâche et le questionnaire
-        if 'order_id' in result:
-            order = self.env['sale.order'].browse(result['order_id'])
-            if order.of_template_id and order.of_template_id.of_intervention_template_id:
-                result['template_id'] = order.of_template_id.of_intervention_template_id.id
-        return result
-
-    @api.onchange('order_id')
-    def _onchange_order_id(self):
-        self.ensure_one()
-        order = self.order_id
-        if order and order.of_template_id and order.of_template_id.of_intervention_template_id:
-            self.template_id = order.of_template_id.of_intervention_template_id
---
+++ of_questionnaire_quote_template/models/sale_config_settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class SaleConfigSettings(models.TransientModel):
+    _inherit = "sale.config.settings"
+
+    of_active_option = fields.Boolean(string="Activer l'option de création automatique de demande d'intervention")
+
+    @api.multi
+    def set_of_active_option(self):
+        return self.env['ir.values'].sudo().set_default(
+            'sale.config.settings', 'of_active_option', self.of_active_option)
