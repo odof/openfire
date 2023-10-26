2cdd4435645a78864873f911d027a6c05359cf04
Saif Eddine ISSAOUI <sei@openfire.fr>
2023-10-26 12:27:36 +0000

    of_questionnaire_quote_template:T14235-[FIX + IMP] - Création auto de DI à la validation de devis

--- of_questionnaire_quote_template/__init__.py
+++ of_questionnaire_quote_template/__init__.py
@@ -1,3 +1,4 @@
 # -*- encoding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
--- of_questionnaire_quote_template/models/__init__.py
+++ of_questionnaire_quote_template/models/__init__.py
@@ -1,3 +1,8 @@
 # -*- encoding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import of_questionnaire_quote_template
+from . import of_planning_intervention
+from . import of_service
+from . import sale_order
+from . import sale_config_settings
