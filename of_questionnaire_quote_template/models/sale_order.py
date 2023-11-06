a2031b5f0327a05dcf814cd39281fed4e187aa39
Stan Heurtault <heusta@openfire.fr>
2023-11-06 15:37:31 +0100

    of_questionnaire_quote_template : T14235  - [IMP] Ajout parc installe et récupération infos du modèle d'intervention dans la génération auto de DI.

--- of_questionnaire_quote_template/models/sale_order.py
+++ of_questionnaire_quote_template/models/sale_order.py
@@ -19,7 +19,7 @@ class SaleOrder(models.Model):
             deux_semaines_da = today_da + timedelta(days=14)
             deux_semaines_str = fields.Date.to_string(deux_semaines_da)
             if self.state == 'sale':
-                self.env['of.service'].create({
+                new_service = self.env['of.service'].create({
                     'partner_id': self.partner_id.id,
                     'company_id': self.company_id.id,
                     'address_id': self.partner_shipping_id and self.partner_shipping_id.id or self.partner_id.id,
@@ -34,5 +34,7 @@ class SaleOrder(models.Model):
                     'template_id': self.of_template_id.of_intervention_template_id.id,
                     'tache_id': self.of_template_id.of_intervention_template_id.tache_id.id,
                     'duree': self.of_template_id.of_intervention_template_id.tache_id.duree,
+                    'parc_installe_id': self.of_parc_installe_ids and self.of_parc_installe_ids[0].id or False,
                 })
+                new_service.onchange_template_id()
         return res
