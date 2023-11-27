da893150e0ff8cb2762a64354ba36088e55cf4e7
Stan Heurtault <heusta@openfire.fr>
2023-11-27 09:33:04 +0100

    of_l10n_fr_siret, of_* : T13979 - [ADD/IMP]
    Gestion de l'impression du SIREN dans les rapports Devis/commande et Facture

--- of_l10n_fr_siret/__manifest__.py
+++ of_l10n_fr_siret/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': u"OpenFire / Option SIREN/NIC",
-    'version': '10.0.1.0.0',
+    'version': '10.0.2.0.0',
     'license': 'AGPL-3',
     'author': u"OpenFire",
     'website': u"http://www.openfire.fr",
@@ -12,16 +12,33 @@
 OpenFire / Option SIREN/NIC
 ===========================
 
- - Ajout d'une option dans la configuration générale pour rendre le SIREN obligatoire sur les partenaires de type société
+Fonctionnalités :
+------------------
+
+* Ajout d'une option dans la configuration générale pour rendre le SIREN obligatoire sur les partenaires de type société
+* Modification de la vue formulaire de configuration des paramètres d'impression des devis pour ajouter l'option SIRET :
+    - Afficher le SIRET dans l'adresse du client;
+    - Afficher le SIRET dans la pastille client.
+* Modification de la vue formulaire de configuration des paramètres d'impression des factures pour ajouter l'option SIRET :
+    - Afficher dans l'encart d'adresse principal;
+    - Afficher dans une pastille d'informations complémentaires;
+    - Afficher dans l'encart d'adresse principal et dans une pastille d'informations complémentaires.
+* Ajustement des rapports de devis et factures pour prendre en compte les options SIRET.
 """,
     'depends': [
         'l10n_fr_siret',
+        'of_sale',
+        'of_account_invoice_report',
     ],
     'init_xml': [],
     'demo_xml': [],
     'data': [
         'views/res_config_settings_views.xml',
         'views/res_partner_views.xml',
+        'views/account_config_settings_views.xml',
+        'wizards/sale_set_printing_params_view.xml',
+        'report/of_sale_report_template.xml',
+        'report/of_account_invoice_template.xml'
     ],
     'installable': True,
 }
