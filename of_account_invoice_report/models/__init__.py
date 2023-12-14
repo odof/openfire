7d76b1416f1477fb8f295f5a022b5366ff93d126
Stan Heurtault <heusta@openfire.fr>
2023-12-14 15:52:52 +0100

    of_account_invoice_report : T14689  - [MOV] Séparation des fichiers en préparation du dev.

--- of_account_invoice_report/models/__init__.py
+++ of_account_invoice_report/models/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import account_config_settings
+from . import account_invoice
 from . import of_account_invoice_report
