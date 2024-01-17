51d1e6bc9219c6e68fb745d641db283a0397afd4
Archibald JEGO <aj@openfire.fr>
2024-01-17 15:44:00 +0100

    of_service: [ADD] Version 10.0
    (from f21e1993f7880741d124ae334729c472f7c85765)

---
+++ of_service/__manifest__.py
@@ -0,0 +1,28 @@
+
+{
+    'name': "OpenFire / Demande d'intervention",
+    'version': "10.0.2.0.0",
+    'author': "OpenFire",
+    'website': "http://www.openfire.fr",
+    'category': "Generic Modules",
+    'description': """Module OpenFire de gestion des demandes d'intervention.""",
+    'depends': [
+        'of_planning',
+        'of_map_view',
+        'of_utils',
+        'sales_team',  # <- bouton 'prévoir intervention' dans les commandes client
+        'date_range',
+        #'of_base_location', < par of_planning
+    ],
+    'init_xml': [],
+    'demo_xml': [],
+    'data': [
+        'reports/of_service_demande_intervention_templates.xml',
+        'data/of_service_data.xml',
+        'security/ir.model.access.csv',
+        'security/res_groups.xml',
+        'wizards/of_service_create_intervention_wizard_views.xml',
+        'views/of_service_view.xml',
+    ],
+    'installable': True,
+}
---
+++ of_service/models/__init__.py
@@ -0,0 +1,10 @@
+
+from . import of_service
+from . import sale
+from . import partner
+from . import of_planning
+from . import report
+from . import ir_attachment
+from . import date_range
+from . import ir_ui_menu
+from . import mail_template
