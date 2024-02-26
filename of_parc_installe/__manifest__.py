1ace4d2e08c81bc40c350a66f85a273c85656092
Archibald JEGO <aj@openfire.fr>
2024-02-26 15:14:39 +0100

    of_parc_installe: [ADD] Version 10.0
    (from 71a70740667bf50dca31a6177ff50aa75371ad83)

---
+++ of_parc_installe/__manifest__.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+#    OpenFire
+#
+##############################################################################
+
+{
+    'name': u"OpenFire / Module Parc installé",
+    'version': "10.0.1.0.0",
+    'author': "OpenFire",
+    'website': "www.openfire.fr",
+    'category': "OpenFire",
+    'description': u"""
+Module Parc installé : gestion des produits installés avec no de série.
+""",
+    'depends': [
+        'product',
+        'of_project_issue',
+        'of_map_view',
+        'of_product_brand',
+    ],
+    'data': [
+        'data/of_parc_installe_data.xml',
+        'security/ir.model.access.csv',
+        'views/of_parc_installe_views.xml',
+        'views/project_issue_views.xml',
+        'views/res_partner_views.xml',
+        'views/sale_order_views.xml',
+        'views/of_parc_installe_views.xml',
+        'views/of_parc_installe_views.xml',
+        'views/of_parc_installe_views.xml',
+        'wizard/of_wizard_saleorder_to_parc_installe_view.xml',
+    ],
+    'installable': True,
+}
---
+++ of_parc_installe/models/sale_order.py
@@ -0,0 +1,35 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    of_parc_installe_ids = fields.Many2many(comodel_name='of.parc.installe', string=u"Parcs installés", copy=False)
+    of_parc_count = fields.Integer(compute='_compute_parc_info')
+    of_parc_installe_id = fields.Many2one(
+        comodel_name='of.parc.installe', string=u"Désignation", compute='_compute_parc_info'
+    )
+    of_parc_address_id = fields.Many2one(
+        comodel_name='res.partner', string=u"Adresse de pose", compute='_compute_parc_info'
+    )
+    of_parc_date = fields.Date(string=u"Date de pose", compute='_compute_parc_info')
+    of_parc_note = fields.Text(string=u"Note", compute='_compute_parc_info')
+
+    @api.multi
+    def action_view_parc_installe(self):
+        action = self.env.ref('of_parc_installe.action_view_of_parc_installe_sale').read()[0]
+        action['domain'] = [('id', 'in', self.of_parc_installe_ids._ids)]
+        return action
+
+    @api.depends('of_parc_installe_ids')
+    def _compute_parc_info(self):
+        for order in self:
+            order.of_parc_count = len(order.of_parc_installe_ids)
+            if order.of_parc_count == 1:
+                parc = order.of_parc_installe_ids
+                order.of_parc_installe_id = parc
+                order.of_parc_address_id = parc.site_adresse_id
+                order.of_parc_date = parc.date_installation
+                order.of_parc_note = parc.note
