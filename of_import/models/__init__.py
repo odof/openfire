db8710ca0c2849fd82343251569a3924c79c0fce
François Le Gal <fanch.legal@gmail.com>
2024-03-04 10:41:19 +0100

    of_import : [MIG] Migration 16.0
    ---
    
    Tasks : T12602

--- of_import/models/__init__.py
+++ of_import/models/__init__.py
@@ -1,4 +1,8 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import of_import
+from . import of_import_product_config_template
+from . import of_import_product_categ_config
+from . import of_product_brand
+from . import product_template
+from . import of_import_message
