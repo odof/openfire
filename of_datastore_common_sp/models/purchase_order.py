0afa6bc42ad50aeb0477aad48db42d21db3f264d
Hounaida SALHI <hs@openfire.fr>
2024-03-08 14:37:33 +0100

    of_datastore_common_sp : [MIG] - Migration 16.0
    ---
    
    Tasks : T15499

---
+++ of_datastore_common_sp/models/purchase_order.py
@@ -0,0 +1,27 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    of_datastore_anomaly = fields.Boolean(
+        string="In Anomaly", compute='_compute_of_datastore_anomaly', search='_search_of_datastore_anomaly'
+    )
+
+    @api.depends('picking_ids', 'picking_ids.of_datastore_anomaly')
+    def _compute_of_datastore_anomaly(self):
+        for purchase in self:
+            purchase.of_datastore_anomaly = any(purchase.picking_ids.mapped('of_datastore_anomaly'))
+
+    @api.model
+    def _search_of_datastore_anomaly(self, operator, value):
+        orders = (
+            self.env['stock.picking']
+            .search([('of_datastore_anomaly', operator, value)])
+            .mapped('move_lines')
+            .mapped('procurement_id')
+            .mapped('purchase_line_id')
+            .mapped('order_id')
+        )
+        return [('id', 'in', orders.ids)]
