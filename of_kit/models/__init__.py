e266ba3dbbdee58118f1715a289ab73d7a958cde
Maxime JACQUET <mj@openfire.fr>
2024-03-13 16:09:49 +0100

    of_kit : [REM] Suppression du module OF
    Nous repartons sur un module OCA `product_pack` comme base pour les kits

--- of_kit/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import of_kit
-from . import of_kit_sale
-from . import of_kit_account
--- of_kit/wizards/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import of_sale_order_gestion_prix
-from . import of_wizard_insert_kit_comps
-from . import of_cost_recompute_wizard
