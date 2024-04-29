ce5dbe8c7dd910997829c50094f31b08afdfbad4
Stéfan Beaulieu <sb@openfire.fr>
2024-04-29 09:24:58 +0200

    module communication base

---
+++ of_communication_base/__manifest__.py
@@ -0,0 +1,25 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': "OpenFire / Communication",
+    'version': '16.0.1.0.0',
+    'license': 'LGPL-3',
+    'author': "OpenFire",
+    'website': "https://www.openfire.fr",
+    'category': 'OpenFire',
+    'summary': "Gestion de la communication",
+    'description': "",
+    'depends': [
+        'base',
+        'mail',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/of_communication_menus.xml',
+        'views/of_communication_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ of_communication_base/models/of_communication_message.py
@@ -0,0 +1,93 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, exceptions, fields, models
+from odoo.exceptions import ValidationError
+
+
+class OfCommunication(models.Model):
+    _name = 'communication'
+    _inherit = 'mail.thread'
+    _description = 'Message object, which contains all the variables'
+    _order = 'date desc'
+
+    name = fields.Text(string="Title", required=True, help="Title of message")
+    date = fields.Date(
+        string="Date", default=fields.Date.today, required=True, copy=False, help="Post publication date"
+    )
+    type = fields.Selection(
+        string="Type",
+        selection=[
+            ('informative_message', "Informative Message"),
+            ('patch_note', "Patch Note"),
+            ('non-critical_alert_message', "Non-Critical Alert Message"),
+            ('critical_alert_message', "Critical Alert Message"),
+        ],
+        required=True,
+        help="Type of message",
+    )
+    state = fields.Selection(
+        string="State",
+        readonly=True,
+        selection=[
+            ('draft', "Draft"),
+            ('published', "Published"),
+            ('edit', "Edit"),
+            ('canceled', "Canceled"),
+        ],
+        required=True,
+        default="draft",
+        help="State of message",
+    )
+    active = fields.Boolean(string="Archived", default=True)
+    start_scheduled_publication = fields.Datetime(
+        string="Start of Scheduled Publication",
+        readonly=False,
+        required=True,
+        help="The message's display date and time.",
+    )
+    end_scheduled_publication = fields.Datetime(
+        string="End of Scheduled Publication",
+        readonly=False,
+        help="The message's removed date and time. If not set, it stays visible until replaced by a new message.",
+    )
+    summary = fields.Text(
+        string="Summary",
+        default="",
+        required=True,
+        help="Content of the message notification. The maximum number of characters is 280",
+    )
+    message = fields.Html(string="Message", default="", help="Content of the message")
+
+    def action_publish(self):
+        for record in self:
+            if record.state in ['published', 'edit', 'canceled']:
+                raise exceptions.UserError("You can only publish message in 'Draft' state.")
+            record.state = 'published'
+
+    def action_all_publish(self):
+        for record in self:
+            if record.state in ['draft', 'edit']:
+                record.state = 'published'
+
+    def action_edit(self):
+        for record in self:
+            if record.state not in ['published']:
+                raise exceptions.UserError("You can only edit message in 'Published' state.")
+            record.state = 'edit'
+
+    def action_publish_edit(self):
+        for record in self:
+            if record.state in ['published', 'canceled']:
+                raise exceptions.UserError("You can only publish message in 'Edit' state.")
+            record.state = 'published'
+
+    def action_cancel(self):
+        for record in self:
+            if record.state in ['published']:
+                raise exceptions.UserError('You can only cancel message in \'Draft\' state.')
+            record.state = 'canceled'
+
+    @api.constrains('summary')
+    def _check_char_max_summary(self):
+        for record in self:
+            if len(record.summary) > 280:
+                raise ValidationError('You cannot have more than 280 characters in the summary')
