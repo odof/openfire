8467b9dc6ab442e74d2cea07010b9107b46cdfc1
Maxime JACQUET <mj@openfire.fr>
2024-05-06 09:45:30 +0200

    of_*graphql : [IMP] Mise à plat des fichiers __manifest__.py
    Ajout d'une icone de module maquante sur of_graphql

--- of_planning_graphql/__manifest__.py
+++ of_planning_graphql/__manifest__.py
@@ -1,15 +1,19 @@
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "OF Planning GraphQL",
-    "version": "16.0.1.0.1",
-    "license": "LGPL-3",
+    'name': "OpenFire / OF Planning GraphQL",
+    'version': '16.0.1.0.0',
+    'license': 'AGPL-3',
     'author': "OpenFire",
-    'website': "https://www.openfire.fr",
     'category': "OpenFire",
     'summary': "Ajoute les données de of_planning dans le schéma Graphql",
-    "depends": ["of_graphql", "graphql_base", "of_planning", "of_account_graphql", "of_base_graphql"],
-    "installable": True,
+    'website': "https://www.openfire.fr",
+    'depends': [
+        'of_planning',
+        'of_account_graphql',
+    ],
+    'data': [],
+    'installable': True,
     'application': False,
     'auto_install': True,
 }
