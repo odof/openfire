67410aea35a2a64e215a9596037b2a6c5336df2d
Hounaida <hs@openfire.fr>
2024-05-23 11:51:29 +0200

    of_planning : T16008 - [IMP] Notification des partipants
    On désactive les notifications aux participants dans le cadre d'un
     évènement de type "intervention"

--- of_planning/models/__init__.py
+++ of_planning/models/__init__.py
@@ -1,6 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import account_move_line
+from . import calendar_attendee
 from . import calendar_event
 from . import hr_employee
 from . import res_partner
---
+++ of_planning/models/calendar_attendee.py
@@ -0,0 +1,11 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class Attendee(models.Model):
+    _inherit = 'calendar.attendee'
+
+    def _should_notify_attendee(self):
+        """Override to not notify attendees for interventions"""
+        return super()._should_notify_attendee() and self.event_id.of_type != 'intervention'
