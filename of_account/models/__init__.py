1b20019459cc8a2eef7ea95ff87ea4f7d3f45e07
Maxime JACQUET <mj@openfire.fr>
2024-06-04 10:20:50 +0200

    of_account : [FIX] Initialisation base de données
    Lors de l'initialisation d'une base de données via l'option `--init`
     le champ `of_is_account_warn` n'était pas connu sur la
     table `res_partner`. On ajuste donc l'ordre des imports des modèles.

--- of_account/models/__init__.py
+++ of_account/models/__init__.py
@@ -1,11 +1,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import res_partner
 from . import account_account
 from . import account_journal
 from . import account_move_line
 from . import account_move
 from . import account_payment_term
-from . import res_partner
 from . import product_template
 from . import product_product
 from . import res_company
