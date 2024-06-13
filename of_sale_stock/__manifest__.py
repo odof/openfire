bda9b0780d1edc24fba920e8ee90421fa1c9fdf8
Hounaida <hs@openfire.fr>
2024-06-13 10:41:53 +0200

    of_sale_stock,vof_product : T16082 - [FIX] Typo et vue incorrecte
    Le champ `route_ids` sur `product.category` est amené par le module
     `of_sale_stock`.`
    
    L'ajout des routes dans la vue liste des catégories doit donc se faire
     dans ce module.
    
    Correction d'une erreur de typo dans le nom d'une vue (vrand -> brand).

--- of_sale_stock/__manifest__.py
+++ of_sale_stock/__manifest__.py
@@ -14,6 +14,7 @@
     ],
     'data': [
         'views/sale_order_views.xml',
+        'views/product_category_views.xml',
         'reports/ir_actions_report_templates.xml',
         'reports/of_sale_report_view.xml',
     ],
