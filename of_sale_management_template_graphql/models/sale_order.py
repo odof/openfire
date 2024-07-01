925dc44e578437b27ac4b276f61d5da097a5c92c
fanch <fl@openfire.fr>
2024-07-01 10:11:49 +0200

    of_sale_management_template_graphql : T16488 - [IMP] Position fiscale
    Lors de la création d'un devis depuis le mobile, si le partenaire a une
     position fiscale on l'utilise.
    
    Si le client n'a pas de position fiscale alors c'est la position fiscale
     du modèle de devis qui est utilisée.

--- of_sale_management_template_graphql/models/sale_order.py
+++ of_sale_management_template_graphql/models/sale_order.py
@@ -15,4 +15,25 @@ class SaleOrder(models.Model):
 
         if template := args.get('sale_template'):
             mutation['sale_order_template_id'] = many2one(self=self, model='sale.order.template', input=template)
+
+            if 'fiscal_position_id' not in mutation:
+                if 'partner_id' in mutation:
+                    partner = self.env['res.partner'].browse(mutation['partner_id'])
+                    if partner.property_account_position_id:
+                        position = partner.property_account_position_id.id
+                    else:
+                        position = (
+                            self.env['sale.order.template']
+                            .browse(mutation['sale_order_template_id'])
+                            .of_fiscal_position_id.id
+                        )
+                else:
+                    position = (
+                        self.env['sale.order.template']
+                        .browse(mutation['sale_order_template_id'])
+                        .of_fiscal_position_id.id
+                    )
+
+                mutation['fiscal_position_id'] = position
+
         return mutation
