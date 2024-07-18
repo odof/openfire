ebea531866b7ae4969151de595d1d824fafcd6a0
Saif eddine Issaoui <sei@openfire.fr>
2024-07-18 12:02:40 +0200

    of_contract_custom : T16628 - [ADD] Prise en compte dans la facturation de la date de fin à l'annulation d'une ligne de contrat

--- of_contract_custom/wizards/of_contract_line_cancel_wizard.py
+++ of_contract_custom/wizards/of_contract_line_cancel_wizard.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.exceptions import UserError
+
 from odoo.addons.of_utils.models.of_utils import format_date
 
 
@@ -15,22 +16,34 @@ class OFContractLineCancelWizard(models.TransientModel):
     def onchange_date_end(self):
         lang = self.env['res.lang']._lang_get(self.env.lang or 'fr_FR')
         if self.contract_line_id.contract_id.date_end and self.date_end > self.contract_line_id.contract_id.date_end:
-            raise UserError(u"Vous ne pouvez sélectionner une date de fin supérieure à la date de fin du contrat. "
-                            u"(%s)" % format_date(self.contract_line_id.contract_id.date_end, lang))
-        if self.contract_line_id.last_invoicing_date and self.date_end < self.contract_line_id.last_invoicing_date:
-            raise UserError(u"Vous ne pouvez sélectionner une date de fin antérieure à la date de dernière facturation. "
-                            u"(%s)" % format_date(self.contract_line_id.last_invoicing_date, lang))
-
+            raise UserError(
+                u"Vous ne pouvez sélectionner une date de fin supérieure à la date de fin du contrat. "
+                u"(%s)" % format_date(self.contract_line_id.contract_id.date_end, lang)
+            )
+        if self.contract_line_id.last_invoicing_date and self.date_end and \
+        self.date_end < self.contract_line_id.last_invoicing_date:
+            raise UserError(
+                u"Vous ne pouvez sélectionner une date de fin antérieure à la date de dernière facturation. "
+                u"(%s)" % format_date(self.contract_line_id.last_invoicing_date, lang)
+            )
 
     @api.multi
     def button_end(self):
         lang = self.env['res.lang']._lang_get(self.env.lang or 'fr_FR')
         if self.contract_line_id.contract_id.date_end and self.date_end > self.contract_line_id.contract_id.date_end:
-            raise UserError(u"Vous ne pouvez sélectionner une date de fin supérieure à la date de fin du contrat. "
-                            u"(%s)" % format_date(self.contract_line_id.contract_id.date_end, lang))
-        if self.contract_line_id.last_invoicing_date and self.date_end < self.contract_line_id.last_invoicing_date:
-            raise UserError(u"Vous ne pouvez sélectionner une date de fin antérieure à la date de dernière facturation. "
-                            u"(%s)" % format_date(self.contract_line_id.last_invoicing_date, lang))
-        self.contract_line_id.with_context(no_verification=True).write({
+            raise UserError(
+                u"Vous ne pouvez sélectionner une date de fin supérieure à la date de fin du contrat. "
+                u"(%s)" % format_date(self.contract_line_id.contract_id.date_end, lang)
+            )
+        if self.contract_line_id.last_invoicing_date and self.date_end and \
+        self.date_end < self.contract_line_id.last_invoicing_date:
+            raise UserError(
+                u"Vous ne pouvez sélectionner une date de fin antérieure à la date de dernière facturation. "
+                u"(%s)" % format_date(self.contract_line_id.last_invoicing_date, lang)
+            )
+        data = {
             'date_end'       : self.date_end,
-            })
+        }
+        if not self.date_end or self.date_end > fields.Date.today():
+            data['state'] = 'validated'
+        self.contract_line_id.with_context(no_verification=True).write(data)
