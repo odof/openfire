ea44c482031d5a22bf12e2ca75c64aa82edb9f84
oussama <os@openfire.fr>
2024-08-01 12:26:43 +0200

    of_service : T16817 - [FIX] Recherche DI par intervenants
    Je peux rechercher une DI à partir de ses intervenants, afin de pouvoir filtrer les DI sur cette information

--- of_service/__manifest__.py
+++ of_service/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Demande d'intervention",
-    'version': '16.0.1.1.1',
+    'version': '16.0.1.1.2',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'category': "OpenFire",
