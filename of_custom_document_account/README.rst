a9ee7ddb8542bcfef08f972f517b7079eebfeba5
oussama <os@openfire.fr>
2024-08-05 18:55:49 +0200

    of_custom_document_account : T16830 - [ADD]  Ajout d'un nouveau module
    Dans une facture client/fournisseur, quand j'ai sélectionné des documents personnalisés, alors je retrouve ces documents à la suite de la facture lorsque je l'imprime

---
+++ of_custom_document_account/README.rst
@@ -0,0 +1,8 @@
+===================================
+OF custom documents for account move
+===================================
+
+Rapports personnalisés dans les factures clients/fournisseurs PDF :
+-----------------------------------------------------
+Ce module permet de sélectionner des documents personnalisés dans les factures clients/fournissuers pour les imprimer
+conjointement avec les facture clients/fournisseurs PDF.
---
+++ of_custom_document_account/models/account_move.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    _name = "account.move"
+    _inherit = ['account.move', 'of.custom.document.mixin']
+
+    of_custom_document_ids = fields.Many2many(
+        comodel_name='of.custom.document',
+        string="Custom documents",
+        help="Documents to include into pdf reports.",
+    )
+
+    @api.model
+    def _allowed_reports(self):
+        return ['account.report_invoice', 'account.report_invoice_with_payments', 'account.report_original_vendor_bill']
