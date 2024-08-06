6f4dc68b265129ed6bd17003d747ae0afb049f63
fanch <fl@openfire.fr>
2024-08-06 16:31:39 +0200

    of_account_payment, of_sale_advance_payment : T16524 - [IMP] Wizard SaP
    Ajustement du wizard de paiement du module `Sale Advance Payment`
     pour mieux correspondre à nos besoins et l'unifier avec le wizard
     de paiement disponible depuis une facture.

--- of_account_payment/__init__.py
+++ of_account_payment/__init__.py
@@ -3,4 +3,4 @@
 from .hooks import post_init_hook
 
 from . import models
-from . import wizard
+from . import wizards
--- of_account_payment/wizards/account_payment_register.py
+++ of_account_payment/wizards/account_payment_register.py
@@ -1,5 +1,3 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from odoo import api, fields, models
 
 
@@ -14,12 +12,6 @@ class AccountPaymentRegister(models.TransientModel):
     of_payment_ref = fields.Char(size=64, string="Payment reference")
     of_tag_ids = fields.Many2many(comodel_name="of.payment.tags", string="Payment tags")
 
-    def _compute_payment_method_line_id(self):
-        super()._compute_payment_method_line_id()
-        for record in self:
-            if record.of_payment_mode_id:
-                record.payment_method_line_id = record.of_payment_mode_id.payment_method_line_id.id
-
     @api.depends("of_payment_mode_id")
     def _compute_journal_id(self):
         super()._compute_journal_id()
@@ -32,6 +24,6 @@ class AccountPaymentRegister(models.TransientModel):
     def _create_payment_vals_from_wizard(self, batch_result):
         payment_vals = super()._create_payment_vals_from_wizard(batch_result)
         payment_vals["of_payment_mode_id"] = self.of_payment_mode_id.id
-        payment_vals["payment_reference"] = self.of_payment_ref
+        payment_vals["of_payment_ref"] = self.of_payment_ref
         payment_vals["of_tag_ids"] = self.of_tag_ids.ids
         return payment_vals
