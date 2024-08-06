53074e9d211f2d03aee129d07f7b517d9be51b08
fanch <fl@openfire.fr>
2024-08-06 13:28:40 +0200

    of_account_payment : T16573 - [ADD][IMP] modification de module
    Ajout de model "of.payment.tags"
    Traduction des termes
    Ajout de champs manquants

--- of_account_payment/models/of_payment_mode.py
+++ of_account_payment/models/of_payment_mode.py
@@ -6,10 +6,13 @@ from odoo import api, fields, models
 class OFPaymentMode(models.Model):
     _name = 'of.payment.mode'
     _description = "Payment Mode"
+    _order = "journal_id asc"
 
     name = fields.Char(compute='_compute_name')
     shortname = fields.Char(related='payment_method_line_id.name', string="Short Name")
-    journal_id = fields.Many2one(comodel_name='account.journal', string="Journal", required=True, ondelete='cascade')
+    journal_id = fields.Many2one(
+        comodel_name='account.journal', string="Journal", required=True, ondelete='cascade', _order="id asc"
+    )
     payment_method_line_id = fields.Many2one(
         comodel_name='account.payment.method.line', string="Payment Method Line", required=True, ondelete='cascade'
     )
