80a57cb3d5d3890cc9e36ee7c1c58dfd12598802
fanch <fl@openfire.fr>
2024-08-06 16:31:39 +0200

    of_account_payment, of_sale_advance_payment : T16524 - [IMP] Wizard SaP
    Ajustement du wizard de paiement du module `Sale Advance Payment`
     pour mieux correspondre à nos besoins et l'unifier avec le wizard
     de paiement disponible depuis une facture.

--- of_account_payment/__init__.py
+++ of_account_payment/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from .hooks import post_init_hook
 
 from . import models
-from . import wizard
+from . import wizards
--- of_sale_advance_payment/__manifest__.py
+++ of_sale_advance_payment/__manifest__.py
@@ -1,21 +1,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
-    'name': "Openfire / Paiement d'avance de vente",
+    'name': "OpenFire / OF Sale Advance Payment",
     'version': '16.0.1.0.0',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
-    'category': 'OpenFire',
-    'summary': "Module d'extension pour les paiements d'avance sur les ventes",
+    'category': "OpenFire",
+    'summary': "Personnalisation des modules de OCA ",
     'depends': [
-        'of_sale',
         'sale_advance_payment',
+        'of_sale',
+        'of_account_payment',
     ],
     'data': [
         'views/sale_order_views.xml',
         'views/account_payment_views.xml',
+        'wizards/sale_advance_payment_wizard_views.xml',
     ],
     'installable': True,
     'application': False,
-    'auto_install': True,
+    'auto_install': False,
 }
