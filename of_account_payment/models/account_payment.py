a6b56e7be2367eb4340379904bc63e03e5efae53
fanch <fl@openfire.fr>
2024-08-06 13:28:40 +0200

    of_account_payment : T16573 - [ADD/IMP] Formulaire de paiement client
    Revue du formulaire de paiement client pour simplifier son utilisation
     et ajouter des fonctionnalités liées à OpenFire.

--- of_account_payment/models/account_payment.py
+++ of_account_payment/models/account_payment.py
@@ -7,8 +7,18 @@ from odoo import api, fields, models
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
-    of_expected_deposit_date = fields.Date(string="Scheduled delivery date")
-    of_payment_mode_id = fields.Many2one(comodel_name="of.payment.mode", string="Payment Mode")
+    of_payment_mode_id = fields.Many2one(
+        comodel_name='of.payment.mode', string="Payment Mode", domain="[('payment_type', '=', payment_type)]"
+    )
+    of_ref_reglement = fields.Char(size=64, string="Payment reference")
+    of_tag_ids = fields.Many2many(comodel_name='of.payment.tags', string="Payment tags")
+
+    @api.onchange('of_payment_mode_id')
+    def _onchange_of_payment_mode_id(self):
+        if self.of_payment_mode_id:
+            self.journal_id = self.of_payment_mode_id.journal_id
+        else:
+            self.journal_id = False
 
     @api.model_create_multi
     def create(self, vals_list):
