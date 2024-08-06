b663b05ce5145019db6f9456d2125cd4bd7fb8d9
fanch <fl@openfire.fr>
2024-08-06 16:31:39 +0200

    of_account_payment, of_sale_advance_payment : T16524 - [IMP] Wizard SaP
    Ajustement du wizard de paiement du module `Sale Advance Payment`
     pour mieux correspondre à nos besoins et l'unifier avec le wizard
     de paiement disponible depuis une facture.

--- of_account_payment/wizards/account_payment_register.py
+++ of_account_payment/wizards/account_payment_register.py
@@ -11,7 +11,7 @@ class AccountPaymentRegister(models.TransientModel):
         string="Payment Mode",
         domain="[('payment_type', '=', payment_type)]",
     )
-    of_ref_reglement = fields.Char(size=64, string="Payment reference")
+    of_payment_ref = fields.Char(size=64, string="Payment reference")
     of_tag_ids = fields.Many2many(comodel_name="of.payment.tags", string="Payment tags")
 
     def _compute_payment_method_line_id(self):
@@ -20,16 +20,18 @@ class AccountPaymentRegister(models.TransientModel):
             if record.of_payment_mode_id:
                 record.payment_method_line_id = record.of_payment_mode_id.payment_method_line_id.id
 
-    @api.onchange("of_payment_mode_id")
-    def _onchange_of_payment_mode_id(self):
-        if self.of_payment_mode_id:
-            self.journal_id = self.of_payment_mode_id.journal_id
-        else:
-            self.journal_id = False
+    @api.depends("of_payment_mode_id")
+    def _compute_journal_id(self):
+        super()._compute_journal_id()
+        for wizard in self:
+            if wizard.of_payment_mode_id:
+                wizard.journal_id = wizard.of_payment_mode_id.journal_id
+            else:
+                wizard.journal_id = False
 
     def _create_payment_vals_from_wizard(self, batch_result):
         payment_vals = super()._create_payment_vals_from_wizard(batch_result)
         payment_vals["of_payment_mode_id"] = self.of_payment_mode_id.id
-        payment_vals["of_ref_reglement"] = self.of_ref_reglement
+        payment_vals["of_payment_ref"] = self.of_payment_ref
         payment_vals["of_tag_ids"] = self.of_tag_ids.ids
         return payment_vals
