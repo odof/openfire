fc6940ad8f2e85e4870e35d877cb75bd37212ded
fanch <fl@openfire.fr>
2024-08-09 12:15:37 +0200

    of_account_payment : T16607- [MIG] Migration de modèle 'of.payment.tags'

---
+++ of_account_payment/models/of_payment_tags.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class OFPaymentTags(models.Model):
+    """Tags of payment"""
+
+    _name = "of.payment.tags"
+    _description = "Payment Category"
+
+    name = fields.Char(string="Category name", help="Category name", translate=True)
+    color = fields.Integer()
+    description = fields.Text(string="Category description", help="Payment category description")
+
+    _sql_constraints = [('name_uniq', 'unique (name)', "The category name already exists!")]
