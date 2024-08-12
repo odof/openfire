39dbb56544f9fc9b2e80043621fee51f432d7560
Hounaida <hs@openfire.fr>
2024-08-12 11:50:32 +0200

    of_planning_survey : T16832 - [IMP] Ne pas afficher les réponses vides

--- of_planning_survey/__manifest__.py
+++ of_planning_survey/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Questionnaires intervention",
-    'version': '16.0.1.1.0',
+    'version': '16.0.1.2.0',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'category': "OpenFire",
