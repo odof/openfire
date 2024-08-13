52ce4135c887f9d4735caf898a7deafa551f4898
Hounaida <hs@openfire.fr>
2024-08-13 17:14:57 +0200

    of_account : T16882 - [IMP] Mode de paiement
    Refonte des vues de mode de paiement. Masquage du menu mode de paiement lorsqu'on n'est pas en mode debug.

--- of_account/__manifest__.py
+++ of_account/__manifest__.py
@@ -23,6 +23,7 @@
         'views/account_move_views.xml',
         'views/account_move_line_views.xml',
         'views/account_payment_term_views.xml',
+        'views/account_payment_method_line_views.xml',
         'views/res_partner_views.xml',
         'views/res_company_views.xml',
         'views/res_config_settings_views.xml',
