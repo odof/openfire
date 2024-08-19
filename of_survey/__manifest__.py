7e11fe6160b65aff602d73cf75a18be823c74b52
oussama <os@openfire.fr>
2024-08-19 15:50:32 +0200

    of_survey : T16782 - [FIX]  Affichage conditionnel
     Quand j'ajoute une condition mais que je ne renseigne pas de question ou de réponse, alors la condition est désactivée quand je sauvegarde le questionnaire

--- of_survey/__manifest__.py
+++ of_survey/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Questionnaires",
-    'version': '16.0.1.1.2',
+    'version': '16.0.1.1.3',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
