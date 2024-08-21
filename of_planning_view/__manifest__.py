d1bb7477270939db0d4d7eeb567b613ca015bce3
oussama <os@openfire.fr>
2024-08-21 12:41:32 +0200

    of_planning* : T16664 - [IMP] Popup vue calendrier des interventions
    On a désactivé l'action rapide de création depuis le calendrier pour
     permettre l'utilisation de la vue formulaire complète afin de faciliter
     la saisie d'une intervention depuis ce dernier.

--- of_planning_view/__manifest__.py
+++ of_planning_view/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Planning d'intervention",
-    'version': '16.0.1.0.1',
+    'version': '16.0.1.1.1',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
