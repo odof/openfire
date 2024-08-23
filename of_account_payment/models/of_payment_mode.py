86163a525e91695cd24991833e7745a8a6e0dc37
Maxime JACQUET <mj@openfire.fr>
2024-08-23 13:27:54 +0200

    [FIXUP] Modification revue

--- of_account_payment/models/of_payment_mode.py
+++ of_account_payment/models/of_payment_mode.py
@@ -6,13 +6,11 @@ from odoo import api, fields, models
 class OFPaymentMode(models.Model):
     _name = 'of.payment.mode'
     _description = "Payment Mode"
-    _order = "journal_id asc"
+    _order = 'journal_id asc'
 
     name = fields.Char(compute='_compute_name')
     shortname = fields.Char(related='payment_method_line_id.name', string="Short Name")
-    journal_id = fields.Many2one(
-        comodel_name='account.journal', string="Journal", required=True, ondelete='cascade', _order="id asc"
-    )
+    journal_id = fields.Many2one(comodel_name='account.journal', string="Journal", required=True, ondelete='cascade')
     payment_method_line_id = fields.Many2one(
         comodel_name='account.payment.method.line', string="Payment Method Line", required=True, ondelete='cascade'
     )
