cb3991eacde0fc08aa8e7f40e57c933c6b40d3c8
Archibald JEGO <aj@openfire.fr>
2024-08-30 17:24:32 +0200

    of_* : T16862 - [IMP/ADD] Modification vue form Devis/Commande
    Réorganisation de la vue devis/commande pour une meilleure lisibilité et
     une meilleure ergonomie.
    
    Ajout des modules d'extension OCA comme `of_sale_advance_payment` et
     `of_sale_product_matrix` pour ajuster les vues devis/commande en
     conséquence.

--- of_sale_advance_payment/__manifest__.py
+++ of_sale_advance_payment/__manifest__.py
@@ -1,20 +1,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "Openfire / Commentaires de vente",
-    'version': '16.0.1.1.0',
+    'name': "Openfire / Paiement d'avance de vente",
+    'version': '16.0.1.0.0',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
     'category': 'OpenFire',
-    'summary': "Module d'extension pour les commentaires de vente",
+    'summary': "Module d'extension pour les paiements d'avance sur les ventes",
     'depends': [
-        'of_base_comment_template',
         'of_sale',
-        'sale_comment_template',
+        'sale_advance_payment',
     ],
     'data': [
-        'data/ir_config_parameter.xml',
-        'views/res_config_settings_views.xml',
         'views/sale_order_views.xml',
     ],
     'installable': True,
---
+++ of_sale_product_matrix/README.rst
@@ -0,0 +1,13 @@
+=======================
+OF sale advance payment
+=======================
+
+Module d'extension du module `sale_product_matrix` afin de modifier son fonctionnement pour correspondre à nos besoins.
+
+
+
+Modification de vue
+###################
+
+
+* Déplacement du champ **Imprimer la grille des variantes**
