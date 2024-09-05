56db4ccdccccdf6d808bfabedb44ac11ad15c328
oussama <os@openfire.fr>
2024-09-05 11:20:28 +0200

    of_account_invoice_report_payment_info : T16898 - [ADD] Note de paiement
    Configurer une note qui précise qu'une facture est payé dans le wizard
     d'édition d'un mode de paiement (depuis un journal de type "banque"
     ou "espèce")

---
+++ of_account_invoice_report_payment_info/hooks.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import SUPERUSER_ID, api
+
+
+def post_init_hook(cr, registry):
+    """Add display config mode on all method lines"""
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    methods_to_update = env["account.payment.method.line"].search([])
+    methods_to_update.write({"of_display_config": "{payment_amount} payé par {payment_mode} le {date}"})
+
+
+def uninstall_hook(cr, registry):
+    """Reset the display config mode to default OCA value"""
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["ir.config_parameter"].sudo().set_param("account_invoice_report_payment_info.info_pattern", "({move_ref})")
---
+++ of_account_invoice_report_payment_info/models/account_payment_method_line.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountPaymentMethodLine(models.Model):
+    _inherit = "account.payment.method.line"
+
+    @api.model
+    def _get_default_of_display_config(self):
+        config_parameter_obj = self.env["ir.config_parameter"].sudo()
+        return config_parameter_obj.get_param("account_invoice_report_payment_info.info_pattern", default="")
+
+    of_display_config = fields.Char(string="Paid Invoice Configuration", default=_get_default_of_display_config)
