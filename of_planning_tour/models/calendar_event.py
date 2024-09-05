f40d7a2bc9f910d01c0e00fea35043351a9c7e72
Archibald JEGO <aj@openfire.fr>
2024-09-05 17:08:51 +0200

    of_planning_tour: T17034 - [FIX] Générerer tournée à la volée
    Lorsque qu'aucun technicien n'est sélectionné dans la recherche de
     créneaux, on génère à la volée les tournées des employés capables de
     réaliser la tâche.

--- of_planning_tour/models/calendar_event.py
+++ of_planning_tour/models/calendar_event.py
@@ -564,25 +564,7 @@ class CalendarEvent(models.Model):
 
         dates_eval = self._get_tour_dates()
         address = self.of_address_id
-
-        for employee in self.of_employee_ids:
-            for date_eval in dates_eval:
-                tour = tour_obj.search([('date', '=', date_eval), ('employee_id', '=', employee.id)], limit=1)
-                address_sector = address.of_tech_sector_id
-                if not tour:
-                    tour = tour_obj.create(
-                        {
-                            'date': date_eval,
-                            'employee_id': employee.id,
-                            'sector_ids': [Command.set([address_sector.id])] if address_sector else False,
-                        }
-                    )
-                elif not tour.sector_ids and address_sector:
-                    tour.sector_ids = [Command.set([address_sector.id])]
-                elif address_sector and address_sector.id not in tour.sector_ids.ids:
-                    tour.sector_ids = [Command.link(address_sector.id)]
-                tours += tour
-        return tours
+        return tour_obj._create_tours_for_employees(self.of_employee_ids, dates_eval, address.of_tech_sector_id)
 
     def _events_unlink_get_tours_to_recompute(self):
         """
