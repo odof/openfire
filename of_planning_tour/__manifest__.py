f6a4ec5b1799c1c12b538be6078e31172c0f5e5a
Archibald JEGO <aj@openfire.fr>
2024-09-05 17:08:51 +0200

    of_planning_tour: T17034 - [FIX] Générerer tournée à la volée
    Lorsque qu'aucun technicien n'est sélectionné dans la recherche de
     créneaux, on génère à la volée les tournées des employés capables de
     réaliser la tâche.

--- of_planning_tour/__manifest__.py
+++ of_planning_tour/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Planning des tournées",
-    'version': '16.0.1.1.1',
+    'version': '16.0.1.1.2',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
