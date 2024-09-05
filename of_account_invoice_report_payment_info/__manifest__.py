99ad34317decf3f50f70b72ea8fc0db618cb0c9a
oussama <os@openfire.fr>
2024-09-05 11:20:28 +0200

    of_account_invoice_report_payment_info : T16898 - [ADD] Note de paiement
    Configurer une note qui précise qu'une facture est payé dans le wizard
     d'édition d'un mode de paiement (depuis un journal de type "banque"
     ou "espèce")

--- of_account_invoice_report_payment_info/__manifest__.py
+++ of_account_invoice_report_payment_info/__manifest__.py
@@ -1,25 +1,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "OpenFire / Information étendue sur les factures payées",
-    "version": "16.0.1.0.1",
-    "license": "AGPL-3",
-    "author": "OpenFire",
-    "website": "https://www.openfire.fr",
-    "category": "OpenFire",
-    "summary": "Personnalisation du module OCA Account Invoice Report Payment Extended Info",
-    "depends": [
-        "account_invoice_report_payment_info",
-        "of_account_payment",
+    'name': "OpenFire / Information étendue sur les factures payées",
+    'version': '16.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "OpenFire",
+    'website': "https://www.openfire.fr",
+    'category': "OpenFire",
+    'summary': "Personnalisation du module OCA Account Invoice Report Payment Extended Info",
+    'depends': [
+        'account_invoice_report_payment_info',
+        'of_account_payment',
     ],
-    "data": [
-        "data/data.xml",
-        "views/account_payment_method_line_views.xml",
-        "reports/report_account_move.xml",
+    'data': [
+        'data/data.xml',
+        'views/account_payment_method_line_views.xml',
+        'reports/report_account_move.xml',
     ],
-    "installable": True,
-    "application": False,
-    "auto_install": True,
-    "post_init_hook": "post_init_hook",
-    "uninstall_hook": "uninstall_hook",
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'post_init_hook': 'post_init_hook',
+    'uninstall_hook': 'uninstall_hook',
 }
