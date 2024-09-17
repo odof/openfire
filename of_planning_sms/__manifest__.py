dd999e2529323d8740632ea5cf7fa55e6dee30e5
fanch <fl@openfire.fr>
2024-09-17 10:25:24 +0200

    of_sms, of_*_sms : [MIG] Migration 16.0
    Ajout de nos besoins dans le standard Odoo (gestion sms commerciaux,
     envoi différés, expéditeurs différents)
    
    Déplacement des actions dans des modules séparés : of_sale_sms,
     of_account_sms, of_planning_sms
    
    ---
    
    Tasks : T17004

--- of_planning_sms/__manifest__.py
+++ of_planning_sms/__manifest__.py
@@ -1,25 +1,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / SMS",
+    'name': "OpenFire / SMS Planning",
     'version': "16.0.1.0.0",
     'license': 'AGPL-3',
     'author': "OpenFire",
     'category': "OpenFire",
-    'summary': "Module de SMS OpenFire",
+    'summary': "Module de SMS OpenFire pour le planning",
     'website': "https://www.openfire.fr",
     'depends': [
-        'sms_ovh_http',
-        'of_base',
-        'mail',
+        'of_sms',
+        'of_planning',
     ],
     'data': [
-        'security/ir.model.access.csv',
         'data/ir_cron.xml',
-        'views/of_sms_sender_views.xml',
-        'views/res_partner_views.xml',
-        'views/sms_sms_views.xml',
-        'wizards/sms_composer_views.xml',
+        'data/sms_template.xml',
+        'data/ir_actions_server.xml',
+        'views/res_config_settings_views.xml',
     ],
     'installable': True,
     'application': False,
---
+++ of_sms/README.rst
@@ -0,0 +1,32 @@
+======
+OF SMS
+======
+
+
+Fonctionnalités
+###############
+
+
+Expéditeur (`of.sms.sender`)
+----------------------------
+
+*  Ajout d'un modèle pour les expéditeurs de SMS avec les champs suivants :
+    - Nom;
+    - Nom de l'expéditeur;
+    - Modèle;
+
+* Cela permet de définir des expéditeurs de SMS en fonction du modèle Odoo (partenaire, intervention, etc.).
+
+
+SMS (`sms.sms`)
+---------------
+
+* Ajout de champs pour les SMS :
+  - Expéditeur;
+  - Est commercial;
+  - Date d'envoi;
+
+* Ajout d'un état "À envoyer" pour les SMS qui n'ont pas encore été envoyés.
+
+* Ajout d'une action planifiée pour l'envoi des SMS différés
+  - Les SMS sont changés de statut de "À envoyer" à "Dans la file d'attente" et seront envoyés à la prochaine exécution de l'action planifiée qui envoie les SMS.
