e5b578bd24562c8200e2e68a3e39700ecdf01c61
Archibald JEGO <aj@openfire.fr>
2024-09-20 15:27:33 +0200

    of_base : T17145 - [IMP] URLs Préférences utilisateurs
    On remplace les urls `Documentation` et `Assistance` et on retire le
     menu `Mon Compte`.

--- of_base/static/src/js/user_menu_item.js
+++ of_base/static/src/js/user_menu_item.js
@@ -38,5 +38,6 @@ function supportItemOF(env) {
 
 registry.category("user_menuitems").remove("documentation");
 registry.category("user_menuitems").remove("support");
+registry.category("user_menuitems").remove("odoo_account");
 registry.category("user_menuitems").add("documentation", documentationItemOF);
 registry.category("user_menuitems").add("support", supportItemOF);
