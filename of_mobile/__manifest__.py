8c8270cbc087fd68f9c9359c8d9b5052479e9a11
Mickael Renault <mr@openfire.fr>
2024-09-23 19:59:35 +0200

    of_mobile : T17124 - [FIX] États DI planifiées depuis le mobile

--- of_mobile/__manifest__.py
+++ of_mobile/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Mobile",
-    'version': '16.0.1.0.2',
+    'version': '16.0.1.0.3',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
