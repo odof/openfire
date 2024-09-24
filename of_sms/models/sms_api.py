5d734df165b61ea095e184947ee4ab4181b00cd2
fanch <fl@openfire.fr>
2024-09-24 10:45:47 +0200

    of_sms : [FiX] SMS - envoi par OVH
    Le message partait avec l’id du message au lieu du message en lui même.

--- of_sms/models/sms_api.py
+++ of_sms/models/sms_api.py
@@ -10,12 +10,13 @@ OVH_HTTP_ENDPOINT = "https://www.ovh.com/cgi-bin/sms/http2sms.cgi"
 class SmsApi(models.AbstractModel):
     _inherit = 'sms.api'
 
-    def _prepare_ovh_http_params(self, account, message, sms_id):
-        params = super()._prepare_ovh_http_params(account, message, sms_id)
+    def _prepare_ovh_http_params(self, account, number, message, sms_id):
         sms = self.env['sms.sms'].browse(sms_id)
+        params = super()._prepare_ovh_http_params(account, sms.number, message)
+
         params.update(
             {
-                'from': sms.of_sender_id.name or account.sms_ovh_http_from,
+                'from': sms.of_sender_id.sender_name or account.sms_ovh_http_from,
                 'noStop': '0' if sms.of_is_commercial else '1',
                 'to': sms.number,
             }
@@ -28,7 +29,9 @@ class SmsApi(models.AbstractModel):
         if not number:
             return 'wrong_number_format'
         account = self._get_sms_account()
-        r = requests.get(OVH_HTTP_ENDPOINT, params=self._prepare_ovh_http_params(account, message, sms_id), timeout=60)
+        r = requests.get(
+            OVH_HTTP_ENDPOINT, params=self._prepare_ovh_http_params(account, number, message, sms_id), timeout=60
+        )
         response = r.text
         if response[:2] != 'OK':
             self.env["sms.sms"].browse(sms_id).error_detail = response
