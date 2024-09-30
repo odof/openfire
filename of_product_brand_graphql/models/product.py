0198301caa6e96f589e8aa88fe8d377f9312d2c9
Maxime JACQUET <mj@openfire.fr>
2024-09-30 09:07:05 +0200

    of_*, .gitlab-ci.yml - [LINT] Black, Bandit, Flake8
    Mise à jour de notre configuration de la CI gitlab pour permettre à
     Black de gérer le formatage des strings.
    
    Suite à un point de normes OF nous avons décidé de laisser Black gérer
     les strings pour nous.
    
    Ce commit modifie donc tous les formatage de strings suite au
     changement de la configuration de Black mais également des ajouts
     d'éléments `#noqa` et `#nosec`pour pour les points remontés par Flake8
     et Bandit dans ce commit de linting.

--- of_product_brand_graphql/models/product.py
+++ of_product_brand_graphql/models/product.py
@@ -6,13 +6,13 @@ from odoo.addons.of_graphql.graphql.odoo_graphql import many2one
 
 
 class Product(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
     @api.model
     def _prepare_mutation_values(self, **args):
         mutation = super()._prepare_mutation_values(**args)
 
-        if brand := args.get('brand'):
-            mutation['brand_id'] = many2one(self=self, model='of.product.brand', input=brand)
+        if brand := args.get("brand"):
+            mutation["brand_id"] = many2one(self=self, model="of.product.brand", input=brand)
 
         return mutation
