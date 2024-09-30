fbf891a3cde1642d4846b11f926756ca79acde7f
Maxime JACQUET <mj@openfire.fr>
2024-09-30 09:07:05 +0200

    of_*, .gitlab-ci.yml - [LINT] Black, Bandit, Flake8
    Mise à jour de notre configuration de la CI gitlab pour permettre à
     Black de gérer le formatage des strings.
    
    Suite à un point de normes OF nous avons décidé de laisser Black gérer
     les strings pour nous.
    
    Ce commit modifie donc tous les formatage de strings suite au
     changement de la configuration de Black mais également des ajouts
     d'éléments `#noqa` et `#nosec`pour pour les points remontés par Flake8
     et Bandit dans ce commit de linting.

--- of_import/models/of_import_product_categ_config.py
+++ of_import/models/of_import_product_categ_config.py
@@ -4,24 +4,24 @@ from odoo import api, fields, models
 
 
 class OfImportProductCategConfig(models.Model):
-    _name = 'of.import.product.categ.config'
-    _inherit = 'of.import.product.config.template'
-    _order = 'categ_origin'
+    _name = "of.import.product.categ.config"
+    _inherit = "of.import.product.config.template"
+    _order = "categ_origin"
 
-    brand_id = fields.Many2one(comodel_name='of.product.brand', required=True, string="Brand")
+    brand_id = fields.Many2one(comodel_name="of.product.brand", required=True, string="Brand")
     categ_origin = fields.Char(string="Origin Category", required=True)
-    product_ids = fields.One2many(comodel_name='product.template', string="Products", compute='_compute_product_ids')
+    product_ids = fields.One2many(comodel_name="product.template", string="Products", compute="_compute_product_ids")
 
-    @api.depends('categ_origin', 'brand_id.product_ids', 'brand_id.product_ids.of_seller_product_category_name')
+    @api.depends("categ_origin", "brand_id.product_ids", "brand_id.product_ids.of_seller_product_category_name")
     def _compute_product_ids(self):
-        product_obj = self.env['product.template']
+        product_obj = self.env["product.template"]
         for categ in self:
             categ.product_ids = product_obj.search(
-                [('brand_id', '=', categ.brand_id.id), ('seller_ids.of_product_category_name', '=', categ.categ_origin)]
+                [("brand_id", "=", categ.brand_id.id), ("seller_ids.of_product_category_name", "=", categ.categ_origin)]
             )
 
     _sql_constraints = [
-        ('categ_origin_uniq', 'unique(brand_id, categ_origin)', "A product category can only be entered once."),
+        ("categ_origin_uniq", "unique(brand_id, categ_origin)", "A product category can only be entered once."),
     ]
 
     def action_button_update_products(self):
@@ -29,4 +29,4 @@ class OfImportProductCategConfig(models.Model):
         Recalcule les champs des articles en fonction de la configuration de la marque
         et des paramètres d'import de l'article (dans product_supplierinfo)
         """
-        self.mapped('product_ids').action_button_update_from_brand()
+        self.mapped("product_ids").action_button_update_from_brand()
