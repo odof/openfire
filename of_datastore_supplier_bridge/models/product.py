1f455a8637bc195da137e9e7589bf63d2893de39
Hounaida <hs@openfire.fr>
2024-10-08 16:19:00 +0200

    of_datastore_supplier_bridge : T17340 - [MIG] Migration 16.0

--- of_datastore_supplier_bridge/models/product.py
+++ of_datastore_supplier_bridge/models/product.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    # if both of_datastore_supplier and of_datastore_product are installed, we want these fields not to be readonly
-    prochain_tarif = fields.Float(readonly=False)
-    date_prochain_tarif = fields.Date(readonly=False)
+    # if both `of_datastore_supplier` and `of_datastore_product` are installed, we want these fields not to be readonly
+    of_prochain_tarif = fields.Float(readonly=False)
+    of_date_prochain_tarif = fields.Date(readonly=False)
