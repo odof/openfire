9f10c38855b6fde8243822ae080cca1fd2309829
Maxime JACQUET <mj@openfire.fr>
2024-10-09 16:32:19 +0200

    of_datastore_common_sp : [CLEAN] Black

--- of_datastore_common_sp/models/purchase_order.py
+++ of_datastore_common_sp/models/purchase_order.py
@@ -3,25 +3,25 @@ from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     of_datastore_anomaly = fields.Boolean(
-        string="In Anomaly", compute='_compute_of_datastore_anomaly', search='_search_of_datastore_anomaly'
+        string="In Anomaly", compute="_compute_of_datastore_anomaly", search="_search_of_datastore_anomaly"
     )
 
-    @api.depends('picking_ids', 'picking_ids.of_datastore_anomaly')
+    @api.depends("picking_ids", "picking_ids.of_datastore_anomaly")
     def _compute_of_datastore_anomaly(self):
         for purchase in self:
-            purchase.of_datastore_anomaly = any(purchase.picking_ids.mapped('of_datastore_anomaly'))
+            purchase.of_datastore_anomaly = any(purchase.picking_ids.mapped("of_datastore_anomaly"))
 
     @api.model
     def _search_of_datastore_anomaly(self, operator, value):
         orders = (
-            self.env['stock.picking']
-            .search([('of_datastore_anomaly', operator, value)])
-            .mapped('move_lines')
-            .mapped('procurement_id')
-            .mapped('purchase_line_id')
-            .mapped('order_id')
+            self.env["stock.picking"]
+            .search([("of_datastore_anomaly", operator, value)])
+            .mapped("move_lines")
+            .mapped("procurement_id")
+            .mapped("purchase_line_id")
+            .mapped("order_id")
         )
-        return [('id', 'in', orders.ids)]
+        return [("id", "in", orders.ids)]
--- of_datastore_common_sp/models/stock_picking.py
+++ of_datastore_common_sp/models/stock_picking.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     of_datastore_anomaly = fields.Boolean(string="In Anomaly")
     of_datastore_id = fields.Integer(string="Transfer voucher ID connected base")
