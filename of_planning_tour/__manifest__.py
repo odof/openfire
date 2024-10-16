6262f9de19aa72de18e0b996bbb790776b7aa226
Hounaida <hs@openfire.fr>
2024-10-16 10:09:01 +0200

    of_planning_tour : T17345 - [FIX] Filtre semaine actuelle
    Il y avait une erreur concernant l'interprétation du filtre pour la
     semaine actuelle des tournées.

--- of_planning_tour/__manifest__.py
+++ of_planning_tour/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Planning des tournées",
-    'version': '16.0.1.1.3',
+    'version': '16.0.1.1.4',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
