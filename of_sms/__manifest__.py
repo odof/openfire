673366b9b034d2512d4156c9208fb5712a774a5a
Hounaida <hs@openfire.fr>
2024-10-16 10:34:08 +0200

    of_sms : T17369 - [IMP] cacher le champ mobile dans la fiche contact

--- of_sms/__manifest__.py
+++ of_sms/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / SMS",
-    'version': "16.0.1.1.1",
+    'version': "16.0.1.2.0",
     'license': 'AGPL-3',
     'author': "OpenFire",
     'category': "OpenFire",
@@ -18,7 +18,6 @@
         'data/ir_cron.xml',
         'data/of_sms_sender.xml',
         'views/of_sms_sender_views.xml',
-        'views/res_partner_views.xml',
         'views/sms_sms_views.xml',
         'wizards/sms_composer_views.xml',
     ],
