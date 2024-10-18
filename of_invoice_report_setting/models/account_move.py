28dc637566955beeca592ee47b8ee3a165d79f76
Maxime JACQUET <mj@openfire.fr>
2024-10-18 10:06:44 +0200

    of_* : [CLEAN] Black

--- of_invoice_report_setting/models/account_move.py
+++ of_invoice_report_setting/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import models
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     # ---------------------------------------------------------
     # Helper methods for QWeb reports
--- of_invoice_report_setting/models/res_config_settings.py
+++ of_invoice_report_setting/models/res_config_settings.py
@@ -4,16 +4,16 @@ from odoo import _, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     def action_button_invoice_printings_params(self):
         return {
-            'name': _("Configure PDF printing"),
-            'type': 'ir.actions.act_window',
-            'res_model': 'of.invoice.document.layout',
-            'view_mode': 'form',
-            'view_type': 'form',
-            'view_id': self.env.ref('of_invoice_report_setting.of_invoice_document_layout_view_form').id,
-            'target': 'new',
-            'context': self.env.context.copy(),
+            "name": _("Configure PDF printing"),
+            "type": "ir.actions.act_window",
+            "res_model": "of.invoice.document.layout",
+            "view_mode": "form",
+            "view_type": "form",
+            "view_id": self.env.ref("of_invoice_report_setting.of_invoice_document_layout_view_form").id,
+            "target": "new",
+            "context": self.env.context.copy(),
         }
