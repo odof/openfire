3fe9f24ae107bb93c49226f69f5b15286ba7d139
Maxime JACQUET <mj@openfire.fr>
2024-10-18 10:06:44 +0200

    of_* : [CLEAN] Black

--- of_invoice_report_setting/models/account_move_line.py
+++ of_invoice_report_setting/models/account_move_line.py
@@ -6,9 +6,9 @@ from odoo import fields, models
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
-    of_display_name = fields.Text(string="Display name for reports", compute='_compute_of_display_name')
+    of_display_name = fields.Text(string="Display name for reports", compute="_compute_of_display_name")
 
     def _compute_of_display_name(self):
         """Compute the display name for the invoice report"""
@@ -17,6 +17,6 @@ class AccountMoveLine(models.Model):
                 line.of_display_name = line.name
             else:
                 name = line.with_context(lang=line.move_id.partner_id.lang, partner=line.move_id.partner_id.id).name
-                if match := re.search(r'\[(.*?)\]', name):
-                    name = name.replace(match[0], '').strip()
+                if match := re.search(r"\[(.*?)\]", name):
+                    name = name.replace(match[0], "").strip()
                 line.of_display_name = name
