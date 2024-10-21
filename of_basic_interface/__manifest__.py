a5d4377597262d7d09d6342b08c427f1a57cc746
Archibald JEGO <aj@openfire.fr>
2024-10-21 17:34:06 +0200

    of_basic_interface : T17429 - [ADD] Interface simplifiée OpenFire
    Ajout du module de simplification de l'interface.
    
    Ce module aura pour but de simplifier l'interface en cachant des menus
     et en ajoutant des raccourcis directement dans un menu dédié.
    
    Tout est centralisé dans un seul menu "OpenFire" qui contient les
     raccourcis vers les modules les plus utilisés.
    
    Il est a destination des utilisateurs OpenFire "basiques" afin
     d'apporter une approche plus en douceur de l'outil.

---
+++ of_basic_interface/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "OpenFire / Basic Interface",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "category": "OpenFire",
+    "summary": "Module de simplification de l'interface OpenFire",
+    "website": "https://www.openfire.fr",
+    "depends": [
+        "of_base",
+    ],
+    "data": [],
+    "external_dependencies": {},
+    "assets": {},
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
