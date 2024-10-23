14b979d5092e3f874565c273c1eb8c19f8ff92a3
Hounaida <hs@openfire.fr>
2024-10-23 17:20:58 +0200

    of_wizville_base : [MIG] Migration 16.0
    Lors de cette migration du module on en profite pour se séparer de
     l'objet `of.connector.config.settings` pour se rabattre sur l'objet
     `res.config.settings`.
    
    En Odoo 10.0 il y avait plusieurs objet qui héritaient de
     `res.config.settings` et qui étaient utilisés pour la configuration des
     différents modules. Ce n'est plus le cas depuis Odoo 11.0.
    
    ---
    
    Tasks: T17424

--- of_wizville_base/models/__init__.py
+++ of_wizville_base/models/__init__.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import of_connector_config_settings
+from . import res_config_settings
 from . import of_wizville_history
 from . import res_company
 from . import res_partner
