63acb0ca9ee5e4ddffeb341d206b75f973837f07
Hounaida <hs@openfire.fr>
2024-10-24 12:09:42 +0200

    of_planning_tour : T17382 - [FIX] - Archivage d'une intervention
    Les intervnetions archivées n'étaient pas retirées des tournées.
    On s'assure maintenant de retirer/ajouter les interventions en fonction
     de leur état d'archivage.

--- of_planning_tour/__manifest__.py
+++ of_planning_tour/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Planning des tournées",
-    'version': '16.0.1.1.4',
+    'version': '16.0.1.1.5',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
