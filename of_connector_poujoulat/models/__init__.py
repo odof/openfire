497c7f214e481a3e533de270c0bdcf588be6edaf
Hounaida <hs@openfire.fr>
2024-10-25 13:17:55 +0200

    of_connector_poujoulat : T17423 - [MIG] Migration 16.0

--- of_connector_poujoulat/models/__init__.py
+++ of_connector_poujoulat/models/__init__.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import res_config
+from . import res_config_settings
 from . import purchase_order
 from . import product_template
+from . import tools
