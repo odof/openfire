ee747a5e6a15c5b8ad43ab147ede487d4bdf1d3a
Archibald JEGO <aj@openfire.fr>
2024-10-31 10:49:37 +0100

    of_account_payment : T17523 - [FIX] Méthode de paiement
    On récupère le champs payment_method_line_id du mode de paiement.

--- of_account_payment/__manifest__.py
+++ of_account_payment/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Paiments",
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.1',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
