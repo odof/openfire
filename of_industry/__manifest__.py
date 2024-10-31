6390fa56f67e25747c0ffa1b884d051c1a91fd39
Archibald JEGO <aj@openfire.fr>
2024-10-31 15:46:29 +0100

    of_industry : T17491 - [ADD] Module `of_industry`
    Nous avons fait le choix de ne pas migrer le module `of_product_chem`
     historiquement utilisé pour gérer les données techniques spécifiques
     dans OpenFire 10.
    
    Le module `of_industry` vient donc en remplacement et permet lui aussi
     de gérer les données techniques spécifiques à un secteur d'activité
     donné. Il est le module socle pour les modules métiers qui viendront se greffer
     dessus.
    
    Il y aura un module par secteur d'activité, qui viendra ajouter des
     champs spécifiques à chaque modèle concerné.

---
+++ of_industry/__manifest__.py
@@ -0,0 +1,22 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "OpenFire / Données techniques",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "category": "OpenFire",
+    "summary": "Gestion des données techniques",
+    "website": "https://www.openfire.fr",
+    "depends": [
+        "of_base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_category_views.xml",
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
