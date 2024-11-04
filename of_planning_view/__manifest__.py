01acefe5f40f607c0d29e4f0e1b68d4ea92bdeba
Hounaida <hs@openfire.fr>
2024-11-04 17:55:32 +0100

    of_planning_view : T17478 - [IMP] Tooltip de l'intervention
    On ajoute des informations (étiquettes de l'intervention, description de
     l'intervention) dans le tooltip de l'intervention sur le planning :
    
    * Pour les étiquettes on ajoute un champ fonction afin de construire un
     dictionnaire de données pour la tooltip car le `record.of_tags_ids` ne
     remonte que les ids des étiquettes.
    Le champ est `store=False` pour éviter de devoir re-calculer les données
     à chaque fois que l'on renomme ou change la couleur d'une étiquette.
    
    * Pour la description on passe par du `plaintext` pour éviter d'avoir du
     html dans le tooltip.

--- of_planning_view/__manifest__.py
+++ of_planning_view/__manifest__.py
@@ -1,30 +1,30 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Planning d'intervention",
-    'version': '16.0.1.3.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "OpenFire",
-    'summary': "Mise en place de la vue planning pour les interventions",
-    'depends': [
-        'of_planning',
-        'of_web_planning_view',
-        'base_geolocalize',
+    "name": "OpenFire / Planning d'intervention",
+    "version": "16.0.1.4.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Mise en place de la vue planning pour les interventions",
+    "depends": [
+        "of_planning",
+        "of_web_planning_view",
+        "base_geolocalize",
     ],
-    'data': [
-        'views/calendar_event_views.xml',
-        'views/of_planning_intervention_template_views.xml',
-        'views/menuitems.xml',
+    "data": [
+        "views/calendar_event_views.xml",
+        "views/of_planning_intervention_template_views.xml",
+        "views/menuitems.xml",
     ],
-    'assets': {
-        'web.assets_backend': [
-            'of_planning_view/static/src/**/*',
+    "assets": {
+        "web.assets_backend": [
+            "of_planning_view/static/src/**/*",
         ],
     },
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    'uninstall_hook': 'uninstall_hook',
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "uninstall_hook": "uninstall_hook",
 }
