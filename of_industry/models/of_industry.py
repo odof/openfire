ea4c8580d13ab9c5be88d35930c0ff02a71693f0
Archibald JEGO <aj@openfire.fr>
2024-11-05 16:10:49 +0100

    of_industry : T17505 - [IMP] Données techniques et devis
    Mise en place de la remontée des données techniques dans les devis.

--- of_industry/models/of_industry.py
+++ of_industry/models/of_industry.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class OFIndustry(models.Model):
@@ -10,5 +10,20 @@ class OFIndustry(models.Model):
     _description = __doc__
     _order = "name"
 
-    name = fields.Char(translate=True)
-    code = fields.Char()
+    name = fields.Char(translate=True, required=True)
+    code = fields.Char(required=True, readonly=True)
+    content = fields.Text(translate=True, readonly=True)
+    custom_content = fields.Text(translate=True)
+    show_in_sales = fields.Boolean(help="Add technical information in sale line description", default=True)
+    is_custom = fields.Boolean()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("content") and not vals.get("custom_content"):
+                vals["custom_content"] = vals.get("content")
+        return super().create(vals_list)
+
+    def _get_content(self):
+        self.ensure_one()
+        return self.custom_content if self.is_custom else self.content
