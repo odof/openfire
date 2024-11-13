bfcdbef91d013b16c06dee4ac74d6b8847611d6f
Maxime JACQUET <mj@openfire.fr>
2024-11-13 12:48:19 +0100

    of_planning, of_planning_graphql : T17350 -  [FIX] Signature client
    Lorsque le mobile termine l'intervention la signature n'est pas
     affichée dans le rapport.
    
    Il est possible que le mobile renvoie dans la foulée les informations de
     l'intervention qu'il est en train de terminer. Donc on s'assure par le
     contrôle de l'état avant/après le write que nous ne sommes pas dans
     un cas d'appel "fantôme" qui provoquerait un nouvel envoi d'email et
     une nouvelle impression du rapport.
    
    On choisit de changer l'affichage de l'image dans le rapport car même si
     le champ `of_customer_signature` contient bien le base64 de l'image
     le `<span t-field.../>` ne semble pas l'afficher. Par exemple, si
     jamais on duplique l'affichage du champ dans le rapport avec un
     simple `<span t-esc.../>` juste au dessus de l'image de la signature
     on se rend compte que le base64 est bien présent alors que le
     `<span t-field.../>` lui ne permet pas le render de cette image au
     moment de l'appel via l'API mobile.
    On change donc pour un `<img t-att-src.../>` avec un `image_data_uri`
     pour être en mesure afficher l'image de la signature lors de l'appel
     via l'API mobile.
    
    ---
    Tasks : T17350, T16445

--- of_planning_graphql/__manifest__.py
+++ of_planning_graphql/__manifest__.py
@@ -1,19 +1,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / OF Planning GraphQL",
-    'version': '16.0.1.1.1',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'category': "OpenFire",
-    'summary': "Ajoute les données de of_planning dans le schéma Graphql",
-    'website': "https://www.openfire.fr",
-    'depends': [
-        'of_planning',
-        'of_account_graphql',
+    "name": "OpenFire / OF Planning GraphQL",
+    "version": "16.0.1.1.2",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "category": "OpenFire",
+    "summary": "Ajoute les données de of_planning dans le schéma Graphql",
+    "website": "https://www.openfire.fr",
+    "depends": [
+        "of_planning",
+        "of_account_graphql",
     ],
-    'data': [],
-    'installable': True,
-    'application': False,
-    'auto_install': True,
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
 }
