283ce7794e5d566020e0aa2aa8f73d21f3ec0a5f
Hounaida <hs@openfire.fr>
2024-11-14 17:21:07 +0100

    of_product_pack : T17489 - [FIX] Correction vue form
    On corrige les bugs d'affichage des vues form des produits et des variantes.

--- of_product_pack/__manifest__.py
+++ of_product_pack/__manifest__.py
@@ -14,6 +14,7 @@
     ],
     'data': [
         'views/product_template_views.xml',
+        'views/product_product_views.xml',
         'security/ir.model.access.csv',
     ],
     'installable': True,
