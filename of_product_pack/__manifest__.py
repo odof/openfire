70e0c9d01d877fbe93ab1813bdb5802eaad9f343
Hounaida <hs@openfire.fr>
2024-11-14 17:21:07 +0100

    of_product_pack : T17489 - [FIX] Correction vue form
    On corrige les bugs d'affichage des vues form des produits et des variantes.

--- of_product_pack/__manifest__.py
+++ of_product_pack/__manifest__.py
@@ -1,22 +1,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Kits produits",
-    'version': '16.0.1.2.1',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': 'https://www.openfire.fr',
-    'category': 'OpenFire',
-    'summary': "Module d'extension du module OCA product_pack",
-    'depends': [
-        'product_pack',
-        'of_product_brand',
+    "name": "OpenFire / Kits produits",
+    "version": "16.0.1.2.2",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Module d'extension du module OCA product_pack",
+    "depends": [
+        "product_pack",
+        "of_product_brand",
     ],
-    'data': [
-        'views/product_template_views.xml',
-        'security/ir.model.access.csv',
+    "data": [
+        "views/product_template_views.xml",
+        "views/product_product_views.xml",
+        "security/ir.model.access.csv",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
