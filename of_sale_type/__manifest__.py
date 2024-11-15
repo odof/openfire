50caa740af7f5e2582410c9397ab9f37586c370c
Maxime JACQUET <mj@openfire.fr>
2024-11-15 11:34:28 +0100

    of_sale_type : T17598 - [IMP] Vues listes
    Ajout de champs optionnels sur les vues listes `sale.order.line`

--- of_sale_type/__manifest__.py
+++ of_sale_type/__manifest__.py
@@ -1,23 +1,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Types de devis",
-    'version': '16.0.1.1.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "OpenFire",
-    'summary': "Extension du module OCA sale_order_type",
-    'depends': [
-        'sale_order_type',  # OCA/sale-workflow
-        'of_sale_management_template',
+    "name": "OpenFire / Types de devis",
+    "version": "16.0.1.2.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Extension du module OCA sale_order_type",
+    "depends": [
+        "sale_order_type",  # OCA/sale-workflow
+        "of_sale_management_template",
     ],
-    'data': [
-        'views/sale_order_type.xml',
-        'views/res_partner.xml',
-        'views/sale_order_template.xml',
+    "data": [
+        "views/sale_order_type.xml",
+        "views/res_partner.xml",
+        "views/sale_order_views.xml",
+        "views/sale_order_line_views.xml",
+        "views/sale_order_template.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
--- of_sale_type/models/__init__.py
+++ of_sale_type/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import sale_order_template
 from . import sale_order
+from . import sale_order_line
