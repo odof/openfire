79c9c27356d59b98ed809b778c8143083d445eed
Maxime JACQUET <mj@openfire.fr>
2024-11-15 11:33:33 +0100

    of_sale_stock : T17598 - [IMP] Vues listes
    Ajout de champs optionnels sur les vues listes `product.template`

--- of_sale_stock/__manifest__.py
+++ of_sale_stock/__manifest__.py
@@ -1,24 +1,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Sale Stock",
-    'version': '16.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'category': 'OpenFire',
-    'summary': "Module de lien entre les ventes, les marques et les stocks",
-    'website': 'https://www.openfire.fr',
-    'depends': [
-        'of_sale_report_setting',  # of_sale_report_setting > of_sale > (of_account / sale_stock)
-        'of_product_brand',
+    "name": "OpenFire / Sale Stock",
+    "version": "16.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "category": "OpenFire",
+    "summary": "Module de lien entre les ventes, les marques et les stocks",
+    "website": "https://www.openfire.fr",
+    "depends": [
+        "of_sale_report_setting",  # of_sale_report_setting > of_sale > (of_account / sale_stock)
+        "of_product_brand",
     ],
-    'data': [
-        'views/sale_order_views.xml',
-        'views/product_category_views.xml',
-        'reports/ir_actions_report_templates.xml',
-        'reports/of_sale_report_view.xml',
+    "data": [
+        "views/sale_order_views.xml",
+        "views/product_category_views.xml",
+        "views/product_template_views.xml",
+        "reports/ir_actions_report_templates.xml",
+        "reports/of_sale_report_view.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
