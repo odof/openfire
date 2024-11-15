2d19fec7f789e090286b884042fe6fde82041f90
Maxime JACQUET <mj@openfire.fr>
2024-11-15 11:32:10 +0100

    of_sale_order_dates : T17598 - [IMP] Vues listes
    Ajout de champs optionnels sur les vues listes `sale.order.line`

--- of_sale_order_dates/__manifest__.py
+++ of_sale_order_dates/__manifest__.py
@@ -1,25 +1,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Dates sur les commandes de vente",
-    'version': '16.0.1.2.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "OpenFire",
-    'summary': "Dates sur les commandes de vente",
-    'depends': [
-        'of_sale_report_setting',
+    "name": "OpenFire / Dates sur les commandes de vente",
+    "version": "16.0.1.3.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Dates sur les commandes de vente",
+    "depends": [
+        "of_sale_report_setting",
     ],
-    'data': [
-        'data/ir_config_parameter.xml',
-        'views/of_sale_document_layout.xml',
-        'views/sale_order_views.xml',
-        'views/account_move_views.xml',
-        'reports/sale_report_templates.xml',
-        'reports/account_move_report_templates.xml',
+    "data": [
+        "data/ir_config_parameter.xml",
+        "views/of_sale_document_layout.xml",
+        "views/sale_order_views.xml",
+        "views/account_move_views.xml",
+        "reports/sale_report_templates.xml",
+        "reports/account_move_report_templates.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
