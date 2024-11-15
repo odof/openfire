563ea866e93613a693df41315533f0d80c759be3
Maxime JACQUET <mj@openfire.fr>
2024-11-15 11:30:19 +0100

    of_sale_crm, of_sale_margin : T17598 - [ADD] Reste à facturer
    Ajout de nouveaux champs de suivi pour le reste à facturer sur la
     commande.

--- of_sale_crm/models/sale_order_line.py
+++
@@ -1,9 +0,0 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import fields, models
-
-
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    of_amount_to_invoice = fields.Float(string="Remains to be invoiced in €")
