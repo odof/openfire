ae04e3a11e864900aadabd966f1aeb3c85cc5c83
Maxime JACQUET <mj@openfire.fr>
2024-11-15 11:35:20 +0100

    of_stock_account : T17598 - [IMP] Vues listes
    Ajout de champs optionnels sur les vues listes `product.template`

--- of_stock_account/__manifest__.py
+++ of_stock_account/__manifest__.py
@@ -1,23 +1,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Stock Account",
-    'version': '16.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "OpenFire",
-    'summary': "Module de lien entre les stocks, les produits, les marques et la comptabilité",
-    'depends': [
-        'stock_account',
-        'of_account',  # of_account > of_product_brand > of_product
+    "name": "OpenFire / Stock Account",
+    "version": "16.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Module de lien entre les stocks, les produits, les marques et la comptabilité",
+    "depends": [
+        "stock_account",
+        "of_account",  # of_account > of_product_brand > of_product
     ],
-    'data': [
-        'views/product_category_views.xml',
-        'views/product_views.xml',
-        'reports/of_account_report_views.xml',
+    "data": [
+        "views/product_category_views.xml",
+        "views/product_views.xml",
+        "reports/of_account_report_views.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
