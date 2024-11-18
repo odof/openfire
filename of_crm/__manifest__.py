e65dbf99faef0efc3e95e1ec9a376629b04b9292
Hounaida <hs@openfire.fr>
2024-11-18 11:24:31 +0100

    of_crm : T17623 - [IMP] Afficher le modèle sur le type d'activité
    On souhaite toujours afficher le modèle sur le type d'activité.
    
    Il y a 2 champs `res_model` sur `mail.activity.type`.
    Un est visible seulement avec le mode développeur et l'autre est
     invisible.
    
    On rend invisible le champ `res_model` limité au mode développeur et
     on rend visible tout le temps visible le 2ème champ `res_model`.

--- of_crm/__manifest__.py
+++ of_crm/__manifest__.py
@@ -1,31 +1,31 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / CRM",
-    'version': '16.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "OpenFire",
-    'summary': "Personnalisation du CRM",
-    'depends': [
-        'crm',
-        'of_base_location',  # -> of_base
-        'of_utm',
-        'of_utils',
-        'of_geolocalize',
+    "name": "OpenFire / CRM",
+    "version": "16.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Personnalisation du CRM",
+    "depends": [
+        "crm",
+        "of_base_location",  # -> of_base
+        "of_utm",
+        "of_utils",
+        "of_geolocalize",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/crm_stage_views.xml',
-        'views/crm_lead_views.xml',
-        'views/res_partner_views.xml',
-        'views/res_company_views.xml',
-        'views/mail_activity_type_views.xml',
-        'views/utm_menus.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/crm_stage_views.xml",
+        "views/crm_lead_views.xml",
+        "views/res_partner_views.xml",
+        "views/res_company_views.xml",
+        "views/mail_activity_type_views.xml",
+        "views/utm_menus.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    'post_init_hook': 'post_init_hook',
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "post_init_hook": "post_init_hook",
 }
