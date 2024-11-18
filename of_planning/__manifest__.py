edd5e46ca7cf44684503fd5de77928004a61ee2b
Hounaida <hs@openfire.fr>
2024-11-18 10:06:26 +0100

    of_planning : T17659 - [FIX] - Fiche d'intervention avec section
    L'impression d'une fiche d'intervention depuis une commande
     avec des lignes de section provoquait une erreur de rendu Qweb.

--- of_planning/__manifest__.py
+++ of_planning/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "OpenFire / Planning",
-    "version": "16.0.1.17.0",
+    "version": "16.0.1.17.1",
     "license": "AGPL-3",
     "author": "OpenFire",
     "category": "OpenFire",
