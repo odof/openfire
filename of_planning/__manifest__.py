f9cc957018c8588cc61ab9798d1e385ab662580b
Paul LE COZ <plc@openfire.fr>
2024-11-19 13:58:12 +0100

    of_planning : T17615 - [FIX] Intervention en journée complète
    Correction du champ `allday` de `calendar.event` pour rester sur le même
     fonctionnement que le standard Odoo.

--- of_planning/__manifest__.py
+++ of_planning/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "OpenFire / Planning",
-    "version": "16.0.1.15.4",
+    "version": "16.0.1.15.5",
     "license": "AGPL-3",
     "author": "OpenFire",
     "category": "OpenFire",
