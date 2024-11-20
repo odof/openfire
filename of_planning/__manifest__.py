842c1cd30ee4f867593cb5b7d2186ddc80a8e281
Paul LE COZ <plc@openfire.fr>
2024-11-20 16:06:05 +0100

    of_planning : T17615 - [FIX] Gestion des techniciens archivés
    Ajout du context `active_test: False` sur le champ `of_employee_ids` de
     calendar.event pour voir les techniciens archivés.

--- of_planning/__manifest__.py
+++ of_planning/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "OpenFire / Planning",
-    "version": "16.0.1.15.5",
+    "version": "16.0.1.15.6",
     "license": "AGPL-3",
     "author": "OpenFire",
     "category": "OpenFire",
