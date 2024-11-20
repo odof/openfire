d02264599b9a9577e3573ec266391714df894dcf
Archibald JEGO <aj@openfire.fr>
2024-11-20 18:41:04 +0100

    of_sale_crm : T17697 - [IMP] Compute of_customer_state
    On repends la façon de mettre à jour le champ `of_customer_state`
     en fonction du `customer_rank`, `supplier_rank` et de l'état
     d'avancement d'une opportunité.
    
    Le hook d'installation est supprimé car il devient obsolète avec le
     champs compute qui se déclenchera à l'installation.

--- of_sale_crm/models/crm_lead.py
+++ of_sale_crm/models/crm_lead.py
@@ -8,6 +8,10 @@ class CrmLead(models.Model):
 
     of_customer_state = fields.Selection(related="partner_id.of_customer_state", required=False)
 
+    # -------------------------------------------------------------------------
+    # Onchange methods
+    # -------------------------------------------------------------------------
+
     @api.onchange("partner_id")
     def _onchange_partner_id_warning(self):
         if not (partner := self.partner_id):
@@ -25,14 +29,72 @@ class CrmLead(models.Model):
                 self.partner_id = False
             return {"warning": warning}
 
-    def _prepare_opportunity_quotation_context(self):
-        quotation_context = super()._prepare_opportunity_quotation_context()
-        quotation_context["default_of_referred_id"] = self.of_referred_id.id
-        quotation_context["default_of_canvasser_id"] = self.of_canvasser_id.id
-        return quotation_context
+    # -------------------------------------------------------------------------
+    # ORM methods
+    # -------------------------------------------------------------------------
+
+    def write(self, vals):
+        saved_values = {
+            opportunity: {
+                "probability": opportunity.probability,
+                "stage_id": opportunity.stage_id,
+                "active": opportunity.active,
+            }
+            for opportunity in self
+        }
+        res = super().write(vals)
+        if not self.env.context.get("of_avoid_customer_state_compute"):
+            self._handle_of_customer_state_update(vals, saved_values)
+        return res
+
+    # -------------------------------------------------------------------------
+    # Action methods
+    # -------------------------------------------------------------------------
 
     def action_set_lost(self, **additional_values):
         res = super().action_set_lost(**additional_values)
         orders = self.mapped("order_ids").filtered(lambda order: order.state in ["draft", "sent"])
         orders and orders._action_cancel()
         return res
+
+    # -------------------------------------------------------------------------
+    # Business methods
+    # -------------------------------------------------------------------------
+
+    def _prepare_opportunity_quotation_context(self):
+        quotation_context = super()._prepare_opportunity_quotation_context()
+        quotation_context["default_of_referred_id"] = self.of_referred_id.id
+        quotation_context["default_of_canvasser_id"] = self.of_canvasser_id.id
+        return quotation_context
+
+    def _handle_of_customer_state_update(self, vals, saved_values):
+        if (
+            "probability" not in vals
+            and "stage_id" not in vals
+            and "active" not in vals
+            and "lost_reason_id" not in vals
+        ):
+            return
+
+        def _get_all_parent_partners(partners):
+            all_partners = partners
+            for partner in partners:
+                current = partner.parent_id
+                while current:
+                    if current not in partners:
+                        all_partners |= current
+                    current = current.parent_id
+            return all_partners
+
+        to_process = self.filtered(
+            lambda o: (
+                ((o.probability == 100) != (saved_values[o]["probability"] == 100))
+                or (o.stage_id.is_won != saved_values[o]["stage_id"].is_won)
+                or ("lost_reason_id" in vals and o.active != saved_values[o]["active"])
+            )
+        )
+
+        if to_process:
+            partners = to_process.mapped("partner_id")
+            all_partners = _get_all_parent_partners(partners)
+            all_partners and all_partners._compute_of_customer_state()
