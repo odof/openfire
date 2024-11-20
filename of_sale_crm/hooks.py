72bc9e73f1500762a8c7a0c6458876f81ce5f7b5
Archibald JEGO <aj@openfire.fr>
2024-11-20 18:41:04 +0100

    of_sale_crm : T17697 - [IMP] Compute of_customer_state
    On repends la façon de mettre à jour le champ `of_customer_state`
     en fonction du `customer_rank`, `supplier_rank` et de l'état
     d'avancement d'une opportunité.
    
    Le hook d'installation est supprimé car il devient obsolète avec le
     champs compute qui se déclenchera à l'installation.

--- of_sale_crm/hooks.py
+++ of_sale_crm/hooks.py
@@ -21,57 +21,6 @@ def _account_invoice_post_install(cr):
     )
 
 
-def _update_of_customer_state_on_partners(env):
-    """
-    Update of_customer_state on partners.
-
-    For no customer partners (customer = False): of_customer_state = 'other'
-    For customer partners:
-        if a partner (or its parent if any) has at least one sale or
-            one invoice: of_customer_state = 'customer'
-        else: of_customer_state = 'lead'
-    We take care of children after taking care of their parent.
-    """
-    partner_obj = env["res.partner"].with_context(active_test=False)
-    # all partners
-    partners = partner_obj.search([])
-    # init of_customer_state to 'other' for all partners
-    partners.write({"of_customer_state": "other"})
-
-    # init of_customer_state for customers
-    customers = partner_obj.search([("customer_rank", ">=", 1)])
-    to_lead = env["res.partner"]
-    to_customer = env["res.partner"]
-    len_customers = len(customers)
-
-    todo = partner_obj.search(
-        [("customer_rank", ">=", 1), "|", ("parent_id", "=", False), ("parent_id.customer_rank", ">=", 1)]
-    )
-    while todo:
-        partner = todo[0]
-        todo -= partner
-        if partner.sale_order_count == 0 and partner.total_invoiced == 0:
-            if partner.parent_id and partner.parent_id in to_customer:
-                to_customer += partner
-            else:
-                to_lead += partner
-        else:
-            to_customer += partner
-        # potentially inactive children
-        todo += partner_obj.search([("parent_id", "=", partner.id), ("customer_rank", ">=", 1)])
-
-    to_lead.write({"of_customer_state": "lead"})
-    to_customer.write({"of_customer_state": "customer"})
-    len_done = len(to_lead) + len(to_customer)
-
-    if len_customers != len_done:  # not all partners have been processed
-        _logger.warning(
-            f"hook '_res_partner_post_install' inconsistency. Customers: {len_customers}, processed: {len_done}"
-        )
-    else:
-        _logger.info(f"hook '_res_partner_post_install' done. {len_done} partners processed")
-
-
 def _sale_order_post_install(cr):
     """
     À l'installation du module :
@@ -103,5 +52,4 @@ def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     _account_invoice_post_install(cr)
     _sale_order_post_install(cr)
-    _update_of_customer_state_on_partners(env)
     _set_default_order_start_state(env)
