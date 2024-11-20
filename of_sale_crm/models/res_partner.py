af11d3520c2fcdc6e93e5d3f403cc51199f7b4e1
Archibald JEGO <aj@openfire.fr>
2024-11-20 18:41:04 +0100

    of_sale_crm : T17697 - [IMP] Compute of_customer_state
    On repends la façon de mettre à jour le champ `of_customer_state`
     en fonction du `customer_rank`, `supplier_rank` et de l'état
     d'avancement d'une opportunité.
    
    Le hook d'installation est supprimé car il devient obsolète avec le
     champs compute qui se déclenchera à l'installation.

--- of_sale_crm/models/res_partner.py
+++ of_sale_crm/models/res_partner.py
@@ -9,7 +9,10 @@ class ResPartner(models.Model):
     of_customer_state = fields.Selection(
         selection=[("lead", "Prospect"), ("customer", "Signed customer"), ("other", "Other")],
         string="Customer State",
+        compute="_compute_of_customer_state",
         default="lead",
+        store=True,
+        readonly=False,
         help="This field is only useful for customer partners."
         "A customer is considered a prospect as long as he/she has neither confirmed an order nor validated "
         "an invoice. This field is updated automatically on order confirmation and invoice validation.",
@@ -20,6 +23,26 @@ class ResPartner(models.Model):
         if not values.get("of_customer_state", False):
             values["of_customer_state"] = "lead"
         return super()._add_missing_default_values(values)
+
+    @api.depends("customer_rank", "supplier_rank", "parent_id.customer_rank", "parent_id.supplier_rank")
+    def _compute_of_customer_state(self):
+        """
+        A partner is considered a signed customer if its `customer_rank` is > 0 or if it has an opportunity won,
+        other if its `supplier_rank` > 0 and prospect otherwise.
+        """
+        lead_obj = self.env["crm.lead"]
+        for partner in self:
+            customer_state = "lead"
+            leads = lead_obj.search([("partner_id", "=", partner.id)])
+            if (
+                partner.customer_rank
+                or (partner.parent_id and partner.parent_id.customer_rank)
+                or any(lead.probability == 100 for lead in leads)
+            ):
+                customer_state = "customer"
+            elif partner.supplier_rank:
+                customer_state = "other"
+            partner.of_customer_state = customer_state
 
     @api.depends("of_is_lead_warn")
     def _compute_of_is_warn(self):
