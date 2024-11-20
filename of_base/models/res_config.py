fb41594a462f4e35eca285270991a6ad11f6d44b
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2024-11-20 17:08:13 +0100

    of_base : [IMP] Ne plus forcer le alias_domain dans les paramètres généraux

--- of_base/models/res_config.py
+++ of_base/models/res_config.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import api, fields, models
+
+
+class BaseConfiguration(models.TransientModel):
+    _inherit = 'base.config.settings'
+
+    @api.model
+    def get_default_alias_domain(self, fields):
+        # Modification de la fonction odoo (module mail)
+        # Si aucun "mail.catchall.domain", on ne veut pas forcer une valeur par défaut dans ce wizard de configuration
+        alias_domain = self.env["ir.config_parameter"].get_param("mail.catchall.domain", default=None)
+        return {'alias_domain': alias_domain or False}
 
 
 class OFConnectorConfigSettings(models.TransientModel):
@@ -8,5 +19,6 @@ class OFConnectorConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     _description = 'Configuration des connecteurs'
 
-    company_id = fields.Many2one('res.company', string='Company', required=True,
+    company_id = fields.Many2one(
+        comodel_name='res.company', string="Company", required=True,
         default=lambda self: self.env.user.company_id)
