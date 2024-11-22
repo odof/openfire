9ef3d8eba5eb0591e62127e87f02485fa91c2f00
Maxime JACQUET <mj@openfire.fr>
2024-11-22 15:00:30 +0100

    of_sale, of_account : T17018 - [IMP] Nomenclature lignes
    On revoit la nomenclature des lignes de commande/facture.
    On n'affiche plus que la référence interne dans la colonne product_id et
     on ne l'affiche plus dans la colonne name.

--- of_sale/models/product_template.py
+++ of_sale/models/product_template.py
@@ -45,3 +45,13 @@ class ProductTemplate(models.Model):
         if deposit_product_id in deleted_ids:
             deposit_product_id.set_param("sale.default_deposit_product_id", False)
         return res
+
+    def name_get(self):
+        if not self.env.context.get("of_only_default_code"):
+            return super().name_get()
+
+        self.browse(self.ids).read(["default_code", "name"])  # prefetch only required fields
+        return [
+            (template.id, f"{template.default_code}") if template.default_code else (template.id, f"{template.name}")
+            for template in self
+        ]
