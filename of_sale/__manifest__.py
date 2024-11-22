fbe0eccd29e8e79b152104aeb2d17c3ce4eabc73
Maxime JACQUET <mj@openfire.fr>
2024-11-22 15:23:25 +0100

    of_sale : [FIX] Calcul de la référence client
    Ajustement d'une ligne dans le compute qui pouvait provoquer une erreur
     `ExpectedSingleton`.

--- of_sale/__manifest__.py
+++ of_sale/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Ventes",
-    'version': '16.0.1.6.0',
+    'version': '16.0.1.6.1',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
