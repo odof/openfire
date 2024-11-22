09c168fca0af82483a32cc8a517fd5bb906a5b05
Maxime JACQUET <mj@openfire.fr>
2024-11-22 15:38:17 +0100

    of_planning : T17598 - [IMP] Vues listes
    Ajout de champs optionnels sur les vues listes `sale.order.line`

--- of_planning/__manifest__.py
+++ of_planning/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Planning",
-    'version': '16.0.1.14.2',
+    'version': '16.0.1.15.2',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'category': "OpenFire",
