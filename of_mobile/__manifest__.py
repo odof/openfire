6cb12e1d45b524d315f76df9b8c510957d29f3e9
Maxime JACQUET <mj@openfire.fr>
2024-11-22 15:33:03 +0100

    of_mobile : T16856 - [ADD] Géolocaliser une adresse depuis le mobile
    Mise en place d'une query GraphQL permettant de géolocaliser une adresse
     depuis le mobile.

--- of_mobile/__manifest__.py
+++ of_mobile/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Mobile",
-    'version': '16.0.1.3.0',
+    'version': '16.0.1.4.0',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
