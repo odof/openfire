81ddca866e9b1aaec28693eba30d2f2706463dd7
Maxime JACQUET <mj@openfire.fr>
2024-11-22 15:20:08 +0100

    of_sale_crm : T17070 - [IMP] Formulaire client
    Ajustement du formulaire client pour la gestion des tags de prospects.
    Modification du style de la notion de prospect dans le formulaire client
     pour que cela resemble à une étiquette de couleur.

--- of_sale_crm/__manifest__.py
+++ of_sale_crm/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "OpenFire / Sale CRM",
-    'version': '16.0.1.2.0',
+    'version': '16.0.1.3.0',
     'license': 'AGPL-3',
     'author': "OpenFire",
     'website': "https://www.openfire.fr",
@@ -25,6 +25,9 @@
         'reports/sale_report_views.xml',
         'reports/account_invoice_report_views.xml',
     ],
+    "assets": {
+        "web.assets_backend": ["of_sale_crm/static/src/scss/prospect_tag.scss"],
+    },
     'installable': True,
     'application': False,
     'auto_install': False,
