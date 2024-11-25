27b8475261ccbc123029e7a1f5b7b4b981fbfca0
Hounaida <hs@openfire.fr>
2024-11-25 13:48:03 +0100

    of_sale_product_pack : T17701 - [IMP] Duplication commande de ventes
    Quand on duplique un devis, les lignes de composant de kit doivent
    être dupliquées avec le devis.

--- of_sale_product_pack/__manifest__.py
+++ of_sale_product_pack/__manifest__.py
@@ -1,22 +1,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Ventes & Kits produits",
-    'version': '16.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'summary': "Module de gestion de vente produits kits",
-    'category': 'OpenFire',
-    'website': 'https://www.openfire.fr',
-    'depends': [
-        'of_product_pack',
-        'of_sale',
-        'sale_product_pack',
+    "name": "OpenFire / Ventes & Kits produits",
+    "version": "16.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "summary": "Module de gestion de vente produits kits",
+    "category": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "depends": [
+        "of_product_pack",
+        "of_sale",
+        "sale_product_pack",
     ],
-    'data': [
-        'views/sale_order_line_views.xml',
+    "data": [
+        "views/sale_order_line_views.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
