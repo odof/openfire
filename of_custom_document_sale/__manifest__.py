16e11efc6464e0d9e66694054510e4a849b9f70a
Hounaida <hs@openfire.fr>
2024-11-26 12:31:35 +0100

    of_custom_document, of_custom_document_sale : [FIX] - Installation
    Correction d'erreur qui s'affiche lors de l'installation de module
     `of_custom_document` sur une base de données vide.

--- of_custom_document_sale/__manifest__.py
+++ of_custom_document_sale/__manifest__.py
@@ -1,18 +1,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'OpenFire / Rapports personnalisés pour les ventes',
-    'version': '16.0.1.1.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "Documents",
-    'summary': "Rapports personnalisés pour les ventes",
-    'depends': ['of_custom_document', 'of_sale'],
-    'data': [
-        'views/sale_order_views.xml',
+    "name": "OpenFire / Rapports personnalisés pour les ventes",
+    "version": "16.0.1.1.1",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "Documents",
+    "summary": "Rapports personnalisés pour les ventes",
+    "depends": [
+        "of_custom_document",
+        "of_sale",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "data": [
+        "views/sale_order_views.xml",
+        "views/menuitems.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
 }
