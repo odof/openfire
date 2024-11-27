f949dd86e1ba262b98cba42b4dc54b0d582dc61d
Hounaida <hs@openfire.fr>
2024-11-27 10:33:56 +0100

    of_custom_document : T17732 - [FIX] Rapport personnalisé
    Il y avait un souci de contenu lors de la génération du rapport
     personnalisé.
    
    Le PDF était généré mais non exploitable (erreur lors de l'ouverture).

--- of_custom_document/__manifest__.py
+++ of_custom_document/__manifest__.py
@@ -1,24 +1,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'OpenFire / Rapports personnalisés',
-    'version': '16.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "Documents",
-    'summary': "Module de rapports personnalisés",
-    'depends': ['of_base'],
-    'external_dependencies': {
-        'python': ['pdfminer', 'pypdftk'],
+    "name": "OpenFire / Rapports personnalisés",
+    "version": "16.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "Documents",
+    "summary": "Module de rapports personnalisés",
+    "depends": ["of_base"],
+    "external_dependencies": {
+        "python": ["pdfminer", "pypdftk"],
     },
-    'data': [
-        'security/ir.model.access.csv',
-        'report/ir_actions_report_templates.xml',
-        'views/ir_actions_views.xml',
-        'views/of_custom_document_views.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "report/ir_actions_report_templates.xml",
+        "views/ir_actions_views.xml",
+        "views/of_custom_document_views.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
