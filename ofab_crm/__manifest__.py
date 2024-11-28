147e6572b992ab5fd66b388735e2f812afdace5e
Hounaida <hs@openfire.fr>
2024-11-28 17:55:51 +0100

    of_base : T17686 - [IMP] Améliorations sur form view de contact
    On supprime la règle css qui causait l'apparition d'une scrollbar sur
     le widget Téléphones. On déplace des champs.

--- ofab_crm/__manifest__.py
+++ ofab_crm/__manifest__.py
@@ -1,21 +1,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Gestion CRM des fabricants",
-    'version': '16.0.1.1.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "OpenFire",
-    'summary': "CRM management of manufacturers",
-    'depends': [
-        'of_base',
-        'of_product_brand',
+    "name": "OpenFire / Gestion CRM des fabricants",
+    "version": "16.0.1.2.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "CRM management of manufacturers",
+    "depends": [
+        "of_base",
+        "of_product_brand",
     ],
-    'data': [
-        'views/res_partner_view.xml',
+    "data": [
+        "views/res_partner_view.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
