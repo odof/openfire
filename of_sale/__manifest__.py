f511d80c9c7a536c0a23e4d37d16989966470f3f
Hounaida <hs@openfire.fr>
2024-11-28 10:41:37 +0100

    of_sale_crm : T17708 - [IMP] Changement de l'opportunité dans le devis
    Lors du changement d'opportunité dans le devis, on met à jour le
     prospecteur et le vendeur du devis avec les données de l'opportunité.
    
    Afin de prévenir l'utilisateur de la modification des données, on
     affiche une `div alert` sous le champ opportunité.

--- of_sale/__manifest__.py
+++ of_sale/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "OpenFire / Ventes",
-    "version": "16.0.1.7.1",
+    "version": "16.0.1.8.1",
     "license": "AGPL-3",
     "author": "OpenFire",
     "website": "https://www.openfire.fr",
