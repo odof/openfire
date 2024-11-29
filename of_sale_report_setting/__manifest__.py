d8accc577f8411c9a84e784078adde92e62ee544
Hounaida <hs@openfire.fr>
2024-11-29 13:41:07 +0100

    of_sale_report_setting : T17776 - [IMP] Amélioration du bloc signature
    Lorsque la mention de signature est longue il arrivait qu'elle se superpose au bloc signature.

--- of_sale_report_setting/__manifest__.py
+++ of_sale_report_setting/__manifest__.py
@@ -1,34 +1,34 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Paramétrage du rapport de vente",
-    'version': '16.0.1.1.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': 'https://www.openfire.fr',
-    'category': 'OpenFire',
-    'summary': "Options de paramétrage du rapport de vente devis/commande",
-    'depends': [
-        'of_sale',
+    "name": "OpenFire / Paramétrage du rapport de vente",
+    "version": "16.0.1.2.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Options de paramétrage du rapport de vente devis/commande",
+    "depends": [
+        "of_sale",
     ],
-    'data': [
-        'security/of_sale_report_setting_security.xml',
-        'security/ir.model.access.csv',
-        'reports/ir_actions_report_templates.xml',
-        'views/of_sale_document_layout.xml',
-        'views/res_config_settings_views.xml',
-        'views/res_users_views.xml',
-        'views/sale_order_views.xml',
+    "data": [
+        "security/of_sale_report_setting_security.xml",
+        "security/ir.model.access.csv",
+        "reports/ir_actions_report_templates.xml",
+        "views/of_sale_document_layout.xml",
+        "views/res_config_settings_views.xml",
+        "views/res_users_views.xml",
+        "views/sale_order_views.xml",
     ],
-    'assets': {
-        'web.assets_common': [
-            'of_sale_report_setting/static/src/css/of_sale_report_setting_style.css',
+    "assets": {
+        "web.assets_common": [
+            "of_sale_report_setting/static/src/css/of_sale_report_setting_style.css",
         ],
-        'web.report_assets_common': [
-            'of_sale_report_setting/static/src/css/sale_report_style.css',
+        "web.report_assets_common": [
+            "of_sale_report_setting/static/src/css/sale_report_style.css",
         ],
     },
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
