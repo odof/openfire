65599f06f738812ee28cf5cb797e436ec0cdd9e5
Paul LE COZ <plc@openfire.fr>
2024-12-02 21:34:50 +0100

    Démo HF - Liste de prix 2

--- of_sale_discount/models/product_pricelist_item.py
+++ of_sale_discount/models/product_pricelist_item.py
@@ -1,7 +1,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import formatLang
 
 
 class PricelistItem(models.Model):
@@ -18,6 +19,7 @@ class PricelistItem(models.Model):
         string="Marques",
         help="Si renseigné, la règle s'appliquera uniquement aux produits de ces marques, sinon à toutes les marques",
     )
+    of_categ_ids = fields.Many2many(comodel_name="product.category", string="Catégories de produit")
 
     @api.depends("of_percent_price_formula")
     def _compute_percent_price(self):
@@ -33,8 +35,89 @@ class PricelistItem(models.Model):
             line.percent_price = 100.0 - price_percent
 
     def _is_applicable_for(self, product, qty_in_product_uom):
-        result = super()._is_applicable_for(product, qty_in_product_uom)
-        if result:
+        self.ensure_one()
+        product.ensure_one()
+        res = True
+
+        is_product_template = product._name == 'product.template'
+        if self.min_quantity and qty_in_product_uom < self.min_quantity:
+            res = False
+
+        elif self.applied_on == "2_product_category":
+            if (
+                product.categ_id.id not in self.of_categ_ids.ids
+                and not any(product.categ_id.parent_path.startswith(categ_id.parent_path) for categ_id in self.of_categ_ids)
+            ):
+                res = False
+        else:
+            # Applied on a specific product template/variant
+            if is_product_template:
+                if self.applied_on == "1_product" and product.id != self.product_tmpl_id.id:
+                    res = False
+                elif self.applied_on == "0_product_variant" and not (
+                    product.product_variant_count == 1
+                    and product.product_variant_id.id == self.product_id.id
+                ):
+                    # product self acceptable on template if has only one variant
+                    res = False
+            else:
+                if self.applied_on == "1_product" and product.product_tmpl_id.id != self.product_tmpl_id.id:
+                    res = False
+                elif self.applied_on == "0_product_variant" and product.id != self.product_id.id:
+                    res = False
+
+        if res:
             if self.of_brand_ids and product.brand_id.id not in self.of_brand_ids.ids:
                 return False
-        return True
+
+        return res
+
+    @api.constrains("product_id", "product_tmpl_id", "categ_id")
+    def _check_product_consistency(self):
+        for item in self:
+            if item.applied_on == "2_product_category" and not (item.categ_id or item.of_categ_ids):
+                raise ValidationError("Veuillez préciser la catégorie à laquelle cette règle doit être appliquée")
+            elif item.applied_on == "1_product" and not item.product_tmpl_id:
+                raise ValidationError("Veuillez préciser le produit pour lequel cette règle doit être appliquée")
+            elif item.applied_on == "0_product_variant" and not item.product_id:
+                raise ValidationError(
+                    "Veuillez préciser la variante de produit pour laquelle cette règle doit être appliquée"
+                )
+
+    @api.depends(
+        "applied_on",
+        "categ_id",
+        "product_tmpl_id",
+        "product_id",
+        "compute_price",
+        "fixed_price",
+        "pricelist_id",
+        "percent_price",
+        "price_discount",
+        "price_surcharge",
+    )
+    def _compute_name_and_price(self):
+        for item in self:
+            if (item.categ_id or item.of_categ_ids) and item.applied_on == "2_product_category":
+                item.name = _("Catégorie(s) : %s") % (
+                    item.categ_id.display_name or ", ".join(item.of_categ_ids.mapped("display_name"))
+                )
+            elif item.product_tmpl_id and item.applied_on == "1_product":
+                item.name = _("Produit : %s") % (item.product_tmpl_id.display_name)
+            elif item.product_id and item.applied_on == "0_product_variant":
+                item.name = _("Variante : %s") % (item.product_id.display_name)
+            else:
+                item.name = _("Tous les produits")
+
+            if item.compute_price == "fixed":
+                item.price = formatLang(
+                    item.env, item.fixed_price, monetary=True, dp="Product Price", currency_obj=item.currency_id
+                )
+            elif item.compute_price == "percentage":
+                item.price = _("%s %% remise", item.percent_price)
+            else:
+                item.price = _(
+                    "%(percentage)s %% remise et supplément de %(price)s ",
+                    percentage=item.price_discount,
+                    price=item.price_surcharge,
+                )
