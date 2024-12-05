d4d06d9119dc0fbc50f6b58ae640629339ef66a5
Hounaida <hs@openfire.fr>
2024-12-05 16:57:15 +0100

    of_sale_report_setting_product_multi_image : T17823 - [FIX] Images
    Repositionnement des images d'articles en annexe de rapports de
     devis/commandes pour permettre un affichage correct dans le document.

--- of_sale_report_setting_product_multi_image/__manifest__.py
+++ of_sale_report_setting_product_multi_image/__manifest__.py
@@ -1,22 +1,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Sale Report Setting Product Multi Image",
-    'version': '16.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': 'https://www.openfire.fr',
-    'category': 'OpenFire',
-    'summary': "Print product images for Quotation/Order report",
-    'depends': [
-        'of_sale_report_setting',
-        'product_multi_image',
+    "name": "OpenFire / Sale Report Setting Product Multi Image",
+    "version": "16.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Print product images for Quotation/Order report",
+    "depends": [
+        "of_sale_report_setting",
+        "product_multi_image",
     ],
-    'data': [
-        'reports/ir_actions_report_templates.xml',
-        'views/sale_order_views.xml',
+    "data": [
+        "reports/ir_actions_report_templates.xml",
+        "views/sale_order_views.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
