41299d8a28943c60edd3e1d7e02af3ac351adb43
Hounaida <hs@openfire.fr>
2024-12-09 10:59:28 +0100

    of_geolocalize : T17839 - [IMP] - Modifications de termes
    On modifie les textes des paramètres de configuration de géolocalisation
     et leurs traductions pour une meilleure compréhension utilisateur.

--- of_geolocalize/__manifest__.py
+++ of_geolocalize/__manifest__.py
@@ -1,31 +1,31 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Geolocalisation",
-    'version': '16.0.1.1.0',
-    'license': 'AGPL-3',
-    'author': "OpenFire",
-    'website': "https://www.openfire.fr",
-    'category': "OpenFire",
-    'summary': "Module d'extension de la géolocalisation des partenaires",
-    'depends': [
-        'contacts',
-        'base_geolocalize',
-        'of_web_widgets',
+    "name": "OpenFire / Geolocalisation",
+    "version": "16.0.1.2.0",
+    "license": "AGPL-3",
+    "author": "OpenFire",
+    "website": "https://www.openfire.fr",
+    "category": "OpenFire",
+    "summary": "Module d'extension de la géolocalisation des partenaires",
+    "depends": [
+        "contacts",
+        "base_geolocalize",
+        "of_web_widgets",
     ],
-    'data': [
-        'data/ir_config_parameter.xml',
-        'security/ir.model.access.csv',
-        'views/res_config_settings_views.xml',
-        'views/res_partner_views.xml',
-        'wizards/of_geo_wizard.xml',
+    "data": [
+        "data/ir_config_parameter.xml",
+        "security/ir.model.access.csv",
+        "views/res_config_settings_views.xml",
+        "views/res_partner_views.xml",
+        "wizards/of_geo_wizard.xml",
     ],
-    'assets': {
-        'web.assets_backend': [
-            'of_geolocalize/static/src/**/*',
+    "assets": {
+        "web.assets_backend": [
+            "of_geolocalize/static/src/**/*",
         ],
     },
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
