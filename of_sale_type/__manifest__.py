eb02886045a1fedbcedda73aa0e65a2c7c0509c7
Hounaida <hs@openfire.fr>
2024-12-09 10:31:17 +0100

    of_sale_type : T17841 - [IMP] Renommer le champs pricelist_id
    On renomme le champ pricelist_id du type de bon de commande en Liste de prix.

--- of_sale_type/__manifest__.py
+++ of_sale_type/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "OpenFire / Types de devis",
-    "version": "16.0.1.2.0",
+    "version": "16.0.1.3.0",
     "license": "AGPL-3",
     "author": "OpenFire",
     "website": "https://www.openfire.fr",
