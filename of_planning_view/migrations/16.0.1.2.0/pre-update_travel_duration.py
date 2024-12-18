2e7290e13b86ffeb16c19d9979edb80192f46c7c
fanch <fl@openfire.fr>
2024-12-18 11:52:43 +0100

    of_planning_view : [FIX] Tester l’existence d’une colonne
    On veut tester l’existence de la colonne of_trip_duration avant de mettre à jour les colonnes of_travel_duration de calendar_event

--- of_planning_view/migrations/16.0.1.2.0/pre-update_travel_duration.py
+++ of_planning_view/migrations/16.0.1.2.0/pre-update_travel_duration.py
@@ -4,7 +4,19 @@
 def migrate(cr, version):
     cr.execute(
         """
-        UPDATE calendar_event
-        SET of_travel_duration = of_trip_duration
+        DO $$
+        BEGIN
+            -- Vérifier si la colonne of_trip_duration existe
+            IF EXISTS (
+                SELECT 1
+                FROM information_schema.columns
+                WHERE table_name = 'calendar_event'
+                  AND column_name = 'of_trip_duration'
+            ) THEN
+                -- Mettre à jour la colonne of_travel_duration
+                UPDATE calendar_event
+                SET of_travel_duration = of_trip_duration;
+            END IF;
+        END $$;
     """
     )
