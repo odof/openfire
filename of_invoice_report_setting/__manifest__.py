c331dd8fc7b815cef2a91e4bdeb8bb20b9e227f4
Maxime JACQUET <mj@openfire.fr>
2025-01-07 16:56:41 +0100

    of_invoice_report_setting, of_sale, of_sale_report_setting : [FIX] XPATH
    Depuis la mise en place du module `of_invoice_report_setting`via le
     commit db90481884c31375d8ffcd96133ed0d81f753193 il y avait un conflit
     de XPATH concernant l'en-tête `th_priceunit` du tableau de ligne de
     facture.
    
    Ce même en-tête était remplacé (retiré) dans le module
     `of_sale_report_setting` pour laisser place à deux autres colonnes.
    
    Ces modules ne sont pas dépendants l'un de l'autre et touchent aux mêmes
     colonnes de rapport.
    
    Afin de corriger le souci et de limiter d'autres potentiels dégâts sur
     les autres colonnes du rapport nous avons remplacé les différents
     `<xpath ... position="replace"/>` par des
     `<xpath ... position="attributes"/>` afin de cacher les éléments.
    
    Les nouvelles colonnes sont donc elles modifiées pour se positionner
     "après" et non plus "à la place de".
    
    ---
    
    Tasks: T12686, T17436

--- of_invoice_report_setting/__manifest__.py
+++ of_invoice_report_setting/__manifest__.py
@@ -1,24 +1,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "OpenFire / Paramétrage du rapport de facture",
-    'author': "OpenFire",
-    'version': '16.0.1.2.0',
-    'license': 'LGPL-3',
-    'category': 'OpenFire',
-    'summary': "Print options for Invoice report",
-    'website': 'https://www.openfire.fr',
-    'depends': [
-        'of_account',
+    "name": "OpenFire / Paramétrage du rapport de facture",
+    "author": "OpenFire",
+    "version": "16.0.1.2.1",
+    "license": "LGPL-3",
+    "category": "OpenFire",
+    "summary": "Print options for Invoice report",
+    "website": "https://www.openfire.fr",
+    "depends": [
+        "of_account",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/of_invoice_document_layout_views.xml',
-        'views/res_config_settings_views.xml',
-        'report/of_invoice_report_templates.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/of_invoice_document_layout_views.xml",
+        "views/res_config_settings_views.xml",
+        "report/of_invoice_report_templates.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    'post_init_hook': 'post_init_hook',
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "post_init_hook": "post_init_hook",
 }
--- of_sale_report_setting/__manifest__.py
+++ of_sale_report_setting/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "OpenFire / Paramétrage du rapport de vente",
-    "version": "16.0.1.2.0",
+    "version": "16.0.1.2.1",
     "license": "AGPL-3",
     "author": "OpenFire",
     "website": "https://www.openfire.fr",
