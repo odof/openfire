809890efb136ef42844f59fd75063a698354a0c5
Hounaida <hs@openfire.fr>
2025-01-14 15:10:39 +0100

    of_survey : T18093 - [IMP] Correction de end_date
    - Si on planifie une inter à partir de devis , on conserve la date de fin est toujours présente
    - Pouvoir changer la end_date manuellement

--- of_service/models/of_service_request.py
+++ of_service/models/of_service_request.py
@@ -503,7 +503,7 @@ class OFServiceRequest(models.Model):
             if request.task_id and not request.duration:
                 request.duration = request.task_id.duration
 
-    @api.depends("task_id", "next_date")
+    @api.depends("template_id", "next_date")
     def _compute_end_date(self):
         for request in self:
             request.end_date = request._get_end_date()
